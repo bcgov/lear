ce48b3d4159f45716e42774156ee384becf33d53
thor wolpert <thor@wolpert.ca>
2019-04-08 12:15:57 -0700

    altered application name to lowercase and excluded from lint, due to s2i expectations
    Signed-off-by: thor wolpert <thor@wolpert.ca>

--- legal-api/wsgi.py
+++ legal-api/wsgi.py
@@ -15,7 +15,8 @@
 """
 from legal_api import create_app
 
-APPLICATION = create_app()
+# Openshift s2i expects a lower case name of application
+application = create_app() # pylint: disable=invalid-name
 
 if __name__ == "__main__":
-    APPLICATION.run()
+    application.run()
