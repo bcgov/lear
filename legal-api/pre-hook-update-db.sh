af0991f325a2c55c6170cd8820e860ca6a2c76af
thor wolpert <thor@wolpert.ca>
2019-04-09 11:03:45 -0700

    comment out command line psql, as not in base image
    Signed-off-by: thor wolpert <thor@wolpert.ca>

--- legal-api/pre-hook-update-db.sh
+++ legal-api/pre-hook-update-db.sh
@@ -4,7 +4,7 @@ export X_SCLS=rh-python35 httpd24
 export LD_LIBRARY_PATH=/opt/rh/rh-python35/root/usr/lib64:/opt/rh/httpd24/root/usr/lib64
 export PATH=/opt/app-root/bin:/opt/rh/rh-python35/root/usr/bin:/opt/rh/httpd24/root/usr/bin:/opt/rh/httpd24/root/usr/sbin:/opt/app-root/src/.local/bin/:/opt/app-root/src/bin:/opt/app-root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'my_db'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE my_db"
+# psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'my_db'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE my_db"
 
 cd /opt/app-root/src
 echo 'ensure database is created;
