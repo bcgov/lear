a9d6b463099afc6dc18a76aba6f548ca36bd8be3
thor wolpert <thor@wolpert.ca>
2019-04-08 23:37:42 -0700

    ensure database has been created, or create it
    Signed-off-by: thor wolpert <thor@wolpert.ca>

---
+++ legal-api/pre_hook_create_database.py
@@ -0,0 +1,29 @@
+import contextlib
+import os
+import sys
+
+import sqlalchemy
+import sqlalchemy.exc
+
+from config import ProdConfig
+
+DB_ADMIN_PASSWORD = os.getenv('DB_ADMIN_PASSWORD', None)
+
+if not hasattr(ProdConfig, 'DB_NAME') or not DB_ADMIN_PASSWORD:
+    print("Unable to create database.", sys.stdout)
+    sys.exit(-1)
+
+DATABASE_URI = 'postgresql://postgres:{password}@{host}:{port}/{name}'.format(
+    password=DB_ADMIN_PASSWORD,
+    host=ProdConfig.DB_HOST,
+    port=int(ProdConfig.DB_PORT),
+    name='postgres',
+)
+
+with contextlib.suppress(sqlalchemy.exc.ProgrammingError):
+    with sqlalchemy.create_engine(
+        DATABASE_URI,
+        isolation_level='AUTOCOMMIT'
+    ).connect() as connection:
+        database = ProdConfig.DB_NAME
+        connection.execute(f'CREATE DATABASE {database}')
