173659dbfadb4fff958377c9e42e64cb5d14f273
jeznorth <twenty15creative@gmail.com>
2019-05-07 14:15:19 -0700

    Coops Prototype Development (#56)

---
+++ coops-prototype/src/components/ARFilingDates.vue
@@ -0,0 +1,84 @@
+<template>
+  <ul class="director-list">
+    <li class="container">
+
+      <label>
+        <span>Annual General Meeting Date</span>
+      </label>
+
+      <div class="value">
+        <v-menu
+          ref="menu1"
+          v-model="menu1"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          lazy
+          transition="scale-transition"
+          offset-y
+          full-width
+          max-width="290px"
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              solo
+              flat
+              v-model="dateFormatted"
+              label="Annual General Meeting Date"
+              hint="Example: MM/DD/YYYY"
+              persistent-hint
+              append-icon="event"
+              @blur="date = parseDate(dateFormatted)"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="date" no-title @input="menu1 = false"></v-date-picker>
+        </v-menu>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ARFilingDates',
+
+  data: vm => ({
+    date: new Date().toISOString().substr(0, 10),
+    dateFormatted: vm.formatDate(new Date().toISOString().substr(0, 10)),
+    menu1: false,
+    menu2: false
+  }),
+
+  computed: {
+    computedDateFormatted () {
+      return this.formatDate(this.date)
+    }
+  },
+
+  watch: {
+    date (val) {
+      this.dateFormatted = this.formatDate(this.date)
+    }
+  },
+
+  methods: {
+    formatDate (date) {
+      if (!date) return null
+
+      const [year, month, day] = date.split('-')
+      return `${month}/${day}/${year}`
+    },
+    parseDate (date) {
+      if (!date) return null
+
+      const [month, day, year] = date.split('/')
+      return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "../assets/styles/theme.styl"
+</style>
