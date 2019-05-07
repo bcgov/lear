5ace9361e2f168e30b9be9acc41af8abb92cac47
Jeremy Dunsdon <twenty15creative@gmail.com>
2019-05-07 14:12:31 -0700

    Coops Prototype Development

---
+++ coops-prototype/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <EntityInfo/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import EntityInfo from '@/components/EntityInfo.vue'
+
+@Component({
+  components: {
+    EntityInfo
+  }
+})
+export default class Home extends Vue {}
+</script>
