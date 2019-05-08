92679addcff3809aee8ba1da38f6430d82bef603
Kial Jinnah <kialj876@gmail.com>
2019-05-08 08:50:41 -0700

    Fixed redirect url
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- coops-ui/src/main.ts
+++ coops-ui/src/main.ts
@@ -7,7 +7,7 @@ import router from './router'
 import store from './store'
 import './plugins/vuetify'
 import './registerServiceWorker'
-import { API_URL } from '../public/config/configuration.js'
+import { API_URL, AUTH_URL } from '../public/config/configuration.js'
 
 Vue.use(Vuelidate)
 Vue.config.productionTip = false
@@ -16,7 +16,7 @@ axios.defaults.baseURL = API_URL
 console.log('Setting axios.baseURL to: ' + axios.defaults.baseURL)
 
 window.addEventListener('message', function (e) {
-  if (e.origin === process.env.VUE_APP_AUTH_URL) {
+  if (e.origin === AUTH_URL) {
     sessionStorage.setItem('KEYCLOAK_TOKEN', e.data)
     sessionStorage.setItem('REDIRECTED', 'false')
   }
