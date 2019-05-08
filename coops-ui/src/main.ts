86ecc39b5e0b84a318f676c815bce821fcc0f03d
kialj876 <kialj876@gmail.com>
2019-05-08 11:56:35 -0700

    fixed url issue (#64)
    * fixed url issue
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>
    
    * cleaned up logging statement
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- coops-ui/src/main.ts
+++ coops-ui/src/main.ts
@@ -7,20 +7,9 @@ import router from './router'
 import store from './store'
 import './plugins/vuetify'
 import './registerServiceWorker'
-import { API_URL, AUTH_URL } from '../public/config/configuration.js'
 
 Vue.use(Vuelidate)
 Vue.config.productionTip = false
-
-axios.defaults.baseURL = API_URL
-console.log('Setting axios.baseURL to: ' + axios.defaults.baseURL)
-
-window.addEventListener('message', function (e) {
-  if (e.origin === AUTH_URL) {
-    sessionStorage.setItem('KEYCLOAK_TOKEN', e.data)
-    sessionStorage.setItem('REDIRECTED', 'false')
-  }
-})
 
 new Vue({
   router,
--- coops-ui/src/router.ts
+++ coops-ui/src/router.ts
@@ -1,9 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import { AUTH_URL } from '../public/config/configuration.js'
+import axios from '@/axios-auth'
 
 Vue.use(Router)
+var payURL
+var authURL
+/* load configurations from file */
+var req = new XMLHttpRequest()
+// TODO - change request to async:true once UI is more complete - currently too quick because we jump straight to AR
+req.open('GET', '/config/configuration.json', false)
+req.setRequestHeader('Accept', 'application/json')
+req.setRequestHeader('ResponseType', 'application/json')
+req.onreadystatechange = function (response) {
+  if (req.readyState === 4) {
+    if (req.status === 200) {
+      axios.defaults.baseURL = JSON.parse(req.responseText)['API_URL']
+      console.log('Setting axios.baseURL to: ' + axios.defaults.baseURL)
+      payURL = JSON.parse(req.responseText)['PAY_URL']
+      console.log('Setting payURL to: ' + payURL)
+      authURL = JSON.parse(req.responseText)['AUTH_URL']
+      console.log('Setting AUTH_URL to: ' + authURL)
+    } else {
+      // nothing
+      console.log('could not load configurations')
+    }
+  }
+}
+req.send()
+Vue.mixin({
+  data: function () {
+    return {
+      get payURL () {
+        return payURL
+      },
+      get authURL () {
+        return authURL
+      }
+    }
+  }
+})
+
+window.addEventListener('message', function (e) {
+  if (e.origin === authURL) {
+    sessionStorage.setItem('KEYCLOAK_TOKEN', e.data)
+    sessionStorage.setItem('REDIRECTED', 'false')
+  }
+})
 
 let router = new Router({
   mode: 'history',
@@ -37,7 +80,7 @@ router.afterEach((to, from) => {
       } else {
         console.log('AUTH FAILED')
         sessionStorage.setItem('REDIRECTED', 'true')
-        window.location.href = AUTH_URL
+        window.location.href = authURL
       }
     }
   }
