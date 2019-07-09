45d9c0ae0c932160503d2dc49957e9d338b4936f
Severin Beauvais <severin.beauvais@gmail.com>
2019-07-09 09:19:45 -0700

    get addresses from API

--- coops-ui/src/components/Dashboard/AddressListSm.vue
+++ coops-ui/src/components/Dashboard/AddressListSm.vue
@@ -1,39 +1,65 @@
 <template>
   <ul class="list">
-    <li class="list-item">
-      {{ mailingAddress.street }}
-      {{ mailingAddress.city }}
-      {{ mailingAddress.country }}
-      {{ mailingAddress.region }}
+    <li class="list-item" v-if="mailingAddress">
+      {{ mailingAddress.streetAddress }}<br>
+      {{ mailingAddress.addressCity }}, {{ mailingAddress.addressRegion }}<br>
+      {{ mailingAddress.addressCountry }}
     </li>
-    <li class="list-item">
-      {{ deliveryAddress.street }}
-      {{ deliveryAddress.city }}
-      {{ deliveryAddress.country }}
-      {{ deliveryAddress.region }}
+    <li class="list-item" v-if="deliveryAddress">
+      {{ deliveryAddress.streetAddress }}<br>
+      {{ deliveryAddress.addressCity }}, {{ deliveryAddress.addressRegion }}<br>
+      {{ deliveryAddress.addressCountry }}
     </li>
   </ul>
 </template>
 
 <script>
+import axios from '@/axios-auth'
+import { mapState } from 'vuex'
+
 export default {
   name: 'AddressListSm',
 
   data () {
     return {
-      mailingAddress: {
-        street: '1234 Main Street',
-        city: 'Victoria',
-        country: 'Canada',
-        region: 'BC'
-      },
-      deliveryAddress: {
-        inherit: false,
-        street: '1234 Main Street',
-        city: 'Victoria',
-        country: 'Canada',
-        region: 'BC'
+      mailingAddress: null,
+      deliveryAddress: null
+    }
+  },
+
+  computed: {
+    ...mapState(['corpNum'])
+  },
+
+  mounted () {
+    // reload data for this page
+    this.getAddresses()
+  },
+
+  methods: {
+    getAddresses () {
+      if (this.corpNum) {
+        const url = this.corpNum + '/addresses'
+        axios.get(url).then(response => {
+          if (response && response.data && response.data.mailingAddress) {
+            this.mailingAddress = response.data.mailingAddress
+          } else {
+            console.log('getAddresses() error - invalid Mailing Address')
+          }
+          if (response && response.data && response.data.deliveryAddress) {
+            this.deliveryAddress = response.data.deliveryAddress
+          } else {
+            console.log('getAddresses() error - invalid Delivery Address')
+          }
+        }).catch(error => console.error('getAddresses() error =', error))
       }
+    }
+  },
+
+  watch: {
+    corpNum (val) {
+      // when Corp Num is set or changes, get new addresses
+      this.getAddresses()
     }
   }
 }
