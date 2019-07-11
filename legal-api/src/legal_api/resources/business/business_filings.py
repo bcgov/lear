ed5b2f49e23c2a28f8934763a3983bf09d5db36b
thor <thor@wolpert.ca>
2019-07-11 10:40:14 -0700

    changed error returned by failure to create an invoice (#145)
    Signed-off-by: thor wolpert <thor@wolpert.ca>

--- legal-api/src/legal_api/resources/business/business_filings.py
+++ legal-api/src/legal_api/resources/business/business_filings.py
@@ -111,7 +111,9 @@ class ListFilingResource(Resource):
         if not draft or draft.lower() != 'true':
             err_msg, err_code = ListFilingResource._create_invoice(business, filing)
             if err_code:
-                return jsonify(err_msg), err_code
+                reply = filing.json
+                reply['errors'] = [err_msg, ]
+                return jsonify(reply), err_code
 
         # all done
         return jsonify(filing.json),\
@@ -252,5 +254,6 @@ class ListFilingResource(Resource):
 
         pid = rv.json().get('id')
         filing.payment_token = pid
+        filing.status = Filing.Status.PENDING.value
         filing.save()
         return None, None
