03f77af2de75a89683ec538e4befa8ea3cd27ea2
Katie McGoff <katie@silverbirchsolutions.com>
2019-07-31 15:13:22 -0700

    added keycloak token to all axios calls (#204)

--- coops-ui/src/axios-auth.ts
+++ coops-ui/src/axios-auth.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const instance = axios.create()
 instance.interceptors.request.use(
   config => {
-    // config.headers.common['Authorization'] = `Bearer ${sessionStorage.getItem('KEYCLOAK_TOKEN')}`
+    config.headers.common['Authorization'] = `Bearer ${sessionStorage.getItem('KEYCLOAK_TOKEN')}`
     return config
   },
   error => Promise.reject(error)
