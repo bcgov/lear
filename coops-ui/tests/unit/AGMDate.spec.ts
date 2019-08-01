3a7debf979015c260a860d3f47a395c0035d884e
Severin Beauvais <severin.beauvais@gmail.com>
2019-08-01 16:06:06 -0700

    added unit test to check for subject issue changed tests from nextTick() to async-await

--- coops-ui/tests/unit/AGMDate.spec.ts
+++ coops-ui/tests/unit/AGMDate.spec.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import Vuelidate from 'vuelidate'
+import flushPromises from 'flush-promises'
 
 import store from '@/store/store'
 import AGMDate from '@/components/AnnualReport/AGMDate.vue'
@@ -11,14 +12,7 @@ Vue.use(Vuelidate)
 describe('AGMDate.vue', () => {
   let vm
 
-  function click (id) {
-    const button = vm.$el.querySelector(id)
-    const window = button.ownerDocument.defaultView
-    const click = new window.Event('click')
-    button.dispatchEvent(click)
-  }
-
-  beforeEach(done => {
+  beforeEach(async () => {
     // init store
     store.state.corpNum = 'CP0001191'
     store.state.currentDate = '2019/07/15'
@@ -28,9 +22,7 @@ describe('AGMDate.vue', () => {
     let instance = new Constructor({ store: store })
     vm = instance.$mount()
 
-    Vue.nextTick(() => {
-      done()
-    })
+    await flushPromises()
   })
 
   it('initializes the local variables properly', () => {
@@ -44,65 +36,52 @@ describe('AGMDate.vue', () => {
 
     // verify global state
     expect(vm.$store.state.agmDateValid).toBe(true)
-
-    console.log('Passed Test 1')
   })
 
-  it('sets date picker when text field is set', done => {
+  it('sets date picker when text field is set', async () => {
     vm.$data.dateFormatted = '2017/05/12'
 
-    Vue.nextTick(() => {
-      // check local variable
-      expect(vm.$data.date).toBe('2017-05-12')
+    await flushPromises()
 
-      // check global variable
-      expect(vm.agmDate).toBe('2017-05-12')
+    // check local variable
+    expect(vm.$data.date).toBe('2017-05-12')
 
-      console.log('Passed Test 2')
-      done()
-    })
+    // check global variable
+    expect(vm.agmDate).toBe('2017-05-12')
   })
 
-  it('sets text field when date picker is set', done => {
+  it('sets text field when date picker is set', async () => {
     vm.$data.date = '2017-12-05'
 
-    Vue.nextTick(() => {
-      // check local variable
-      expect(vm.$data.dateFormatted).toBe('2017/12/05')
+    await flushPromises()
 
-      // check global variable
-      expect(vm.agmDate).toBe('2017-12-05')
+    // check local variable
+    expect(vm.$data.dateFormatted).toBe('2017/12/05')
 
-      console.log('Passed Test 3')
-      done()
-    })
+    // check global variable
+    expect(vm.agmDate).toBe('2017-12-05')
   })
 
-  it('doesn\'t render checkbox in current year', done => {
+  it('doesn\'t render checkbox in current year', async () => {
     store.state.currentDate = '2019/07/15'
     store.state.ARFilingYear = 2019
 
-    Vue.nextTick(() => {
-      expect(vm.$el.querySelector('#agm-checkbox')).toBeNull()
-      console.log('Passed Test 4')
-      done()
-    })
+    await flushPromises()
+
+    expect(vm.$el.querySelector('#agm-checkbox')).toBeNull()
   })
 
-  it('invalidates the component when date is empty', done => {
+  it('invalidates the component when date is empty', async () => {
     vm.$data.dateFormatted = ''
 
-    Vue.nextTick(() => {
-      expect(vm.$store.state.agmDateValid).toBe(false)
-      expect(vm.$el.querySelector('.v-messages').textContent)
-        .toContain('An Annual General Meeting date is required.')
+    await flushPromises()
 
-      console.log('Passed Test 5')
-      done()
-    })
+    expect(vm.$store.state.agmDateValid).toBe(false)
+    expect(vm.$el.querySelector('.v-messages').textContent)
+      .toContain('An Annual General Meeting date is required.')
   })
 
-  it('invalidates the component when entered format is invalid', done => {
+  it('invalidates the component when entered format is invalid', async () => {
     const validationErrorInfo = vm.$el.querySelector('.validationErrorInfo')
 
     // check initial state
@@ -112,40 +91,37 @@ describe('AGMDate.vue', () => {
     // invalid length
     vm.$data.dateFormatted = '2017/06/1'
 
-    Vue.nextTick(() => {
-      expect(vm.$store.state.agmDateValid).toBe(false)
-      expect(validationErrorInfo.textContent).toContain('Date must be in format YYYY/MM/DD.')
+    await flushPromises()
 
-      // invalid first slash
-      vm.$data.dateFormatted = '2017.06/15'
+    expect(vm.$store.state.agmDateValid).toBe(false)
+    expect(validationErrorInfo.textContent).toContain('Date must be in format YYYY/MM/DD.')
 
-      Vue.nextTick(() => {
-        expect(vm.$store.state.agmDateValid).toBe(false)
-        expect(validationErrorInfo.textContent).toContain('Date must be in format YYYY/MM/DD.')
+    // invalid first slash
+    vm.$data.dateFormatted = '2017.06/15'
 
-        // invalid second slash
-        vm.$data.dateFormatted = '2017/06.15'
+    await flushPromises()
 
-        Vue.nextTick(() => {
-          expect(vm.$store.state.agmDateValid).toBe(false)
-          expect(validationErrorInfo.textContent).toContain('Date must be in format YYYY/MM/DD.')
+    expect(vm.$store.state.agmDateValid).toBe(false)
+    expect(validationErrorInfo.textContent).toContain('Date must be in format YYYY/MM/DD.')
 
-          // invalid third slash
-          vm.$data.dateFormatted = '2017/06///'
+    // invalid second slash
+    vm.$data.dateFormatted = '2017/06.15'
 
-          Vue.nextTick(() => {
-            expect(vm.$store.state.agmDateValid).toBe(false)
-            expect(validationErrorInfo.textContent).toContain('Date must be in format YYYY/MM/DD.')
+    await flushPromises()
 
-            console.log('Passed Test 6')
-            done()
-          })
-        })
-      })
-    })
+    expect(vm.$store.state.agmDateValid).toBe(false)
+    expect(validationErrorInfo.textContent).toContain('Date must be in format YYYY/MM/DD.')
+
+    // invalid third slash
+    vm.$data.dateFormatted = '2017/06///'
+
+    await flushPromises()
+
+    expect(vm.$store.state.agmDateValid).toBe(false)
+    expect(validationErrorInfo.textContent).toContain('Date must be in format YYYY/MM/DD.')
   })
 
-  it('invalidates the component when entered year is invalid', done => {
+  it('invalidates the component when entered year is invalid', async () => {
     const validationErrorInfo = vm.$el.querySelector('.validationErrorInfo')
 
     // check initial state
@@ -155,16 +131,13 @@ describe('AGMDate.vue', () => {
     // invalid year
     vm.$data.dateFormatted = '2018/01/01'
 
-    Vue.nextTick(() => {
-      expect(vm.$store.state.agmDateValid).toBe(false)
-      expect(validationErrorInfo.textContent).toContain('Please enter a date within 2017.')
+    await flushPromises()
 
-      console.log('Passed Test 7')
-      done()
-    })
+    expect(vm.$store.state.agmDateValid).toBe(false)
+    expect(validationErrorInfo.textContent).toContain('Please enter a date within 2017.')
   })
 
-  it('invalidates the component when entered month is invalid', done => {
+  it('invalidates the component when entered month is invalid', async () => {
     const validationErrorInfo = vm.$el.querySelector('.validationErrorInfo')
 
     store.state.currentDate = '2019/07/15'
@@ -177,71 +150,70 @@ describe('AGMDate.vue', () => {
     // invalid month number
     vm.$data.dateFormatted = '2019/13/01'
 
-    Vue.nextTick(() => {
-      expect(vm.$store.state.agmDateValid).toBe(false)
-      expect(validationErrorInfo.textContent).toContain('Please enter a valid month in the past.')
+    await flushPromises()
 
-      // invalid future month
-      vm.$data.dateFormatted = '2019/08/15'
+    expect(vm.$store.state.agmDateValid).toBe(false)
+    expect(validationErrorInfo.textContent).toContain('Please enter a valid month in the past.')
 
-      Vue.nextTick(() => {
-        expect(vm.$store.state.agmDateValid).toBe(false)
-        expect(validationErrorInfo.textContent).toContain('Please enter a valid month in the past.')
+    // invalid future month
+    vm.$data.dateFormatted = '2019/08/15'
 
-        console.log('Passed Test 8')
-        done()
-      })
-    })
+    await flushPromises()
+
+    expect(vm.$store.state.agmDateValid).toBe(false)
+    expect(validationErrorInfo.textContent).toContain('Please enter a valid month in the past.')
   })
 
-  it('invalidates the component when entered day is invalid', done => {
+  it('invalidates the component when entered day is invalid', async () => {
     const validationErrorInfo = vm.$el.querySelector('.validationErrorInfo')
 
     store.state.currentDate = '2019/07/15'
     store.state.ARFilingYear = 2019
 
-    // check initial state
+    // check initial state (date was initialized to 2019/01/01)
     expect(vm.$store.state.agmDateValid).toBe(true)
     expect(validationErrorInfo.textContent).toBe('')
 
-    // invalid day number
+    // invalid day number (Feb 29 on non leap year)
     vm.$data.dateFormatted = '2019/02/29'
 
-    Vue.nextTick(() => {
-      expect(vm.$store.state.agmDateValid).toBe(false)
-      expect(validationErrorInfo.textContent).toContain('Please enter a valid day in the past.')
+    await flushPromises()
 
-      // invalid future day
-      vm.$data.dateFormatted = '2019/07/16'
+    expect(vm.$store.state.agmDateValid).toBe(false)
+    expect(validationErrorInfo.textContent).toContain('Please enter a valid day in the past.')
 
-      Vue.nextTick(() => {
-        expect(vm.$store.state.agmDateValid).toBe(false)
-        expect(validationErrorInfo.textContent).toContain('Please enter a valid day in the past.')
+    // invalid day number (future day)
+    vm.$data.dateFormatted = '2019/07/20'
 
-        console.log('Passed Test 9')
-        done()
-      })
-    })
+    await flushPromises()
+
+    expect(vm.$store.state.agmDateValid).toBe(false)
+    expect(validationErrorInfo.textContent).toContain('Please enter a valid day in the past.')
+
+    // valid day number (day in the past, this year, with day number > current day number)
+    vm.$data.dateFormatted = '2019/06/20'
+
+    await flushPromises()
+
+    expect(vm.$store.state.agmDateValid).toBe(true)
+    expect(validationErrorInfo.textContent).toBe('')
   })
 
-  it('validates the component when No AGM is checked', done => {
+  it('validates the component when No AGM is checked', async () => {
     // set an invalid date
     vm.$data.dateFormatted = null
 
-    Vue.nextTick(() => {
-      // verify that state is now false
-      expect(vm.$store.state.agmDateValid).toBe(false)
+    await flushPromises()
 
-      // set No AGM
-      vm.$data.didNotHoldAGM = true
+    // verify that state is now false
+    expect(vm.$store.state.agmDateValid).toBe(false)
 
-      Vue.nextTick(() => {
-        // verify that state is now true
-        expect(vm.$store.state.agmDateValid).toBe(true)
+    // set No AGM
+    vm.$data.didNotHoldAGM = true
 
-        console.log('Passed Test 10')
-        done()
-      })
-    })
+    await flushPromises()
+
+    // verify that state is now true
+    expect(vm.$store.state.agmDateValid).toBe(true)
   })
 })
