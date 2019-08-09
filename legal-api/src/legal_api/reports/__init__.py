f25192898c87f49f7f5ed01b2d252a470975756f
Walter Moar <35532993+WalterMoar@users.noreply.github.com>
2019-08-09 16:44:09 -0700

    Fixed many bugs and layout problems with documents (#1012) (#234)

--- legal-api/src/legal_api/reports/__init__.py
+++ legal-api/src/legal_api/reports/__init__.py
@@ -9,8 +9,20 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-from .report_otann import ReportOtann
+import sys
+from flask import current_app
+from http import HTTPStatus
+
+from .report import Report
 
 
 def get_pdf(filing):
-    return ReportOtann(filing).get_pdf()
+    try:
+        return Report(filing).get_pdf()
+    except FileNotFoundError:
+        # We don't have a template for it, so it must only be available on paper.
+        return 'Available on paper only', HTTPStatus.NOT_FOUND
+    except:
+        current_app.logger.error("Unexpected error:", sys.exc_info())
+
+        return '', HTTPStatus.INTERNAL_SERVER_ERROR
