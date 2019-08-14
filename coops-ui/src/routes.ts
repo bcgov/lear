ebcbc4ca381bf6c9dbb8b5211aacdef875d3a945
Severin Beauvais <severin.beauvais@gmail.com>
2019-08-14 11:12:13 -0700

    moved routes out of router declaration

---
+++ coops-ui/src/routes.ts
@@ -0,0 +1,56 @@
+import Dashboard from '@/views/Dashboard.vue'
+import AnnualReport from '@/views/AnnualReport.vue'
+import StandaloneDirectorsFiling from '@/views/StandaloneDirectorsFiling.vue'
+import StandaloneOfficeAddressFiling from '@/views/StandaloneOfficeAddressFiling.vue'
+
+export default [
+  {
+    path: '/',
+    redirect: '/dashboard'
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: Dashboard,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/annual-report',
+    name: 'annual-report',
+    component: AnnualReport,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/standalone-directors',
+    name: 'standalone-directors',
+    component: StandaloneDirectorsFiling,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/standalone-addresses',
+    name: 'standalone-addresses',
+    component: StandaloneOfficeAddressFiling,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+  },
+  {
+    // default/fallback route
+    path: '*',
+    redirect: '/'
+  }
+]
