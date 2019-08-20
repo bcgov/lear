fe3d185b5c54d0846fe29281b8869e87a7d0b2f5
Kial Jinnah <kialj876@gmail.com>
2019-08-20 15:03:46 -0700

    took out rollback
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- colin-api/Jenkinsfiles/deploy-prod.groovy
+++ colin-api/Jenkinsfiles/deploy-prod.groovy
@@ -98,26 +98,4 @@ node {
             }
         }
     }
-    stage("Rollback?") {
-        def rollback = true
-        try {
-            timeout(time: 1, unit: 'DAYS') {
-                input message: "Rollback ${COMPONENT_NAME}-${TAG_NAME}?", id: "1234", submitter: 'admin,thorwolpert-admin,rarmitag-admin,kialj876-admin,katiemcgoff-edit,WalterMoar-admin'
-            }
-        } catch (Exception e) {
-            rollback = false
-        }
-        if (rollback) {
-            echo("Rolling back deployment ${COMPONENT_NAME}-${TAG_NAME}.")
-            def deploy = openshift.selector("dc", "${COMPONENT_NAME}-${TAG_NAME}")
-            try {
-                echo "here ${deploy}"
-                openshift.rollback(deploy)
-            } catch (Exception e) {
-                echo e.getMessage()
-                echo "here2 ${deploy}"
-                deploy.rollout().undo()
-            }
-        }
-    }
 }//end node
