b45e900d4e81b0d08aec8eb778ffb55d447b8c4d
thor wolpert <thor@wolpert.ca>
2019-09-14 21:21:38 -0700

    implemented calls to new authorization end-point
    Signed-off-by: thor wolpert <thor@wolpert.ca>

--- legal-api/src/legal_api/services/authz.py
+++ legal-api/src/legal_api/services/authz.py
@@ -11,10 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """This manages all of the authentication and authorization service."""
-from flask import g
+from http import HTTPStatus
+from typing import List
+
+from flask import current_app
 from flask_jwt_oidc import JwtManager
+from requests import Session, exceptions
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 
 STAFF_ROLE = 'staff'
@@ -22,19 +27,43 @@ BASIC_USER = 'basic'
 COLIN_SVC_ROLE = 'colin'
 
 
-def authorized(identifier: str, jwt: JwtManager) -> bool:
+def authorized(identifier: str, jwt: JwtManager, action: List[str]) -> bool:
     """Assert that the user is authorized to create filings against the business identifier."""
     # if they are registry staff, they are always authorized
-    if jwt.validate_roles([STAFF_ROLE]):
+    if not action or not identifier or not jwt:
+        return False
+
+    if jwt.validate_roles([STAFF_ROLE]) \
+            or jwt.validate_roles([COLIN_SVC_ROLE]):
         return True
 
-    if jwt.validate_roles([COLIN_SVC_ROLE]):
-        return True
+    if jwt.validate_roles([BASIC_USER]):
 
-    token = g.jwt_oidc_token_info
-    username = token.get('username', None)
+        template_url = current_app.config.get('AUTH_SVC_URL')
+        auth_url = template_url.format(**vars())
 
-    if username and jwt.validate_roles([BASIC_USER]) and identifier.upper() == username.upper():
-        return True
+        token = jwt.get_token_auth_header()
+        headers = {'Authorization': 'Bearer ' + token}
+        try:
+            http = Session()
+            retries = Retry(total=5,
+                            backoff_factor=0.1,
+                            status_forcelist=[500, 502, 503, 504])
+            http.mount('http://', HTTPAdapter(max_retries=retries))
+            rv = http.get(url=auth_url, headers=headers)
+
+            if rv.status_code != HTTPStatus.OK \
+                    or not rv.json().get('roles'):
+                return False
+
+            if all(elem.lower() in rv.json().get('roles') for elem in action):
+                return True
+
+        except (exceptions.ConnectionError,  # pylint: disable=broad-except
+                exceptions.Timeout,
+                ValueError,
+                Exception) as err:
+            current_app.logger.error(f'Authorization connection failure for {identifier}', err)
+            return False
 
     return False
