0a461f92e846b59e153e6a386387c05f45703571
kialj876 <kialj876@gmail.com>
2019-09-23 14:32:23 -0700

    * Additional flaking issues
    * Fixed issues with P.R

--- queue_services/entity-filer/src/entity_filer/filing_processors/annual_report.py
+++ queue_services/entity-filer/src/entity_filer/filing_processors/annual_report.py
@@ -22,8 +22,6 @@ from legal_api.models import Business, Filing
 
 from legal_api.services.filings import validations
 
-from entity_filer.service_utils import logger
-
 
 def process(business: Business, filing: Dict):
     """Render the annual_report onto the business model objects."""
