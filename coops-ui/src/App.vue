e05ef2fbcc5d901a07282851ae5b5d40a8aaef6d
kialj876 <kialj876@gmail.com>
2019-09-23 14:32:23 -0700

    address/director component disabled based on last filing dates (#333)
    * address/director compnent disable validation
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>
    
    * changed eslint plugins to devdependencies
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>
    
    Implemented Caddy, Docker files for ui, api, db, modified env variables
    
    Added changes to .env-wf
    
    Updated legal-api .env-wf with JWT properties
    
    Added Local Config file. Updated GitIgnore
    
    Updated Make file for local builds
    
    Ongoing containerization
    
    Update GitIgnore
    
    gitIgnore
    
    gitIgnore Updates
    
    gitIgnore Changes
    
    Ongoing GitIgnore
    
    Update config
    
    gitIgnore update
    
    .gitignore fix
    
    gitIgnore fix
    
    gitIgnore fix
    
    Reverted config and gitIgnore Change
    
    add jenkins build/deploy config
    
    fix library version
    
    Implemented EntityFilter. Unit Testing. Updated AR component
    
    CleanUp local dev Settings
    
    Clean Up Local Settings
    
    Local Dev Cleanup
    
    code for 2, 35, and 146
    
    Implemented ARDate Component UI & Unit testing.
    
    Implemented EntityFilter. Unit Testing. Updated AR component
    
    CleanUp local dev Settings
    
    Clean Up Local Settings
    
    Local Dev Cleanup
    
    Implemented ARDate Component UI & Unit testing.
    
    Fixed incorrect date method
    
    Implemented ARDate Functionality. Ongoing Unit testing
    
    Styling Updates.
    
    Implemented AR-Date Functionality & Unit Testing
    
    Add logic for tasklist based on rules for BCorp, fix nextAnniversary date
    
    Different task list, as BCORP AR date is on the anniversary of the business. Determine AR date
    
    Updated dateMixinFunc
    
    Updated Unit Testing
    
    Update configuration.json
    
    Update env-wf
    
    Update PR Recomendations

--- coops-ui/src/App.vue
+++ coops-ui/src/App.vue
@@ -228,7 +228,11 @@ export default {
     storeEntityInfo (response) {
       if (response && response.data && response.data.business) {
         this.setEntityName(response.data.business.legalName)
+<<<<<<< HEAD
         this.setEntityType(response.data.business.legalType)
+=======
+        this.setEntityType(EntityTypes.Coop)
+>>>>>>> a3a6614... address/director component disabled based on last filing dates (#333)
         this.setNextARDate(response.data.business.nextAnnualReport)
         this.setEntityStatus(response.data.business.status)
         this.setEntityBusinessNo(response.data.business.taxId)
