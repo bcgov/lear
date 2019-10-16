4b47d20c86dae598d71ecf10588ebe62ff92e9fb
Lekshmi Mallika <lekshmi.mallika@gov.bc.ca>
2019-10-16 16:12:35 -0700

    Fixing the issue in IE11

--- coops-ui/src/service-worker.js
+++ coops-ui/src/service-worker.js
@@ -3,7 +3,7 @@ workbox.precaching.suppressWarnings && workbox.precaching.suppressWarnings()
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
 // install new service worker when ok, then reload page.
-self.addEventListener('message', msg => {
+self.addEventListener && self.addEventListener('message', msg => {
   if (msg.data.action === 'skipWaiting') {
     self.skipWaiting()
   }
