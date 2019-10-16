dc0bbf8c58f1f969687946fbfff76ff36774d410
Lekshmi Mallika <lekshmi.mallika@gov.bc.ca>
2019-10-16 11:21:00 -0700

    PWA first commit

--- coops-ui/vue.config.js
+++ coops-ui/vue.config.js
@@ -3,5 +3,11 @@ module.exports = {
     devtool: 'source-map'
   },
   transpileDependencies: ['vuex-persist', 'vuetify'],
-  publicPath: process.env.VUE_APP_PATH
+  publicPath: process.env.VUE_APP_PATH,
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/service-worker.js'
+    }
+  }
 }
