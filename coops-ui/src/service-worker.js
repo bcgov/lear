3242c7cf3f1f289995fc9319a19a1d1ce9f908b3
Lekshmi Mallika <lekshmi.mallika@gov.bc.ca>
2019-10-16 13:25:09 -0700

    Fix for method does not exist

--- coops-ui/src/service-worker.js
+++ coops-ui/src/service-worker.js
@@ -1,5 +1,5 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.suppressWarnings()
+workbox.precaching.suppressWarnings && workbox.precaching.suppressWarnings()
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
 // install new service worker when ok, then reload page.
