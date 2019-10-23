ab33dbb54e61c8d0efd8f620ad1a8b6e2b12c0ac
thor wolpert <thor@wolpert.ca>
2019-10-23 13:04:54 -0700

    added futureEffectiveDate timestamp to the filing/header. Update all data and tests to support date-time schema checking.
    Signed-off-by: thor wolpert <thor@wolpert.ca>

--- schemas/tests/unit/test_todo.py
+++ schemas/tests/unit/test_todo.py
@@ -25,7 +25,7 @@ def test_valid_todo():
         'todo': {
             'business': {
                 'cacheId': 1,
-                'foundingDate': '2007-04-08',
+                'foundingDate': '2007-04-08T00:00:00+00:00',
                 'identifier': 'CP0002098',
                 'lastLedgerTimestamp': '2019-04-15T20:05:49.068272+00:00',
                 'legalName': 'Legal Name - CP0002098'
