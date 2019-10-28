2e9ac7644bddccc9ded90dcd27264c7883a15a36
thor <thor@wolpert.ca>
2019-10-28 11:48:05 -0700

    1605 effective date json (#444)

--- schemas/src/registry_schemas/example_data/schema_data.py
+++ schemas/src/registry_schemas/example_data/schema_data.py
@@ -24,11 +24,12 @@ FILING_HEADER = {
             'date': '2019-04-08',
             'certifiedBy': 'full name',
             'email': 'no_one@never.get',
-            'filingId': 1
+            'filingId': 1,
+            'effectiveDate': '2019-04-15T00:00:00+00:00'
         },
         'business': {
             'cacheId': 1,
-            'foundingDate': '2007-04-08',
+            'foundingDate': '2007-04-08T00:00:00+00:00',
             'identifier': 'CP1234567',
             'lastLedgerTimestamp': '2019-04-15T20:05:49.068272+00:00',
             'lastPreBobFilingTimestamp': '2019-04-15T20:05:49.068272+00:00',
@@ -40,7 +41,7 @@ FILING_HEADER = {
 
 BUSINESS = {
     'cacheId': 1,
-    'foundingDate': '2007-04-08',
+    'foundingDate': '2007-04-08T00:00:00+00:00',
     'identifier': 'CP1234567',
     'lastLedgerTimestamp': '2019-04-15T20:05:49.068272+00:00',
     'lastPreBobFilingTimestamp': '2019-04-15T20:05:49.068272+00:00',
@@ -59,7 +60,7 @@ ANNUAL_REPORT = {
         },
         'business': {
             'cacheId': 1,
-            'foundingDate': '2007-04-08',
+            'foundingDate': '2007-04-08T00:00:00+00:00',
             'identifier': 'CP1234567',
             'lastLedgerTimestamp': '2019-04-15T20:05:49.068272+00:00',
             'lastPreBobFilingTimestamp': '2019-01-01T20:05:49.068272+00:00',
@@ -271,7 +272,7 @@ FILING_TEMPLATE = {
         },
         'business': {
             'cacheId': 1,
-            'foundingDate': '2007-04-08',
+            'foundingDate': '2007-04-08T00:00:00+00:00',
             'identifier': 'CP1234567',
             'lastLedgerTimestamp': '2019-04-15T20:05:49.068272+00:00',
             'lastPreBobFilingTimestamp': '2019-04-15T20:05:49.068272+00:00',
