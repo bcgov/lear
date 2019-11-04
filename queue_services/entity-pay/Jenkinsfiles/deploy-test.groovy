10743683e0e7a15ee1a56da697ca2c51aa897430
thor wolpert <thor@wolpert.ca>
2019-11-03 17:42:39 -0800

    moved all of the payment processing to this service. altered to use the common queue services.
    Signed-off-by: thor wolpert <thor@wolpert.ca>

---
+++ queue_services/entity-pay/Jenkinsfiles/deploy-test.groovy
@@ -0,0 +1,199 @@
+#!/usr/bin/env groovy
+// Copyright © 2018 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//JENKINS DEPLOY ENVIRONMENT VARIABLES:
+// - JENKINS_JAVA_OVERRIDES  -Dhudson.model.DirectoryBrowserSupport.CSP= -Duser.timezone=America/Vancouver
+//   -> user.timezone : set the local timezone so logfiles report correxct time
+//   -> hudson.model.DirectoryBrowserSupport.CSP : removes restrictions on CSS file load, thus html pages of test reports are displayed pretty
+//   See: https://docs.openshift.com/container-platform/3.9/using_images/other_images/jenkins.html for a complete list of JENKINS env vars
+// define constants
+def NAMESPACE = 'gl2uos'
+def COMPONENT_NAME = 'entity-pay'
+def TAG_NAME = 'test'
+def SOURCE_TAG = 'dev'
+def E2E_TAG = 'e2e'
+def E2E_NAMESPACE = 'd7eovc'
+def E2E_PROJ = 'tools'
+
+// define groovy functions
+import groovy.json.JsonOutput
+
+// Get an image's hash tag
+String getImageTagHash(String imageName, String tag = "") {
+
+  if(!tag?.trim()) {
+    tag = "latest"
+  }
+
+  def istag = openshift.raw("get istag ${imageName}:${tag} -o template --template='{{.image.dockerImageReference}}'")
+  return istag.out.tokenize('@')[1].trim()
+}
+
+// pipeline
+// define job properties - keep 10 builds only
+properties([
+    [$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10'
+        ]
+    ]
+])
+
+node {
+    stage("Tag image for E2E") {
+        script {
+            openshift.withCluster() {
+                openshift.withProject() {
+                    echo "Tagging ${COMPONENT_NAME}:${E2E_TAG}-prev with ${E2E_TAG}..."
+
+                    def IMAGE_HASH = getImageTagHash("${COMPONENT_NAME}", "${E2E_TAG}")
+                    echo "IMAGE_HASH: ${IMAGE_HASH}"
+                    openshift.tag("${COMPONENT_NAME}@${IMAGE_HASH}", "${COMPONENT_NAME}:${E2E_TAG}-prev")
+
+                    echo "Tagging ${COMPONENT_NAME} to ${E2E_TAG} ..."
+
+                    IMAGE_HASH = getImageTagHash("${COMPONENT_NAME}", "${SOURCE_TAG}")
+                    echo "IMAGE_HASH: ${IMAGE_HASH}"
+                    openshift.tag("${COMPONENT_NAME}@${IMAGE_HASH}", "${COMPONENT_NAME}:${E2E_TAG}")
+                }
+            }
+        }
+    }
+    def passed = true
+    stage("Run E2E Tests") {
+        script {
+            try {
+                timeout(time: 1, unit: 'DAYS') {
+                    input message: "Run E2E pipeline?", id: "1234", submitter: 'admin,thorwolpert-admin,rarmitag-admin,kialj876-admin,katiemcgoff-admin,WalterMoar-admin,JohnamLane-edit'
+                }
+                openshift.withCluster() {
+                    openshift.withProject("${E2E_NAMESPACE}-${E2E_PROJ}") {
+                        def e2e_pipeline = openshift.selector('bc', 'e2e-pipeline')
+                        try {
+                            echo "Running e2e pipeline (check ${E2E_NAMESPACE}-${E2E_PROJ} to view progress)..."
+                            e2e_pipeline.startBuild('--wait=true').logs('-f')
+                            echo "E2E tests passed!"
+                        } catch (Exception e) {
+                            echo "E2E tests failed: ${e.getMessage()}"
+                            passed = false
+                        }
+                    }
+                }
+            } catch (Exception e0) {
+                echo "Did not run E2E pipeline."
+                passed = false
+            }
+        }
+    }
+    def end_pipeline = false
+    stage("Verify E2E Tests") {
+        script {
+            if (!passed) {
+                try {
+                    timeout(time: 1, unit: 'DAYS') {
+                        input message: "E2E failed or were not run. Proceed to test?", id: "1234", submitter: 'admin,thorwolpert-admin,rarmitag-admin,kialj876-admin,katiemcgoff-admin,WalterMoar-admin,JohnamLane-edit'
+                    }
+                } catch (Exception e1) {
+                    try {
+                        timeout(time: 1, unit: 'DAYS') {
+                            input message: "Keep E2E image?", id: "1234", submitter: 'admin,thorwolpert-admin,rarmitag-admin,kialj876-admin,katiemcgoff-admin,WalterMoar-admin,JohnamLane-edit'
+                        }
+                    } catch (Exception e2) {
+                        echo "Reverting E2E image back to previous image..."
+                        openshift.withCluster() {
+                            openshift.withProject() {
+                                echo "Tagging ${COMPONENT_NAME}:${E2E_TAG} with ${E2E_TAG}-prev ..."
+
+                                def IMAGE_HASH = getImageTagHash("${COMPONENT_NAME}", "${E2E_TAG}-prev")
+                                echo "IMAGE_HASH: ${IMAGE_HASH}"
+                                openshift.tag("${COMPONENT_NAME}@${IMAGE_HASH}", "${COMPONENT_NAME}:${E2E_TAG}")
+                            }
+                        }
+                    } finally {
+                        currentBuild.result = 'FAILURE'
+                        end_pipeline = true
+                        return
+                    }
+                }
+            }
+        }
+    }
+    if (!end_pipeline) {
+        def old_version
+        stage("Deploy ${COMPONENT_NAME}:${TAG_NAME}") {
+            script {
+                openshift.withCluster() {
+                    openshift.withProject("${NAMESPACE}-${TAG_NAME}") {
+                        old_version = openshift.selector('dc', "${COMPONENT_NAME}-${TAG_NAME}").object().status.latestVersion
+                    }
+                }
+                openshift.withCluster() {
+                    openshift.withProject() {
+
+                        echo "Tagging ${COMPONENT_NAME} for deployment to ${TAG_NAME} ..."
+
+                        // Don't tag with BUILD_ID so the pruner can do it's job; it won't delete tagged images.
+                        // Tag the images for deployment based on the image's hash
+                        def IMAGE_HASH = getImageTagHash("${COMPONENT_NAME}", "${SOURCE_TAG}")
+                        echo "IMAGE_HASH: ${IMAGE_HASH}"
+                        openshift.tag("${COMPONENT_NAME}@${IMAGE_HASH}", "${COMPONENT_NAME}:${TAG_NAME}")
+                    }
+                }
+            }
+        }
+        stage("Verify deployment") {
+            sleep 10
+            script {
+                openshift.withCluster() {
+                    openshift.withProject("${NAMESPACE}-${TAG_NAME}") {
+                        def new_version = openshift.selector('dc', "${COMPONENT_NAME}-${TAG_NAME}").object().status.latestVersion
+                        if (new_version == old_version) {
+                            echo "New deployment was not triggered."
+                            currentBuild.result = "FAILURE"
+                            return
+                        }
+                        def pod_selector = openshift.selector('pod', [ app:"${COMPONENT_NAME}-${TAG_NAME}" ])
+                        pod_selector.untilEach {
+                            deployment = it.objects()[0].metadata.labels.deployment
+                            echo deployment
+                            if (deployment ==  "${COMPONENT_NAME}-${TAG_NAME}-${new_version}" && it.objects()[0].status.phase == 'Running' && it.objects()[0].status.containerStatuses[0].ready) {
+                                return true
+                            } else {
+                                echo "Pod for new deployment not ready"
+                                sleep 5
+                                return false
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        stage("Run pytests for ${COMPONENT_NAME}:${TAG_NAME}") {
+            script {
+                openshift.withCluster() {
+                    openshift.withProject() {
+                        def test_pipeline = openshift.selector('bc', 'pytest-pipeline')
+                        try {
+                            test_pipeline.startBuild('--wait=true', "-e=component=${COMPONENT_NAME}", "-e=component_tag=${TAG_NAME}", "-e=tag=${TAG_NAME}", "-e=namespace=${NAMESPACE}", "-e=db_type=PG").logs('-f')
+                            echo "All tests passed"
+                        } catch (Exception e) {
+                            echo e.getMessage()
+                            echo "Not all tests passed."
+                            currentBuild.result = 'FAILURE'
+                        }
+                    }
+                }
+            }
+        }
+    }
+}//end node
---
+++ queue_services/entity-pay/src/entity_pay/worker.py
@@ -0,0 +1,139 @@
+# Copyright © 2019 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The unique worker functionality for this service is contained here.
+
+The entry-point is the **cb_subscription_handler**
+
+The design and flow leverage a few constraints that are placed upon it
+by NATS Streaming and using AWAIT on the default loop.
+- NATS streaming queues require one message to be processed at a time.
+- AWAIT on the default loop effectively runs synchronously
+
+If these constraints change, the use of Flask-SQLAlchemy would need to change.
+Flask-SQLAlchemy currently allows the base model to be changed, or reworking
+the model to a standalone SQLAlchemy usage with an async engine would need
+to be pursued.
+"""
+import asyncio
+import datetime
+import json
+import os
+
+import nats
+from flask import Flask
+from legal_api import db
+from legal_api.models import Filing
+from sentry_sdk import capture_message
+from sqlalchemy.exc import OperationalError
+from entity_queue_common.messages import create_filing_msg
+from entity_queue_common.service_utils import FilingException, QueueException, logger
+from entity_queue_common.service import QueueServiceManager
+
+
+from entity_pay import config
+
+qsm = QueueServiceManager()  # pylint: disable=invalid-name
+APP_CONFIG = config.get_named_config(os.getenv('DEPLOYMENT_ENV', 'production'))
+FLASK_APP = Flask(__name__)
+FLASK_APP.config.from_object(APP_CONFIG)
+db.init_app(FLASK_APP)
+
+
+def extract_payment_token(msg: nats.aio.client.Msg) -> dict:
+    """Return a dict of the json string in the Msg.data."""
+    return json.loads(msg.data.decode('utf-8'))
+
+
+def get_filing_by_payment_id(payment_id: int) -> Filing:
+    """Return the outcome of Filing.get_filing_by_payment_token."""
+    return Filing.get_filing_by_payment_token(str(payment_id))
+
+
+async def publish_filing(filing: Filing):
+    payload = create_filing_msg(filing.id)
+    subject = APP_CONFIG.FILER_PUBLISH_OPTIONS['subject']
+
+    await qsm.service.publish(subject, payload)
+
+
+async def process_payment(payment_token, flask_app):
+    """Render the payment status."""
+    if not flask_app:
+        raise QueueException('Flask App not available.')
+
+    with flask_app.app_context():
+
+        # try to find the filing 5 times before putting back on the queue - in case payment token ends up on the queue
+        # before it is assigned to filing.
+        counter = 1
+        filing_submission = None
+        while not filing_submission and counter <= 5:
+            filing_submission = get_filing_by_payment_id(payment_token['paymentToken'].get('id'))
+            counter += 1
+            if not filing_submission:
+                await asyncio.sleep(0.2)
+        if not filing_submission:
+            raise FilingException
+
+        if filing_submission.status == Filing.Status.COMPLETED.value:
+            # log and skip this
+            # it shouldn't be an error, but there could be something to investigate if
+            # multiple retries are happening on something that should have been completed.
+            logger.warning('Queue: Attempting to reprocess business.id=%s, filing.id=%s payment=%s',
+                           filing_submission.business_id, filing_submission.id, payment_token)
+            capture_message(f'Queue Issue:Attempting to reprocess business.id={filing_submission.business_id}, filing.id={filing_submission.id} payment={payment_token}')
+            return
+
+        if payment_token['paymentToken'].get('statusCode') == 'TRANSACTION_FAILED':
+            # TODO: The customer has cancelled out of paying, so we could note this better
+            # technically the filing is still pending payment/processing
+            return
+
+        if payment_token['paymentToken'].get('statusCode') == Filing.Status.COMPLETED.value:
+            filing_submission.payment_completion_date = datetime.datetime.utcnow()
+            db.session.add(filing_submission)
+            db.session.commit()
+
+            try:
+                await publish_filing(filing_submission)
+            except Exception as err:  # pylint: disable=broad-except, unused-variable; mark any failure for human review
+                capture_message('Queue Error: Failied to place filing:{filing_submission.id} on Queue with error:{err}', level='error')
+
+            return
+
+        # if we're here and haven't been able to action it,
+        # then we've received an unknown status and should throw an error
+        logger.error('Unknown payment status given: %s', payment_token['paymentToken'].get('statusCode'))
+        raise QueueException
+
+
+async def cb_subscription_handler(msg: nats.aio.client.Msg):
+    """Use Callback to process Queue Msg objects."""
+    try:
+        logger.info('Received raw message seq:%s, data=  %s', msg.sequence, msg.data.decode())
+        payment_token = extract_payment_token(msg)
+        logger.debug('Extracted payment token: %s', payment_token)
+        await process_payment(payment_token, FLASK_APP)
+    except OperationalError as err:
+        logger.error('Queue Blocked - Database Issue: %s', json.dumps(payment_token), exc_info=True)
+        raise err  # We don't want to handle the error, as a DB down would drain the queue
+    except FilingException as err:
+        logger.error('Queue Error - cannot find filing: %s'
+                     '\n\nThis message has been put back on the queue for reprocessing.',
+                     json.dumps(payment_token), exc_info=True)
+        raise err  # we don't want to handle the error, so that the message gets put back on the queue
+    except (QueueException, Exception):  # pylint: disable=broad-except
+        # Catch Exception so that any error is still caught and the message is removed from the queue
+        capture_message('Queue Error:' + json.dumps(payment_token), level='error')
+        logger.error('Queue Error: %s', json.dumps(payment_token), exc_info=True)
