501d8981aed3ad6823f65c76dbc1a318cf8623e0
Katie McGoff <katie@silverbirchsolutions.com>
2019-11-04 10:43:14 -0800

    Added Severin to pipeline permissions.

--- jobs/update-colin-filings/Jenkinsfiles/build.groovy
+++ jobs/update-colin-filings/Jenkinsfiles/build.groovy
@@ -93,7 +93,7 @@ if( triggerBuild("jobs/${COMPONENT_NAME}") == "" ) {
     node {
         try {
             timeout(time: 1, unit: 'DAYS') {
-                input message: "Run ${COMPONENT_NAME}-${TAG_NAME}-pipeline?", id: "1234", submitter: 'admin,thorwolpert-admin,rarmitag-admin,kialj876-admin,katiemcgoff-admin,WalterMoar-admin'
+                input message: "Run ${COMPONENT_NAME}-${TAG_NAME}-pipeline?", id: "1234", submitter: 'admin,thorwolpert-admin,rarmitag-admin,kialj876-admin,katiemcgoff-admin,WalterMoar-admin,severinbeauvais-edit'
             }
         } catch (Exception e) {
             run_pipeline = false;
