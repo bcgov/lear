3ff37be5cdf584d91f2b6abdefaf22b2b059166b
Severin Beauvais <severin.beauvais@gmail.com>
2019-11-14 10:24:10 -0800

    - deleted About route and page - deleted unused image - fixed font format issue (woff vs woff2)

--- coops-ui/src/routes.ts
+++ coops-ui/src/routes.ts
@@ -40,14 +40,14 @@ export default [
       requiresAuth: true
     }
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+  // },
   {
     // default/fallback route
     path: '*',
