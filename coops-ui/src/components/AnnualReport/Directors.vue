380915db9f99925e3bda5e3f78d153eadb06ec26
cameron-freshworks <53542131+cameron-freshworks@users.noreply.github.com>
2019-11-19 15:42:43 -0800

    #1824 white space bug (#513)
    * Minor Syntax Update
    
    * Added WhiteSpace Validation
    
    * White Space Validation on Director Name
    
    * Updated Spacing

--- coops-ui/src/components/AnnualReport/Directors.vue
+++ coops-ui/src/components/AnnualReport/Directors.vue
@@ -597,7 +597,8 @@ export default class Directors extends Mixins(DateMixin, ExternalMixin, EntityFi
    */
   private get directorFirstNameRules (): Array<Function> {
     return [
-      v => !!v || 'A first name is required'
+      v => !!v || 'A first name is required',
+      v => !/\s/g.test(v) || 'Invalid spaces'
     ]
   }
 
@@ -607,7 +608,8 @@ export default class Directors extends Mixins(DateMixin, ExternalMixin, EntityFi
    */
   private get directorLastNameRules (): Array<Function> {
     return [
-      v => !!v || 'A last name is required'
+      v => !!v || 'A last name is required',
+      v => !/\s/g.test(v) || 'Invalid spaces'
     ]
   }
 
