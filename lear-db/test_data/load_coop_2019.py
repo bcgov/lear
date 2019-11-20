351b2c29b6cb975979134ad5350b4919208d72ed
Katie McGoff <katie@silverbirchsolutions.com>
2019-11-20 09:21:35 -0800

    loader - include entities without AR filings. (#514)
    Original version assumed AR filing existed but not true for recently-incorporated entities.

--- lear-db/test_data/load_coop_2019.py
+++ lear-db/test_data/load_coop_2019.py
@@ -51,7 +51,8 @@ def create_business(db, business_json):
     business.last_ledger_timestamp = business_json['business']['lastLedgerTimestamp']
     business.legal_name = business_json['business']['legalName']
     business.founding_date = business_json['business']['foundingDate']
-    business.last_agm_date = datetime.date.fromisoformat(business_json['business']['lastAgmDate'])
+    business.last_agm_date = datetime.date.fromisoformat(business_json['business']['lastAgmDate']) \
+        if business_json['business']['lastAgmDate'] else None
     business.last_ar_date = datetime.date.fromisoformat(business_json['business']['lastArDate'])\
         if business_json['business']['lastArDate'] else business.last_agm_date
     business.legal_type = business_json['business']['legalType']
