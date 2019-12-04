df6dde78112ee512793896e38280f066fbe53909
Peter Veri <peter@freshworks.io>
2019-12-04 11:23:49 -0800

    fix for bug 2044

--- legal-api/src/legal_api/resources/business/business_tasks.py
+++ legal-api/src/legal_api/resources/business/business_tasks.py
@@ -80,27 +80,28 @@ class TaskListResource(Resource):
             tasks.append(task)
             order += 1
 
-        if check_agm:
-            last_ar_date = business.last_ar_date
-            if last_ar_date:
+        last_ar_date = business.last_ar_date
+        if last_ar_date and check_agm:
                 todo_start_date = (datetime(last_ar_date.year + 1, 1, 1)).date()
 
         # Retrieve all previous annual report filings. If there are existing AR filings, determine
         # the latest date of filing
         annual_report_filings = Filing.get_filings_by_type(business.id, 'annualReport')
         if annual_report_filings:
+            # get last AR date from annualReportDate; if not present in json, try annualGeneralMeetingDate and
+            # finally filing date
+            last_ar_date = \
+                annual_report_filings[0].filing_json['filing']['annualReport'].get('annualReportDate', None)
+            if not last_ar_date:
+                last_ar_date = annual_report_filings[0].filing_json['filing']['annualReport']\
+                    .get('annualGeneralMeetingDate', None)
+            if not last_ar_date:
+                last_ar_date = annual_report_filings[0].filing_date
+            last_ar_date = datetime.fromisoformat(last_ar_date)
             if check_agm:
-                # get last AR date from annualReportDate; if not present in json, try annualGeneralMeetingDate and
-                # finally filing date
-                last_ar_date = \
-                    annual_report_filings[0].filing_json['filing']['annualReport'].get('annualReportDate', None)
-                if not last_ar_date:
-                    last_ar_date = annual_report_filings[0].filing_json['filing']['annualReport']\
-                        .get('annualGeneralMeetingDate', None)
-                if not last_ar_date:
-                    last_ar_date = annual_report_filings[0].filing_date
-                last_ar_date = datetime.fromisoformat(last_ar_date)
                 todo_start_date = (datetime(last_ar_date.year+1, 1, 1)).date()
+            else:
+                todo_start_date = (last_ar_date+datedelta.YEAR).date()
 
         start_year = todo_start_date.year
 
