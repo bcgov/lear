983f4a0ff2e43c0f0315cb67c038d200ff1f15a3
Lekshmi Mallika <lekshmi.mallika@gov.bc.ca>
2019-12-05 15:17:58 -0800

    Renaming folder

--- coops-ui/src/components/common/index.ts
+++ coops-ui/src/components/common/index.ts
@@ -3,11 +3,13 @@ import OfficeAddresses from './OfficeAddresses.vue'
 import SummaryCertify from './SummaryCertify.vue'
 import SummaryDirectors from './SummaryDirectors.vue'
 import SummaryStaffPayment from './SummaryStaffPayment.vue'
+import SummaryOfficeAddresses from './SummaryOfficeAddresses.vue'
 
 export {
   ErrorContact,
   OfficeAddresses,
   SummaryCertify,
   SummaryDirectors,
-  SummaryStaffPayment
+  SummaryStaffPayment,
+  SummaryOfficeAddresses
 }
