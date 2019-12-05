10bcc4136155efd9419a918b269ad442d0d9de4f
Lekshmi Mallika <lekshmi.mallika@gov.bc.ca>
2019-12-05 12:16:57 -0800

    Changing the folder name

---
+++ coops-ui/src/components/common/SummaryStaffPayment.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card flat id="staff-payment-container">
+    <div class="routingslipnumber-container">
+      <label>Routing Slip Number</label>
+      <div class="value routingslipnumber">
+        <p>{{ value }}</p>
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component({})
+export default class SummaryStaffPayment extends Vue {
+  // Prop passed into this component.
+  @Prop({ default: '' })
+  private value: string
+}
+</script>
+
+<style lang="scss" scoped>
+// @import '@/assets/styles/theme.scss';
+
+#staff-payment-container {
+  margin-top: 1rem;
+  padding: 1.25rem 1.25rem 0.25rem 1.25rem;
+  font-size: 0.875rem;
+}
+
+.routingslipnumber-container {
+  display: flex;
+  flex-flow: column nowrap;
+  position: relative;
+
+  > label:first-child {
+    font-weight: 700;
+  }
+}
+
+@media (min-width: 768px) {
+  .routingslipnumber-container {
+    flex-flow: row nowrap;
+
+    > label:first-child {
+      flex: 0 0 auto;
+      padding-right: 2rem;
+      width: 14rem;
+    }
+  }
+}
+
+.value.routingslipnumber {
+  min-width: 35rem;
+}
+</style>
---
+++ coops-ui/src/components/common/index.ts
@@ -0,0 +1,13 @@
+import ErrorContact from './ErrorContact.vue'
+import OfficeAddresses from './OfficeAddresses.vue'
+import SummaryCertify from './SummaryCertify.vue'
+import SummaryDirectors from './SummaryDirectors.vue'
+import SummaryStaffPayment from './SummaryStaffPayment.vue'
+
+export {
+  ErrorContact,
+  OfficeAddresses,
+  SummaryCertify,
+  SummaryDirectors,
+  SummaryStaffPayment
+}
