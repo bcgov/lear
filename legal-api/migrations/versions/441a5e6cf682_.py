e529d252fae3421037a330649f484ffaf874694f
Peter Veri <peter@freshworks.io>
2019-12-05 12:39:04 -0800

    change the order of alembic migrations

--- legal-api/migrations/versions/441a5e6cf682_.py
+++ legal-api/migrations/versions/441a5e6cf682_.py
@@ -12,7 +12,7 @@ from sqlalchemy import Table, MetaData
 
 # revision identifiers, used by Alembic.
 revision = '441a5e6cf682'
-down_revision = '0fb2ae5711ed'
+down_revision = '9b5c719f32ef'
 branch_labels = None
 depends_on = None
 
