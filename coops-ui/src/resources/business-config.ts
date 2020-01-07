8df44089ed7902745610c2e5f33e2c49f4dd3f37
peter-freshworks <53542611+peter-freshworks@users.noreply.github.com>
2020-01-07 09:30:45 -0800

    1550 spelling errors (#636)
    * fix typos for 1550
    
    * Additional typos

--- coops-ui/src/resources/business-config.ts
+++ coops-ui/src/resources/business-config.ts
@@ -12,26 +12,26 @@ export class Business {
 
 export const configJson = [{
   typeEnum: 'BC',
-  displayName: 'Benefits Company',
+  displayName: 'Benefit Company',
   flows: [
     {
       feeCode: 'OTADD',
       displayName: 'Change Of Address',
-      certifyText: 'Note: It is an offence to make a false or misleading statement in' +
+      certifyText: 'Note: It is an offence to make a false or misleading statement in ' +
            'respect of a material fact in a record submitted to the Corporate Registry for filing. ' +
            'See sections 35 and 36 of the Business Corporations Act.'
     },
     {
       feeCode: 'OTANN',
       displayName: 'Annual Report',
-      certifyText: 'Note: It is an offence to make a false or misleading statement in' +
+      certifyText: 'Note: It is an offence to make a false or misleading statement in ' +
            'respect of a material fact in a record submitted to the Corporate Registry for filing. ' +
            'See section 51 of the Business Corporations Act.'
     },
     {
       feeCode: 'OTCDR',
       displayName: 'Change Of Directors',
-      certifyText: 'Note: It is an offence to make a false or misleading statement in' +
+      certifyText: 'Note: It is an offence to make a false or misleading statement in ' +
            'respect of a material fact in a record submitted to the Corporate Registry for filing. ' +
            'See section 127 of the Business Corporations Act.'
     }
@@ -44,23 +44,23 @@ export const configJson = [{
     {
       feeCode: 'OTADD',
       displayName: 'Change Of Address',
-      certifyText: 'Note: It is an offence to make a false or misleading statement in' +
+      certifyText: 'Note: It is an offence to make a false or misleading statement in ' +
            'respect of a material fact in a record submitted to the Corporate Registry for filing. ' +
-           'See section 27 of the Cooperative Associations Act.'
+           'See section 27 of the Cooperative Association Act.'
     },
     {
       feeCode: 'OTANN',
       displayName: 'Annual Report',
-      certifyText: 'Note: It is an offence to make a false or misleading statement in' +
+      certifyText: 'Note: It is an offence to make a false or misleading statement in ' +
            'respect of a material fact in a record submitted to the Corporate Registry for filing. ' +
-           'See section 126 of the Cooperative Associations Act.'
+           'See section 126 of the Cooperative Association Act.'
     },
     {
       feeCode: 'OTCDR',
       displayName: 'Change Of Directors',
-      certifyText: 'Note: It is an offence to make a false or misleading statement in' +
+      certifyText: 'Note: It is an offence to make a false or misleading statement in ' +
            'respect of a material fact in a record submitted to the Corporate Registry for filing. ' +
-           'See section 78 of the Cooperative Associations Act.'
+           'See section 78 of the Cooperative Association Act.'
     }
   ]
 }]
