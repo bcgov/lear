520e66cc01511b4defc2c1bc2aa395dac7a2ef61
Katie McGoff <katie@silverbirchsolutions.com>
2020-02-20 09:58:34 -0800

    Update version.py

--- legal-api/src/legal_api/version.py
+++ legal-api/src/legal_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.5.2'  # pylint: disable=invalid-name
+__version__ = '2.6.0'  # pylint: disable=invalid-name
