44eb5184a082b62a4dcce9db12dbec5655786ab2
Peter Veri <peter@freshworks.io>
2020-02-24 12:58:32 -0800

    flake8 and pylint errors

--- colin-api/tests/unit/api/test_business.py
+++ colin-api/tests/unit/api/test_business.py
@@ -44,6 +44,7 @@ def test_get_business_no_results(client):
     assert 404 == rv.status_code
     assert None is not rv.json['message']
 
+
 @oracle_integration
 def test_get_business_new_corp(client):
     """Assert that a new corp number can be retrieved from COLIN."""
