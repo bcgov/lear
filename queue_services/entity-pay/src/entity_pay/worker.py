b3145c13c205f4c90ce2f93669d8697fad6d61a6
kialj876 <kialj876@gmail.com>
2020-06-15 12:16:57 -0700

    fixed publish email message (#866)
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- queue_services/entity-pay/src/entity_pay/worker.py
+++ queue_services/entity-pay/src/entity_pay/worker.py
@@ -121,7 +121,7 @@ async def process_payment(payment_token, flask_app):
 
             try:
                 await publish_email_message(
-                    qsm, filing_submission, APP_CONFIG.EMAIL_PUBLISH_OPTIONS['subject'], 'filed')
+                    qsm, APP_CONFIG.EMAIL_PUBLISH_OPTIONS['subject'], filing_submission, 'filed')
             except Exception as err:  # pylint: disable=broad-except, unused-variable # noqa F841;
                 # mark any failure for human review
                 capture_message(
