a092021eaa1735b5cfe827028e441b53bf6d8d30
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2020-07-08 13:12:25 -0700

    - merged arrays instead of nesting them (#903)
    - updated unit tests accordingly

--- legal-api/tests/unit/services/filings/validations/test_incorporation_application.py
+++ legal-api/tests/unit/services/filings/validations/test_incorporation_application.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Test suite to ensure the Annual Report is validated correctly."""
+"""Test suite to ensure the Incorporation Application is validated correctly."""
 import copy
 from datetime import date
 from http import HTTPStatus
@@ -40,25 +40,25 @@ effective_date = '2020-09-18T00:00:00+00:00'
     [
         ('SUCCESS', 'BC', 'CA', 'BC', 'CA', None, None),
         ('FAIL_NOT_BC_DELIVERY_REGION', 'AB', 'CA', 'BC', 'CA',
-            HTTPStatus.BAD_REQUEST, [[
+            HTTPStatus.BAD_REQUEST, [
                {'error': "Address Region must be 'BC'.",
                 'path':
                 '/filing/incorporationApplication/offices/registeredOffice/deliveryAddress/addressRegion'},
                {'error': "Address Region must be 'BC'.",
                 'path':
                 '/filing/incorporationApplication/offices/recordsOffice/deliveryAddress/addressRegion'}
-            ]]),
+            ]),
         ('FAIL_NOT_BC_MAILING_REGION', 'BC', 'CA', 'AB', 'CA',
-            HTTPStatus.BAD_REQUEST, [[
+            HTTPStatus.BAD_REQUEST, [
                 {'error': "Address Region must be 'BC'.",
                  'path':
                  '/filing/incorporationApplication/offices/registeredOffice/mailingAddress/addressRegion'},
                 {'error': "Address Region must be 'BC'.",
                  'path':
                  '/filing/incorporationApplication/offices/recordsOffice/mailingAddress/addressRegion'}
-            ]]),
+            ]),
         ('FAIL_ALL_ADDRESS_REGIONS', 'WA', 'CA', 'WA', 'CA',
-            HTTPStatus.BAD_REQUEST, [[
+            HTTPStatus.BAD_REQUEST, [
                 {'error': "Address Region must be 'BC'.",
                  'path': '/filing/incorporationApplication/offices/registeredOffice/deliveryAddress/addressRegion'},
                 {'error': "Address Region must be 'BC'.",
@@ -67,23 +67,23 @@ effective_date = '2020-09-18T00:00:00+00:00'
                  'path': '/filing/incorporationApplication/offices/recordsOffice/deliveryAddress/addressRegion'},
                 {'error': "Address Region must be 'BC'.",
                  'path': '/filing/incorporationApplication/offices/recordsOffice/mailingAddress/addressRegion'}
-            ]]),
+            ]),
         ('FAIL_NOT_DELIVERY_COUNTRY', 'BC', 'NZ', 'BC', 'CA',
-            HTTPStatus.BAD_REQUEST, [[
+            HTTPStatus.BAD_REQUEST, [
                 {'error': "Address Country must be 'CA'.",
                  'path': '/filing/incorporationApplication/offices/registeredOffice/deliveryAddress/addressCountry'},
                 {'error': "Address Country must be 'CA'.",
                  'path': '/filing/incorporationApplication/offices/recordsOffice/deliveryAddress/addressCountry'}
-            ]]),
+            ]),
         ('FAIL_NOT_MAILING_COUNTRY', 'BC', 'CA', 'BC', 'NZ',
-            HTTPStatus.BAD_REQUEST, [[
+            HTTPStatus.BAD_REQUEST, [
                 {'error': "Address Country must be 'CA'.",
                  'path': '/filing/incorporationApplication/offices/registeredOffice/mailingAddress/addressCountry'},
                 {'error': "Address Country must be 'CA'.",
                  'path': '/filing/incorporationApplication/offices/recordsOffice/mailingAddress/addressCountry'}
-            ]]),
+            ]),
         ('FAIL_ALL_ADDRESS', 'AB', 'NZ', 'AB', 'NZ',
-            HTTPStatus.BAD_REQUEST, [[
+            HTTPStatus.BAD_REQUEST, [
                 {'error': "Address Region must be 'BC'.",
                  'path': '/filing/incorporationApplication/offices/registeredOffice/deliveryAddress/addressRegion'},
                 {'error': "Address Country must be 'CA'.",
@@ -100,7 +100,7 @@ effective_date = '2020-09-18T00:00:00+00:00'
                  'path': '/filing/incorporationApplication/offices/recordsOffice/mailingAddress/addressRegion'},
                 {'error': "Address Country must be 'CA'.",
                  'path': '/filing/incorporationApplication/offices/recordsOffice/mailingAddress/addressCountry'}
-            ]])
+            ])
     ])
 def test_validate_incorporation_addresses_basic(session, test_name, delivery_region, delivery_country, mailing_region,
                                                 mailing_country, expected_code, expected_msg):
@@ -144,15 +144,15 @@ def test_validate_incorporation_addresses_basic(session, test_name, delivery_reg
     [
         ('SUCCESS', 'Completing Party', 'Director', 'Incorporator', 'Director', None, None),
         ('FAIL_NO_COMPLETING_PARTY', 'Director', 'Incorporator', 'Incorporator', 'Director',
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Must have a minimum of one completing party',
                 'path': '/filing/incorporationApplication/parties/roles'
-            }]]),
+            }]),
         ('FAIL_EXCEEDING_ONE_COMPLETING_PARTY', 'Completing Party', 'Director', 'Incorporator', 'Completing Party',
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Must have a maximum of one completing party',
                 'path': '/filing/incorporationApplication/parties/roles'
-            }]])
+            }])
     ])
 def test_validate_incorporation_role(session, test_name, role_1, role_2, role_3, role_4, expected_code, expected_msg):
     """Assert that incorporation parties roles can be validated."""
@@ -188,30 +188,30 @@ def test_validate_incorporation_role(session, test_name, role_1, role_2, role_3,
     [
         ('SUCCESS', '123 st', 'Vancouver', 'CA', 'h0h0h0', 'BC', None, None),
         ('FAIL_INVALID_STREET', None, 'Vancouver', 'CA', 'h0h0h0', 'BC',
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Person 1: Mailing address streetAddress None is invalid',
                 'path': '/filing/incorporationApplication/parties/1/mailingAddress/streetAddress/None/'
-            }]]),
+            }]),
         ('FAIL_INVALID_CITY', '123 St', None, 'CA', 'h0h0h0', 'BC',
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Person 1: Mailing address addressCity None is invalid',
                 'path': '/filing/incorporationApplication/parties/1/mailingAddress/addressCity/None/'
-            }]]),
+            }]),
         ('FAIL_INVALID_COUNTRY', '123 St', 'Vancouver', None, 'h0h0h0', 'BC',
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Person 1: Mailing address addressCountry None is invalid',
                 'path': '/filing/incorporationApplication/parties/1/mailingAddress/addressCountry/None/'
-            }]]),
+            }]),
         ('FAIL_INVALID_POSTAL_CODE', '123 St', 'Vancouver', 'CA', None, 'BC',
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Person 1: Mailing address postalCode None is invalid',
                 'path': '/filing/incorporationApplication/parties/1/mailingAddress/postalCode/None/'
-            }]]),
+            }]),
         ('FAIL_INVALID_REGION', '123 st', 'Vancouver', 'CA', 'h0h0h0', None,
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Person 1: Mailing address addressRegion None is invalid',
                 'path': '/filing/incorporationApplication/parties/1/mailingAddress/addressRegion/None/'
-            }]])
+            }])
     ])
 def test_validate_incorporation_parties_mailing_address(session, test_name, mock_street, mock_city, mock_country,
                                                         mock_postal_code, mock_region, expected_code, expected_msg):
@@ -252,31 +252,31 @@ def test_validate_incorporation_parties_mailing_address(session, test_name, mock
         ('SUCCESS', False, None, True, 0.875, 'CAD', True, 1000, None, None),
         ('SUCCESS', False, None, False, None, None, False, None, None, None),
         ('FAIL_INVALID_CLASS_MAX_SHARES', True, None, True, 0.875, 'CAD', True, 1000,
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Share class Share Class 1 must provide value for maximum number of shares',
                 'path': '/filing/incorporationApplication/shareClasses/0/maxNumberOfShares/'
-            }]]),
+            }]),
         ('FAIL_INVALID_CURRENCY', True, 5000, True, 0.875, None, True, 1000,
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Share class Share Class 1 must specify currency',
                 'path': '/filing/incorporationApplication/shareClasses/0/currency/'
-            }]]),
+            }]),
         ('FAIL_INVALID_PAR_VALUE', True, 5000, True, None, 'CAD', True, 1000,
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Share class Share Class 1 must specify par value',
                 'path': '/filing/incorporationApplication/shareClasses/0/parValue/'
-            }]]),
+            }]),
         ('FAIL_INVALID_SERIES_MAX_SHARES', True, 5000, True, 0.875, 'CAD', True, None,
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Share series Share Series 1 must provide value for maximum number of shares',
                 'path': '/filing/incorporationApplication/shareClasses/0/series/0/maxNumberOfShares'
-            }]]),
+            }]),
         ('FAIL_SERIES_SHARES_EXCEEDS_CLASS_SHARES', True, 5000, True, 0.875, 'CAD', True, 10000,
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error':
                 'Series Share Series 1 share quantity must be less than or equal to that of its class Share Class 1',
                 'path': '/filing/incorporationApplication/shareClasses/0/series/0/maxNumberOfShares'
-            }]])
+            }])
     ])
 def test_validate_incorporation_share_classes(session, test_name, class_has_max_shares, class_max_shares,
                                               has_par_value, par_value, currency, series_has_max_shares,
@@ -315,17 +315,17 @@ def test_validate_incorporation_share_classes(session, test_name, class_has_max_
         ('SUCCESS', '2020-09-18T00:00:00+00:00', None, None),
         ('SUCCESS', None, None, None),
         ('FAIL_INVALID_DATE_TIME_FORMAT', '2020-09-18T00:00:00Z',
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': '2020-09-18T00:00:00Z is an invalid ISO format for effective_date.'
-            }]]),
+            }]),
         ('FAIL_INVALID_DATE_TIME_MINIMUM', '2020-09-17T00:01:00+00:00',
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Invalid Datetime, effective date must be a minimum of 2 minutes ahead.'
-            }]]),
+            }]),
         ('FAIL_INVALID_DATE_TIME_MAXIMUM', '2020-09-27T00:01:00+00:00',
-            HTTPStatus.BAD_REQUEST, [[{
+            HTTPStatus.BAD_REQUEST, [{
                 'error': 'Invalid Datetime, effective date must be a maximum of 10 days ahead.'
-            }]])
+            }])
     ])
 def test_validate_incorporation_effective_date(session, test_name, effective_date, expected_code, expected_msg):
     """Assert that validator validates share class correctly."""
