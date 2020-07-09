896e05d3d73acda3602e56b4920b543bf6a9ac97
Lekshmi Mallika <lekshmi.mallika@aot-technologies.com>
2020-07-09 13:04:51 -0700

    Name Translation model changes

--- legal-api/src/legal_api/models/__init__.py
+++ legal-api/src/legal_api/models/__init__.py
@@ -13,11 +13,12 @@
 # limitations under the License.
 
 """This exports all of the models and schemas used by the application."""
-from .db import db  # noqa: I001
 from .address import Address
+from .alias import Alias
 from .business import Business  # noqa: I001
 from .colin_update import ColinLastUpdate
 from .comment import Comment
+from .db import db  # noqa: I001
 from .filing import Filing
 from .office import Office, OfficeType
 from .party_role import Party, PartyRole
@@ -28,6 +29,6 @@ from .user import User
 
 
 __all__ = ('db',
-           'Address', 'Business', 'ColinLastUpdate', 'Comment', 'Filing',
+           'Address', 'Alias', 'Business', 'ColinLastUpdate', 'Comment', 'Filing',
            'Office', 'OfficeType', 'Party', 'RegistrationBootstrap',
            'PartyRole', 'ShareClass', 'ShareSeries', 'User')
