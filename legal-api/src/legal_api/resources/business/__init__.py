a98c3cb918ada8163fc612126fcf0cc98cc06568
Lekshmi <lekshmi.mallika@aot-technologies.com>
2020-07-13 17:22:37 -0700

    Endpoint for resolutions (#910)

--- legal-api/src/legal_api/resources/business/__init__.py
+++ legal-api/src/legal_api/resources/business/__init__.py
@@ -21,6 +21,7 @@ from .business_address import AddressResource
 from .business_aliases import AliasResource
 from .business_directors import DirectorResource
 from .business_filings import ListFilingResource
+from .business_resolutions import ResolutionResource
 from .business_share_classes import ShareClassResource
 from .business_tasks import TaskListResource
 from .filing_comments import CommentResource
