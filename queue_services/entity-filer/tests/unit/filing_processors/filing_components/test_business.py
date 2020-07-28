bb285085f531f74e9d04ad1efbf4c09593249f8a
thor wolpert <thor@wolpert.ca>
2020-07-28 12:39:11 -0700

    saving, as switching to other changes

--- queue_services/entity-filer/tests/unit/filing_processors/filing_components/test_business.py
+++ queue_services/entity-filer/tests/unit/filing_processors/filing_components/test_business.py
@@ -11,7 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""This module contains all of the Legal Filing specific processors.
+"""The Unit Tests for the business filing component processors."""
+from legal_api.models import Business
 
-Processors hold the business logic for how a filing is interpreted and saved to the legal database.
-"""
+from entity_filer.filing_processors.filing_components import business
+
+
+def test_set_corp_type(app, session):
+    new_info_json = {
+        'business': {
+            'corpType': 'benefitCompany'
+        }}
+
+    business = Business()
+    business, err = business.set_corp_type(business, new_info_json)
