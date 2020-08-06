06b24edb728a10b6776d7d9ad525dbf5beccd46e
Kial Jinnah <kialj876@gmail.com>
2020-08-06 16:07:35 -0700

    finished maintenance code and tests
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- colin-api/src/colin_api/models/filing.py
+++ colin-api/src/colin_api/models/filing.py
@@ -352,9 +352,10 @@ class Filing:
             querystring += ' AND extract(year from PERIOD_END_DT)=:year'
 
         querystring += ' order by EVENT_TIMESTMP desc'
-
+        print(querystring)
         try:
             filing_type_cd = filing.get_filing_type_code()
+            print(filing_type_cd)
             if not cursor:
                 cursor = DB.connection.cursor()
             if filing.event_id:
