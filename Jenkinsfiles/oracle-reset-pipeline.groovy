4e34ecb7ed8d0f642521b7b8519c7c6629c0f669
Kial Jinnah <kialj876@gmail.com>
2020-08-31 14:11:19 -0700

    test 19
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- Jenkinsfiles/oracle-reset-pipeline.groovy
+++ Jenkinsfiles/oracle-reset-pipeline.groovy
@@ -20,13 +20,29 @@
 //   -> user.timezone : set the local timezone so logfiles report correxct time
 //   -> hudson.model.DirectoryBrowserSupport.CSP : removes restrictions on CSS file load, thus html pages of test reports are displayed pretty
 //   See: https://docs.openshift.com/container-platform/3.9/using_images/other_images/jenkins.html for a complete list of JENKINS env vars
-def NAMESPACE = namespace
-def TAG_NAME = tag
-def COMPONENT_NAME = component
-def COMPONENT_TAG = component_tag
+NAMESPACE = namespace
+TAG_NAME = tag
+COMPONENT_NAME = component
+COMPONENT_TAG = component_tag
+
 def OLD_POD
 def USERNAME
 def PASSWORD
+
+def execute_pod_command(pod, command, is_sql) {
+    if (is_sql) {
+        def run_sql = "echo \\\"${command}\\\"|\\\"\\\$ORACLE_HOME/bin/sqlplus\\\" / as sysdba"
+        command = run_sql
+    }
+    echo "${pod} executing ${command}..."
+    def command_output = openshift.exec(
+        pod,
+        '--',
+        "bash -c \"${command}\""
+    ).actions[0].out
+    echo command_output
+    return command_output[0]
+}
 
 node {
     stage('reset oracle') {
@@ -41,98 +57,67 @@ node {
             openshift.withCluster() {
                 openshift.withProject("${NAMESPACE}-${TAG_NAME}") {
                     def podSelector = openshift.selector('pod', [ app:"${COMPONENT_NAME}-${COMPONENT_TAG}" ])
-
                     OLD_POD = podSelector.objects()[0].metadata.name
                     echo "OLD_POD: ${OLD_POD}"
-                    def shutdownDb = openshift.exec(
-                        OLD_POD,
-                        '--',
-                        "bash -c ' \
-                        echo \"shutdown abort;\"|\"\$ORACLE_HOME/bin/sqlplus\" / as sysdba \
-                        '"
-                    ).actions[0].out
-                    echo shutdownDb
-                    def deleteORCL = openshift.exec(
-                        OLD_POD,
-                        '--',
-                        "bash -c '\
-                        rm -Rf /ORCL/*'"
-                    ).actions[0].out
-                    echo deleteORCL
-                    try {
-                        def resetORCL = openshift.exec(
-                            OLD_POD,
-                            '--',
-                            "bash -c '\
-                            cp -a /ORCL_base/. /ORCL/'"
-                        ).actions[0].out
-                        echo resetORCL
-                    } catch (Exception e) {
-                        echo e.getMessage()
-                    }
+
+                    sql = "select id_num from C##CDEV.system_id where id_typ_cd in ('BC');"
+                    echo execute_pod_command(OLD_POD, sql, true)
+
+                    // sql = 'shutdown abort;'
+                    // execute_pod_command(OLD_POD, run_sql)
+                    // execute_pod_command(OLD_POD, 'rm -Rf /ORCL/*')
+                    // try {
+                    //     execute_pod_command(OLD_POD, 'cp -a /ORCL_base/. /ORCL/')
+                    // } catch (Exception e) {
+                    //     echo e.getMessage()
+                    // }
                 }
             }
         }
     } // end stage
     stage('deploy oracle') {
-        script {
-            openshift.withCluster() {
-                openshift.withProject("${NAMESPACE}-${TAG_NAME}") {
-                    def deploy = openshift.selector("dc", "${COMPONENT_NAME}-${COMPONENT_TAG}")
-                    def podSelector = openshift.selector('pod', [ app:"${COMPONENT_NAME}-${COMPONENT_TAG}" ])
+        // script {
+        //     openshift.withCluster() {
+        //         openshift.withProject("${NAMESPACE}-${TAG_NAME}") {
+        //             def deploy = openshift.selector("dc", "${COMPONENT_NAME}-${COMPONENT_TAG}")
+        //             def podSelector = openshift.selector('pod', [ app:"${COMPONENT_NAME}-${COMPONENT_TAG}" ])
 
-                    deploy.rollout().latest()
-                    def count = 1
-                    podSelector.untilEach {
-                        def pod = it.objects()[0].metadata.name
-                        echo "pod: ${pod}"
-                        if (pod != OLD_POD && it.objects()[0].status.phase == 'Running' && it.objects()[0].status.containerStatuses[0].ready) {
-                            echo "New pod: ${pod}"
-                            sleep 80
-                            echo "waited ${count*80} seconds"
-                            try {
-                                echo "${pod}"
-                                sequences = ['noncorp_event_seq', 'noncorp_address_seq', 'noncorp_party_seq']
-                                for (seq in sequences) {
-                                    def inc_seq_50 = openshift.exec(
-                                        pod,
-                                        '--',
-                                        "bash -c ' \
-                                        echo \"alter sequence C##CDEV.${seq} increment by 50;\"|\"\$ORACLE_HOME/bin/sqlplus\" / as sysdba \
-                                        '"
-                                    ).actions[0].out
-                                    echo inc_seq_50
-                                    def set_seq_val = openshift.exec(
-                                        pod,
-                                        '--',
-                                        "bash -c ' \
-                                        echo \"select C##CDEV.${seq}.NEXTVAL from dual;\"|\"\$ORACLE_HOME/bin/sqlplus\" / as sysdba \
-                                        '"
-                                    ).actions[0].out
-                                    echo set_seq_val
-                                    def reset_seq_inc = openshift.exec(
-                                        pod,
-                                        '--',
-                                        "bash -c ' \
-                                        echo \"alter sequence C##CDEV.${seq} increment by 1;\"|\"\$ORACLE_HOME/bin/sqlplus\" / as sysdba \
-                                        '"
-                                    ).actions[0].out
-                                    echo reset_seq_inc
-                                }
-                                return true
-                            } catch (Exception e) {
-                                echo "${e}"
-                                count++
-                                return false
-                            }
-                        } else {
-                            return false;
-                        }
-                    }
+        //             deploy.rollout().latest()
+        //             def count = 1
+        //             podSelector.untilEach {
+        //                 def pod = it.objects()[0].metadata.name
+        //                 echo "pod: ${pod}"
+        //                 if (pod != OLD_POD && it.objects()[0].status.phase == 'Running' && it.objects()[0].status.containerStatuses[0].ready) {
+        //                     echo "New pod: ${pod}"
+        //                     sleep 80
+        //                     echo "waited ${count*80} seconds"
+        //                     try {
+        //                         echo "${pod}"
+        //                         sequences = ['noncorp_event_seq', 'noncorp_address_seq', 'noncorp_party_seq']
+        //                         for (seq in sequences) {
+        //                             sql = 'alter sequence C##CDEV.${seq} increment by 50;'
+        //                             execute_pod_command(pod, run_sql)
 
-                }
-            }
-        }
+        //                             sql = 'select C##CDEV.${seq}.NEXTVAL from dual;'
+        //                             execute_pod_command(pod, run_sql)
+
+        //                             sql = 'alter sequence C##CDEV.${seq} increment by 1;'
+        //                             execute_pod_command(pod, run_sql)
+        //                         }
+        //                         return true
+        //                     } catch (Exception e) {
+        //                         echo "${e}"
+        //                         count++
+        //                         return false
+        //                     }
+        //                 } else {
+        //                     return false;
+        //                 }
+        //             }
+
+        //         }
+        //     }
+        // }
 
     } // end stage
 } // end node
