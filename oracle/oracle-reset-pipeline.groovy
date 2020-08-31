32ef7736c74c8627eb9e043e27361fef3ca7b1a7
Kial Jinnah <kialj876@gmail.com>
2020-08-31 11:21:07 -0700

    test 10
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- oracle/oracle-reset-pipeline.groovy
+++ oracle/oracle-reset-pipeline.groovy
@@ -59,7 +59,7 @@ node {
                     OLD_POD = podSelector.objects()[0].metadata.name
                     echo "OLD_POD: ${OLD_POD}"
 
-                    sql = 'select id_num from system_id where id_typ_cd in (\\\"BC\\\");'
+                    sql = "select id_num from C##CDEV.system_id where id_typ_cd in ('BC');"
                     execute_pod_command(OLD_POD, sql, true)
 
                     // sql = 'shutdown abort;'
