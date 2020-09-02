842fdf2f8f8b6f2bec8635d3703f75d672e6cbda
Lekshmi Mallika <lekshmi.mallika@aot-technologies.com>
2020-09-01 17:56:26 -0700

    no message

--- legal-api/src/legal_api/models/party_role.py
+++ legal-api/src/legal_api/models/party_role.py
@@ -47,7 +47,7 @@ class PartyRole(db.Model):
     party_id = db.Column('party_id', db.Integer, db.ForeignKey('parties.id'))
 
     # relationships
-    party = db.relationship('Party', cascade='delete-orphan')
+    party = db.relationship('Party')
 
     def save(self):
         """Save the object to the database immediately."""
