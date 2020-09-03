e533100e384343e511eb4b02a54dec3f7578c7e1
kialj876 <kialj876@gmail.com>
2020-09-03 10:29:38 -0700

    updated versions for 2.16 release (#987)
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- queue_services/entity-emailer/src/entity_emailer/version.py
+++ queue_services/entity-emailer/src/entity_emailer/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.15.1'  # pylint: disable=invalid-name
+__version__ = '2.16.0'  # pylint: disable=invalid-name
