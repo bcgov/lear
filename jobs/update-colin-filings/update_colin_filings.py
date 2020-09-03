9cb3cf620d821b56b10d0df3907fc4fb86506695
Kial Jinnah <kialj876@gmail.com>
2020-09-03 08:39:36 -0700

    fixed incorp sync to colin issue
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- jobs/update-colin-filings/update_colin_filings.py
+++ jobs/update-colin-filings/update_colin_filings.py
@@ -72,7 +72,9 @@ def get_filings(app: Flask = None):
 
 def send_filing(app: Flask = None, filing: dict = None, filing_id: str = None):
     """Post to colin-api with filing."""
+    print(filing['filing']['incorporationApplication']['nameTranslations'])
     clean_none(filing)
+    print(filing['filing']['incorporationApplication']['nameTranslations'])
 
     filing_type = filing['filing']['header'].get('name', None)
     identifier = filing['filing']['business'].get('identifier', None)
@@ -112,7 +114,7 @@ def clean_none(dictionary: dict = None):
         if dictionary[key]:
             if isinstance(dictionary[key], dict):
                 clean_none(dictionary[key])
-        else:
+        elif dictionary[key] is None:
             dictionary[key] = ''
 
 
