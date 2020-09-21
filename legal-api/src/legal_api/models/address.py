8c2e0c8c3ecb56f2e5ae790ef293481660c5f609
Lekshmi Mallika <lekshmi.mallika@aot-technologies.com>
2020-09-21 16:01:07 -0700

    Adding ondelete option for office_id foreign key

--- legal-api/src/legal_api/models/address.py
+++ legal-api/src/legal_api/models/address.py
@@ -52,7 +52,7 @@ class Address(db.Model):  # pylint: disable=too-many-instance-attributes
 
     # parent keys
     business_id = db.Column('business_id', db.Integer, db.ForeignKey('businesses.id'), index=True)
-    office_id = db.Column('office_id', db.Integer, db.ForeignKey('offices.id'), nullable=True)
+    office_id = db.Column('office_id', db.Integer, db.ForeignKey('offices.id', ondelete='CASCADE'), nullable=True)
     # Relationships - Users
     # business_mailing_address = db.relationship('Business',
     #                                            backref=backref('business_mailing_address', uselist=False),
