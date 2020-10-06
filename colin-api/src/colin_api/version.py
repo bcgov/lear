351f838c027d4b0286b9023b08133db998f040f5
Kial Jinnah <kialj876@gmail.com>
2020-10-06 10:25:00 -0700

    updated versions for release
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- colin-api/src/colin_api/version.py
+++ colin-api/src/colin_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.16.0'  # pylint: disable=invalid-name
+__version__ = '2.17.0'  # pylint: disable=invalid-name
