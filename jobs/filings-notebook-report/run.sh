95374754adce6b6501b8dc579facf5f012804b25
Chen <Steven.Chen@gov.bc.ca>
2020-11-27 15:45:20 -0800

    1068 - Add github Workflow for Filings Notebook Report
    Signed-off-by: Chen <Steven.Chen@gov.bc.ca>

--- jobs/filings-notebook-report/run.sh
+++ jobs/filings-notebook-report/run.sh
@@ -1,9 +1,3 @@
-#! /bin/sh
-export LIBRARY_PATH=/opt/rh/httpd24/root/usr/lib64
-export X_SCLS=rh-python35 httpd24
-export LD_LIBRARY_PATH=/opt/rh/rh-python35/root/usr/lib64::/opt/rh/httpd24/root/usr/lib64
-export PATH=/opt/app-root/bin:/opt/rh/rh-python35/root/usr/bin::/opt/rh/httpd24/root/usr/bin:/opt/rh/httpd24/root/usr/sbin:/opt/app-root/src/.local/bin/:/opt/app-root/src/bin:/opt/app-root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-cd /opt/app-root/src
-echo 'run notebookreport to generate Jupyter Notebook Report for filings'
-/opt/app-root/bin/python notebookreport.py
+cd /opt/app-root
+echo 'run notebookreport'
+python file_future_effective.py
