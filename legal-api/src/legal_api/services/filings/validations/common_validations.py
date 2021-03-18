69b889b66935ef3dd09367021b71f5d6f1099926
Andre Pestana <andre.pestana@aot-technologies.com>
2021-03-17 19:01:04 -0700

    Code smell issue

--- legal-api/src/legal_api/services/filings/validations/common_validations.py
+++ legal-api/src/legal_api/services/filings/validations/common_validations.py
@@ -93,33 +93,33 @@ def validate_shares(item, memoize_names, filing_type, index) -> Error:
     return msg
 
 
-def validate_court_order(court_order_path, court_order_snippet) -> Error:
+def validate_court_order(court_order_path, court_order):
     """Validate the courtOrder data of the filing."""
     msg = []
 
     # TODO remove it when the issue with schema validation is fixed
-    if 'fileNumber' not in court_order_snippet:
+    if 'fileNumber' not in court_order:
         err_path = court_order_path + '/fileNumber'
         msg.append({'error': 'Court order file number is required.', 'path': err_path})
     else:
-        if len(court_order_snippet['fileNumber']) < 5 or len(court_order_snippet['fileNumber']) > 20:
+        if len(court_order['fileNumber']) < 5 or len(court_order['fileNumber']) > 20:
             err_path = court_order_path + '/fileNumber'
             msg.append({'error': 'Length of court order file number must be from 5 to 20 characters.',
                         'path': err_path})
 
-    if 'effectOfOrder' not in court_order_snippet and (len(court_order_snippet['effectOfOrder']) < 5 or
-                                                       len(court_order_snippet['effectOfOrder']) > 500):
+    if 'effectOfOrder' not in court_order and (len(court_order['effectOfOrder']) < 5 or
+                                               len(court_order['effectOfOrder']) > 500):
         err_path = court_order_path + '/effectOfOrder'
         msg.append({'error': 'Length of court order effect of order must be from 5 to 500 characters.',
                     'path': err_path})
 
     court_order_date_path = court_order_path + '/orderDate'
-    if 'orderDate'not in court_order_snippet:
+    if 'orderDate'not in court_order:
         err_path = court_order_date_path
         msg.append({'error': 'Court order date is required.', 'path': err_path})
     else:
         try:
-            court_order_date = dt.fromisoformat(court_order_snippet['orderDate'])
+            court_order_date = dt.fromisoformat(court_order['orderDate'])
             if court_order_date.timestamp() > datetime.utcnow().timestamp():
                 err_path = court_order_date_path
                 msg.append({'error': 'Court order date cannot be in the future.', 'path': err_path})
