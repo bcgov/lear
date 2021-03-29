1e12136b860e81c99a85dee4d7e1ab851c1f875a
CameronFreshworks <53542131+cameron-freshworks@users.noreply.github.com>
2021-03-29 11:02:48 -0700

    NR name change bug and validator update (#1129)
    * Bug fix and validator update
    * Updated pytest with new scenario

--- legal-api/src/legal_api/services/filings/validations/alteration.py
+++ legal-api/src/legal_api/services/filings/validations/alteration.py
@@ -71,7 +71,7 @@ def company_name_validation(filing):
         nr_response = namex.query_nr_number(nr_number)
         validation_result = namex.validate_nr(nr_response)
 
-        if not nr_response['requestTypeCd'] in ('CCP', 'BEC'):
+        if not nr_response['requestTypeCd'] in ('CCR', 'CCP', 'BEC'):
             msg.append({'error': babel('Alteration only available for Change of Name Name requests.'), 'path': nr_path})
 
         if not validation_result['is_approved']:
