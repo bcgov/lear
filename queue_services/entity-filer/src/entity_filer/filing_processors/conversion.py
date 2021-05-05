2ca7f6acd9c0f22d038be1392a49749c7f937777
thor <thor@wolpert.ca>
2021-05-05 00:14:10 -0700

    Update conversion.py

--- queue_services/entity-filer/src/entity_filer/filing_processors/conversion.py
+++ queue_services/entity-filer/src/entity_filer/filing_processors/conversion.py
@@ -11,118 +11,49 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""File processing rules and actions for the incorporation of a business."""
+"""File processing rules and actions for historic conversion filing.
+
+A conversion filing is for a business that was created before COLIN,
+the original system to manage business corporations.
+
+As the business exists, no new registration identifiers, names, or structures are
+altered.
+
+There are no corrections for a conversion filing.
+"""
 import copy
 from contextlib import suppress
 from http import HTTPStatus
 from typing import Dict
 
-import requests
 import sentry_sdk
 from entity_queue_common.service_utils import QueueException
 from flask import current_app
-from legal_api.models import Business, Filing, RegistrationBootstrap
-from legal_api.services.bootstrap import AccountService
+from legal_api.models import Business, Filing
 
 from entity_filer.filing_processors.filing_components import aliases, business_info, business_profile, shares
 from entity_filer.filing_processors.filing_components.offices import update_offices
 from entity_filer.filing_processors.filing_components.parties import update_parties
 
 
-def get_next_corp_num(legal_type: str):
-    """Retrieve the next available sequential corp-num from COLIN."""
-    try:
-        # TODO: update this to grab the legal 'class' after legal classes have been defined in lear
-        if legal_type == Business.LegalTypes.BCOMP.value:
-            business_type = 'BC'
-        else:
-            business_type = legal_type
-        resp = requests.post(f'{current_app.config["COLIN_API"]}/{business_type}')
-    except requests.exceptions.ConnectionError:
-        current_app.logger.error(f'Failed to connect to {current_app.config["COLIN_API"]}')
-        return None
-
-    if resp.status_code == 200:
-        new_corpnum = int(resp.json()['corpNum'])
-        if new_corpnum and new_corpnum <= 9999999:
-            # TODO: Fix endpoint
-            return f'{business_type}{new_corpnum:07d}'
-    return None
-
-
-def update_affiliation(business: Business, filing: Filing):
-    """Create an affiliation for the business and remove the bootstrap."""
-    try:
-        bootstrap = RegistrationBootstrap.find_by_identifier(filing.temp_reg)
-
-        rv = AccountService.create_affiliation(
-            account=bootstrap.account,
-            business_registration=business.identifier,
-            business_name=business.legal_name,
-            corp_type_code=business.legal_type
-        )
-
-        if rv not in (HTTPStatus.OK, HTTPStatus.CREATED):
-            deaffiliation = AccountService.delete_affiliation(bootstrap.account, business.identifier)
-            sentry_sdk.capture_message(
-                f'Queue Error: Unable to affiliate business:{business.identifier} for filing:{filing.id}',
-                level='error'
-            )
-        else:
-            # flip the registration
-            # recreate the bootstrap, but point to the new business in the name
-            old_bs_affiliation = AccountService.delete_affiliation(bootstrap.account, bootstrap.identifier)
-            new_bs_affiliation = AccountService.create_affiliation(
-                account=bootstrap.account,
-                business_registration=bootstrap.identifier,
-                business_name=business.identifier,
-                corp_type_code='TMP'
-            )
-            reaffiliate = bool(new_bs_affiliation in (HTTPStatus.OK, HTTPStatus.CREATED)
-                               and old_bs_affiliation == HTTPStatus.OK)
-
-        if rv not in (HTTPStatus.OK, HTTPStatus.CREATED) \
-                or ('deaffiliation' in locals() and deaffiliation != HTTPStatus.OK)\
-                or ('reaffiliate' in locals() and not reaffiliate):
-            raise QueueException
-    except Exception as err:  # pylint: disable=broad-except; note out any exception, but don't fail the call
-        sentry_sdk.capture_message(
-            f'Queue Error: Affiliation error for filing:{filing.id}, with err:{err}',
-            level='error'
-        )
-
-
 def process(business: Business, filing: Dict, filing_rec: Filing):  # pylint: disable=too-many-branches
-    """Process the incoming incorporation filing."""
+    """Process the incoming historic conversion filing."""
     # Extract the filing information for incorporation
-    conversion_filing = filing.get('filing', {}).get('conversion')
-    is_correction = filing_rec.filing_type == 'correction'
-    if not conversion_filing:
+    if not (conversion_filing:= filing.get('filing', {}).get('conversion')):
         raise QueueException(f'CONVL legal_filing:conversion missing from {filing_rec.id}')
-    if business and not is_correction:
+
+    if business: 
         raise QueueException(f'Business Already Exist: CONVL legal_filing:conversion {filing_rec.id}')
 
+    if not (corp_num := filing.get('filing',{}).get('business',{}).get('identifier')):
+        raise QueueException(f'conversion {filing_rec.id} missing the business idnetifier.')
+
+    # Initial insert of the business record
     business_info_obj = conversion_filing.get('nameRequest')
+    if not (business := business_info.update_business_info(corp_num, Business(), business_info_obj, filing_rec)):
+        raise QueueException(f'CONVL conversion {filing_rec.id}, Unable to create business.')
 
-    if is_correction:
-        business_info.set_legal_name(business.identifier, business, business_info_obj)
-    else:
-        if filing_rec.colin_event_ids:
-            corp_num = filing['filing']['business']['identifier']
-        else:
-            # Reserve the Corp Number for this entity
-            corp_num = get_next_corp_num(business_info_obj['legalType'])
-            if not corp_num:
-                raise QueueException(
-                    f'conversion {filing_rec.id} unable to get a business registration number.')
-
-        # Initial insert of the business record
-        business = Business()
-        business = business_info.update_business_info(corp_num, business, business_info_obj, filing_rec)
-        if not business:
-            raise QueueException(f'CONVL conversion {filing_rec.id}, Unable to create business.')
-
-    if offices := conversion_filing['offices']:
+    if offices := conversion_filing.get('offices'):
         update_offices(business, offices)
 
     if parties := conversion_filing.get('parties'):
@@ -133,13 +64,6 @@ def process(business: Business, filing: Dict, filing_rec: Filing):  # pylint: di
 
     if name_translations := conversion_filing.get('nameTranslations'):
         aliases.update_aliases(business, name_translations)
-
-    if not is_correction and not filing_rec.colin_event_ids:
-        # Update the filing json with identifier and founding date.
-        ia_json = copy.deepcopy(filing_rec.filing_json)
-        ia_json['filing']['business']['identifier'] = business.identifier
-        ia_json['filing']['business']['foundingDate'] = business.founding_date.isoformat()
-        filing_rec._filing_json = ia_json  # pylint: disable=protected-access; bypass to update filing data
 
     return business, filing_rec
 
