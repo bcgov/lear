8dbc7e0bdf112faead41867d83d8f1cde4678935
argush3 <argus@highwaythreesolutions.com>
2021-06-21 09:23:30 -0700

    7882 Add infra related templates and config to support new tracker db used by entity emailer to track message processing status

---
+++ tracker-db/tracker_db.sql
@@ -0,0 +1,188 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.11 (Debian 11.11-1.pgdg90+1)
+-- Dumped by pg_dump version 13.2
+
+-- Started on 2021-06-21 08:39:12 PDT
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+CREATE USER "userA7C" with SUPERUSER;
+CREATE DATABASE tracker_test OWNER "userA7C";
+\c tracker_test
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+-- SELECT pg_catalog.set_config('search_path', '', false);
+SET search_path=legal_test;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 196 (class 1259 OID 283254)
+-- Name: alembic_version; Type: TABLE; Schema: public; Owner: userA7C
+--
+
+CREATE TABLE public.alembic_version (
+    version_num character varying(32) NOT NULL
+);
+
+
+ALTER TABLE public.alembic_version OWNER TO "userA7C";
+
+--
+-- TOC entry 198 (class 1259 OID 283261)
+-- Name: message_processing; Type: TABLE; Schema: public; Owner: userA7C
+--
+
+CREATE TABLE public.message_processing (
+                                           id integer NOT NULL,
+                                           message_id character varying(36) NOT NULL,
+                                           identifier character varying(36),
+                                           message_type character varying(35) NOT NULL,
+                                           status character varying(10) NOT NULL,
+                                           message_json jsonb NOT NULL,
+                                           create_date timestamp with time zone NOT NULL,
+                                           last_update timestamp with time zone NOT NULL
+);
+
+
+ALTER TABLE public.message_processing OWNER TO "userA7C";
+
+--
+-- TOC entry 197 (class 1259 OID 283259)
+-- Name: message_processing_id_seq; Type: SEQUENCE; Schema: public; Owner: userA7C
+--
+
+CREATE SEQUENCE public.message_processing_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.message_processing_id_seq OWNER TO "userA7C";
+
+--
+-- TOC entry 2883 (class 0 OID 0)
+-- Dependencies: 197
+-- Name: message_processing_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: userA7C
+--
+
+ALTER SEQUENCE public.message_processing_id_seq OWNED BY public.message_processing.id;
+
+
+--
+-- TOC entry 2745 (class 2604 OID 283264)
+-- Name: message_processing id; Type: DEFAULT; Schema: public; Owner: userA7C
+--
+
+ALTER TABLE ONLY public.message_processing ALTER COLUMN id SET DEFAULT nextval('public.message_processing_id_seq'::regclass);
+
+
+--
+-- TOC entry 2875 (class 0 OID 283254)
+-- Dependencies: 196
+-- Data for Name: alembic_version; Type: TABLE DATA; Schema: public; Owner: userA7C
+--
+
+COPY public.alembic_version (version_num) FROM stdin;
+61eb4b7c2c1d
+\.
+
+
+--
+-- TOC entry 2877 (class 0 OID 283261)
+-- Dependencies: 198
+-- Data for Name: message_processing; Type: TABLE DATA; Schema: public; Owner: userA7C
+--
+
+COPY public.message_processing (id, message_id, identifier, message_type, status, message_json, create_date, last_update) FROM stdin;
+\.
+
+
+--
+-- TOC entry 2884 (class 0 OID 0)
+-- Dependencies: 197
+-- Name: message_processing_id_seq; Type: SEQUENCE SET; Schema: public; Owner: userA7C
+--
+
+SELECT pg_catalog.setval('public.message_processing_id_seq', 1, false);
+
+
+--
+-- TOC entry 2747 (class 2606 OID 283258)
+-- Name: alembic_version alembic_version_pkc; Type: CONSTRAINT; Schema: public; Owner: userA7C
+--
+
+ALTER TABLE ONLY public.alembic_version
+    ADD CONSTRAINT alembic_version_pkc PRIMARY KEY (version_num);
+
+
+--
+-- TOC entry 2753 (class 2606 OID 283269)
+-- Name: message_processing message_processing_pkey; Type: CONSTRAINT; Schema: public; Owner: userA7C
+--
+
+ALTER TABLE ONLY public.message_processing
+    ADD CONSTRAINT message_processing_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2748 (class 1259 OID 283270)
+-- Name: ix_message_processing_identifier; Type: INDEX; Schema: public; Owner: userA7C
+--
+
+CREATE INDEX ix_message_processing_identifier ON public.message_processing USING btree (identifier);
+
+
+--
+-- TOC entry 2749 (class 1259 OID 283271)
+-- Name: ix_message_processing_message_id; Type: INDEX; Schema: public; Owner: userA7C
+--
+
+CREATE UNIQUE INDEX ix_message_processing_message_id ON public.message_processing USING btree (message_id);
+
+
+--
+-- TOC entry 2750 (class 1259 OID 283272)
+-- Name: ix_message_processing_message_type; Type: INDEX; Schema: public; Owner: userA7C
+--
+
+CREATE INDEX ix_message_processing_message_type ON public.message_processing USING btree (message_type);
+
+
+--
+-- TOC entry 2751 (class 1259 OID 283273)
+-- Name: ix_message_processing_status; Type: INDEX; Schema: public; Owner: userA7C
+--
+
+CREATE INDEX ix_message_processing_status ON public.message_processing USING btree (status);
+
+
+-- Completed on 2021-06-21 08:39:15 PDT
+
+--
+-- PostgreSQL database dump complete
+--
+
