7460d243c2c47dfef247a4170cdb6e9d3fe84365
Andre Pestana <andre.pestana@aot-technologies.com>
2021-06-25 18:18:37 -0700

    lint issues

--- legal-api/src/legal_api/services/filings/validations/annual_report.py
+++ legal-api/src/legal_api/services/filings/validations/annual_report.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Validation for the Annual Report filing."""
+from datetime import timedelta
 from http import HTTPStatus
 from typing import Dict, List, Tuple
-from datetime import timedelta
 
 from flask_babel import _
 
