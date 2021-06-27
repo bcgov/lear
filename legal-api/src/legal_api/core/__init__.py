587ca79af05b06b7ec31aa4bb0bb956031676fd1
Andre Pestana <andre.pestana@aot-technologies.com>
2021-06-27 04:55:26 -0700

    Ignored some lint errors not related to this issue

--- legal-api/src/legal_api/core/__init__.py
+++ legal-api/src/legal_api/core/__init__.py
@@ -20,4 +20,4 @@ from typing import Any, Callable, Dict, List, MutableMapping, MutableSequence, O
 from .filing import Filing
 
 
-__all__ = ('Filing')
+__all__ = ('Filing') # pylint: disable=E0605
--- legal-api/src/legal_api/services/comments/__init__.py
+++ legal-api/src/legal_api/services/comments/__init__.py
@@ -15,4 +15,4 @@
 from .validations.validation import validate
 
 
-__all__ = ('validate')
+__all__ = ('validate') # pylint: disable=E0605
