7e6de5c6f637f9115dafc3be3df60f41c4c1c73b
argush3 <argus@highwaythreesolutions.com>
2021-06-28 08:04:21 -0700

    7882 Add description to app.py

--- queue_services/entity-emailer/tracker/app.py
+++ queue_services/entity-emailer/tracker/app.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-"""
+"""This module is the used specifically for tracker db migration/upgrade purposes."""
 
 import os
 
