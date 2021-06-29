8b4897679b9333a2608c106e10930ca8a6227554
Argus Chiu <argus@highwaythreesolutions.com>
2021-06-29 06:28:15 -0700

    7788 version number updates (#1217)

--- legal-api/src/legal_api/version.py
+++ legal-api/src/legal_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.23.0'  # pylint: disable=invalid-name
+__version__ = '2.24.0'  # pylint: disable=invalid-name
