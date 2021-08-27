636e3defc31fc43c2cd780199558e5feafcd28b7
Kial Jinnah <kialj876@gmail.com>
2021-08-27 11:23:29 -0700

    nr receipt notification fix
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- queue_services/entity-emailer/tests/unit/test_worker.py
+++ queue_services/entity-emailer/tests/unit/test_worker.py
@@ -20,7 +20,7 @@ from legal_api.services import NameXService
 from legal_api.services.bootstrap import AccountService
 
 from entity_emailer import worker
-from entity_emailer.email_processors import ar_reminder_notification, filing_notification, nr_notification
+from entity_emailer.email_processors import ar_reminder_notification, filing_notification, name_request, nr_notification
 from tests import MockResponse
 from tests.unit import prep_incorp_filing, prep_maintenance_filing
 
@@ -248,3 +248,47 @@ def test_nr_notification(app, session, option, nr_number, subject, expiration_da
                 assert call_args[0][0]['content']['attachments'] == []
                 assert mock_query_nr_number.call_args[0][0] == nr_number
                 assert call_args[0][1] == token
+
+
+def test_nr_receipt_notification(app, session):
+    """Assert that the nr payment notification can be processed."""
+    nr_number = 'NR 1234567'
+    email_address = 'test@test.com'
+    nr_json = {
+        'applicants': {
+            'emailAddress': email_address
+        }
+    }
+    nr_response = MockResponse(nr_json, 200)
+    token = 'token'
+    payment_token = '1234'
+
+    # run worker
+    with patch.object(name_request, 'get_nr_bearer_token', return_value=token):
+        with patch.object(name_request, '_get_pdf', return_value=[]) as mock_pdf:
+            with patch.object(NameXService, 'query_nr_number', return_value=nr_response) \
+                    as mock_query_nr_number:
+                with patch.object(worker, 'send_email', return_value='success') as mock_send_email:
+                    worker.process_email({
+                        'id': '123456789',
+                        'type': 'bc.registry.names.request',
+                        'source': f'/requests/{nr_number}',
+                        'identifier': nr_number,
+                        'data': {
+                            'request': {
+                                'header': {'nrNum': nr_number},
+                                'paymentToken': payment_token,
+                                'statusCode': 'DRAFT'  # not used
+                            }
+                        }
+                    }, app)
+
+                    assert mock_pdf.call_args[0][0]['nr_number'] == nr_number
+                    assert mock_pdf.call_args[0][0]['payment_token'] == payment_token
+                    assert mock_query_nr_number.call_args[0][0] == nr_number
+                    call_args = mock_send_email.call_args
+                    assert call_args[0][0]['content']['subject'] == f'{nr_number} - Receipt from Corporate Registry'
+                    assert call_args[0][0]['recipients'] == 'test@test.com'
+                    assert call_args[0][0]['content']['body']
+                    assert call_args[0][0]['content']['attachments'] == []
+                    assert call_args[0][1] == token
