84259d7b0035f36aa0662edf70a5213827c8806e
Lekshmi Mallika <lekshmi.mallika@aot-technologies.com>
2021-12-13 15:02:51 -0800

    Lint fixes

--- queue_services/entity-filer/src/entity_filer/filing_processors/dissolution.py
+++ queue_services/entity-filer/src/entity_filer/filing_processors/dissolution.py
@@ -21,7 +21,6 @@ from entity_queue_common.service_utils import QueueException, logger
 from legal_api.models import Business, Document, Filing
 from legal_api.models.document import DocumentType
 from legal_api.services.minio import MinioService
-from legal_api.utils.datetime import datetime
 
 from entity_filer.filing_meta import FilingMeta
 from entity_filer.filing_processors.filing_components import create_office, filings
