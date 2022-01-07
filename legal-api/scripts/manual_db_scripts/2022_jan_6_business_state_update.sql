78830888168665e26795aa5b454020d4c303e35f
Argus Chiu <argus@highwaythreesolutions.com>
2022-01-07 07:30:52 -0800

    10596 tweak manual business state update sql (#1393)

--- legal-api/scripts/manual_db_scripts/2022_jan_6_business_state_update.sql
+++ legal-api/scripts/manual_db_scripts/2022_jan_6_business_state_update.sql
@@ -1,3 +1,4 @@
+-- Description:
 -- Update for businesses where businesses.state is null to valid STATE for following scenarios:
 --    1. businesses.dissolution_date is not null and dissolution filing json has dissolutionType of
 --      'voluntary' or 'administrative'. Update businesses.state to 'HISTORICAL'
@@ -7,20 +8,10 @@
 --       Update businesses.state to 'HISTORICAL'
 --    4. businesses.dissolution_date is null.  Update businesses.state to `ACTIVE'
 
--- scenario 1 (valid dissolution filing of sub type 'voluntary' or 'administrative')
-update businesses
-set state = 'HISTORICAL'
-where id in (
-    select b.id
-    from filings f
-             join businesses b ON f.business_id = b.id
-    where f.filing_type = 'dissolution'
-      and b.dissolution_date is not null
-      and b.state is null
-      and f.filing_json -> 'filing' -> 'dissolution' ->> 'dissolutionType' in ('voluntary', 'administrative')
-    );
+-- start a transaction
+begin;
 
--- scenario 2 (dissolution filing json is incomplete but business has dissolution date)
+-- scenario 1 & 2
 update businesses
 set state = 'HISTORICAL'
 where id in (
@@ -30,10 +21,21 @@ where id in (
     where f.filing_type = 'dissolution'
       and b.dissolution_date is not null
       and b.state is null
-      and f.filing_json -> 'filing' -> 'dissolution' is null
+      and (f.filing_json -> 'filing' -> 'dissolution' ->> 'dissolutionType' in ('voluntary', 'administrative')
+        or f.filing_json -> 'filing' -> 'dissolution' is null)
 );
 
--- scenario 3 (obsolete voluntaryDissolution filing type that was previously used)
+-- verify scenario 1 & 2
+select b.id, b.identifier, b.legal_name, b.state, b.dissolution_date
+from filings f
+         join businesses b ON f.business_id = b.id
+where f.filing_type = 'dissolution'
+  and b.dissolution_date is not null
+  and b.state is not null
+  and (f.filing_json -> 'filing' -> 'dissolution' ->> 'dissolutionType' in ('voluntary', 'administrative')
+    or f.filing_json -> 'filing' -> 'dissolution' is null);
+
+-- scenario 3
 update businesses
 set state = 'HISTORICAL'
 where id in (
@@ -44,7 +46,15 @@ where id in (
       and b.dissolution_date is not null
       and b.state is null);
 
--- scenario 4 (business records that have not been dissolved)
+-- verify scenario 3
+select b.id, b.identifier, b.legal_name, b.state, b.dissolution_date
+from filings f
+         join businesses b ON f.business_id = b.id
+where f.filing_type = 'voluntaryDissolution'
+  and b.dissolution_date is not null
+  and b.state is not null;
+
+-- scenario 4
 update businesses
 set state = 'ACTIVE'
 where id in (
@@ -52,3 +62,17 @@ where id in (
     from businesses b
     where b.state is null
       and b.dissolution_date is null);
+
+-- verify scenario 4
+select b.id, b.identifier, b.legal_name, b.state, b.dissolution_date
+from businesses b
+where b.state is not null
+  and b.dissolution_date is null;
+
+-- COMMIT OR ROLLBACK above updates after verification.  i.e. uncomment one of COMMIT or ROLLBACK commands below.
+
+-- uncomment COMMIT; command on next line execute if commit transaction if all verification checks out
+-- commit;
+
+-- uncomment ROLLBACK; command on next line and execute to rollback transaction if verification doesn't checkout
+-- rollback;
