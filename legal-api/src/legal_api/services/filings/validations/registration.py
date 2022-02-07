807da60960e0c7718b8430e8eb5bc8279b1c07bc
Vysakh Menon <vysakh.menon@aot-technologies.com>
2022-02-07 09:44:02 -0800

    9250 registration validation (#1418)

---
+++ legal-api/src/legal_api/services/filings/validations/registration.py
@@ -0,0 +1,160 @@
+# Copyright © 2022 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Validation for the Registration filing."""
+from datetime import timedelta
+from http import HTTPStatus  # pylint: disable=wrong-import-order
+from typing import Dict, Final, Optional
+
+import pycountry
+from dateutil.relativedelta import relativedelta
+from flask_babel import _ as babel  # noqa: N813, I004, I001, I003
+from legal_api.errors import Error
+from legal_api.models import Business, PartyRole
+from legal_api.services import namex
+from legal_api.utils.legislation_datetime import LegislationDatetime
+
+from ...utils import get_date, get_str
+from .common_validations import validate_court_order
+
+
+def validate(registration_json: Dict) -> Optional[Error]:
+    """Validate the Registration filing."""
+    if not registration_json:
+        return Error(HTTPStatus.BAD_REQUEST, [{'error': babel('A valid filing is required.')}])
+
+    legal_type_path = '/filing/business/legalType'
+    legal_type = get_str(registration_json, legal_type_path)
+    if legal_type not in [Business.LegalTypes.SOLE_PROP.value, Business.LegalTypes.PARTNERSHIP.value]:
+        return Error(
+            HTTPStatus.BAD_REQUEST,
+            [{'error': babel('A valid legalType for registration is required.'), 'path': legal_type_path}]
+        )
+
+    msg = []
+    msg.extend(validate_name_request(registration_json, legal_type))
+    msg.extend(validate_business_type(registration_json, legal_type))
+    msg.extend(validate_party(registration_json, legal_type))
+    msg.extend(validate_start_date(registration_json))
+    msg.extend(validate_delivery_address(registration_json))
+    msg.extend(_validate_court_order(registration_json))
+
+    if msg:
+        return Error(HTTPStatus.BAD_REQUEST, msg)
+    return None
+
+
+def validate_name_request(filing: Dict, legal_type: str) -> list:
+    """Validate name request."""
+    nr_path = '/filing/registration/nameRequest/nrNumber'
+    nr_number = get_str(filing, nr_path)
+    msg = []
+
+    # ensure NR is approved or conditionally approved
+    nr_response = namex.query_nr_number(nr_number)
+    validation_result = namex.validate_nr(nr_response.json())
+    if not validation_result['is_consumable']:
+        msg.append({'error': babel('Name Request is not approved.'), 'path': nr_path})
+
+    path = '/filing/registration/nameRequest/legalType'
+    nr_legal_type = get_str(filing, path)
+    if nr_legal_type != legal_type:
+        msg.append({'error': babel('Name Request has invalid legalType.'), 'path': path})
+
+    return msg
+
+
+def validate_business_type(filing: Dict, legal_type: str) -> list:
+    """Validate business type."""
+    msg = []
+    business_type_path = '/filing/registration/businessType'
+    if legal_type == Business.LegalTypes.SOLE_PROP.value and get_str(filing, business_type_path) is None:
+        msg.append({'error': 'Business Type is required.', 'path': business_type_path})
+
+    return msg
+
+
+def validate_party(filing: Dict, legal_type: str) -> list:
+    """Validate party."""
+    msg = []
+    completing_parties = 0
+    proprietor_parties = 0
+    partner_parties = 0
+    parties = filing['filing']['registration']['parties']
+    for party in parties:  # pylint: disable=too-many-nested-blocks;  # noqa: E501
+        for role in party.get('roles', []):
+            role_type = role.get('roleType').lower().replace(' ', '_')
+            if role_type == PartyRole.RoleTypes.COMPLETING_PARTY.value:
+                completing_parties += 1
+            elif role_type == PartyRole.RoleTypes.PROPRIETOR.value:
+                proprietor_parties += 1
+            elif role_type == PartyRole.RoleTypes.PARTNER.value:
+                partner_parties += 1
+
+    party_path = '/filing/registration/parties'
+    if legal_type == Business.LegalTypes.SOLE_PROP.value and (completing_parties < 1 or proprietor_parties < 1):
+        msg.append({'error': '1 Proprietor and a Completing Party is required.', 'path': party_path})
+    elif legal_type == Business.LegalTypes.PARTNERSHIP.value and (completing_parties < 1 or partner_parties < 2):
+        msg.append({'error': '2 Partners and a Completing Party is required.', 'path': party_path})
+
+    return msg
+
+
+def validate_start_date(filing: Dict) -> list:
+    """Validate start date."""
+    # Less than or equal to 2 years in the past, Less than or equal to 90 days in the future
+    msg = []
+    start_date_path = '/filing/registration/startDate'
+    start_date = get_date(filing, start_date_path)
+    now = LegislationDatetime.now().date()
+    greater = now + timedelta(days=90)
+    lesser = now + relativedelta(years=-2)
+    if start_date < lesser or start_date > greater:
+        msg.append({'error': 'Start Date must be less than or equal to 2 years in the past and \
+          less than or equal to 90 days in the future.', 'path': start_date_path})
+
+    return msg
+
+
+def validate_delivery_address(filing: Dict) -> list:
+    """Validate the delivery address of the registration filing."""
+    addresses = filing['filing']['registration']['businessAddress']
+    msg = []
+
+    if delivery_address := addresses.get('deliveryAddress'):
+        region = delivery_address['addressRegion']
+        country = delivery_address['addressCountry']
+
+        if region != 'BC':
+            region_path = '/filing/registration/businessAddress/deliveryAddress/addressRegion'
+            msg.append({'error': "Address Region must be 'BC'.", 'path': region_path})
+
+        try:
+            country = pycountry.countries.search_fuzzy(country)[0].alpha_2
+            if country != 'CA':
+                raise LookupError
+        except LookupError:
+            country_path = '/filing/registration/businessAddress/deliveryAddress/addressCountry'
+            msg.append({'error': "Address Country must be 'CA'.", 'path': country_path})
+
+    return msg
+
+
+def _validate_court_order(filing: Dict) -> list:
+    """Validate court order."""
+    if court_order := filing.get('filing', {}).get('registration', {}).get('courtOrder', None):
+        court_order_path: Final = '/filing/registration/courtOrder'
+        err = validate_court_order(court_order_path, court_order)
+        if err:
+            return err
+    return []
---
+++ legal-api/tests/unit/services/filings/validations/test_registration.py
@@ -0,0 +1,262 @@
+# Copyright © 2022 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Test suite to ensure Registration is validated correctly."""
+import copy
+from datetime import datetime, timedelta
+from unittest.mock import patch
+from dateutil.relativedelta import relativedelta
+from http import HTTPStatus
+
+import pytest
+from registry_schemas.example_data import FILING_HEADER, REGISTRATION
+
+from legal_api.services.filings.validations.registration import validate
+from legal_api.services.namex import NameXService
+from legal_api.utils.legislation_datetime import LegislationDatetime
+
+
+now = datetime.now().strftime('%Y-%m-%d')
+
+GP_REGISTRATION = copy.deepcopy(FILING_HEADER)
+GP_REGISTRATION['filing']['header']['name'] = 'registration'
+GP_REGISTRATION['filing']['business']['legalType'] = 'GP'
+GP_REGISTRATION['filing']['registration'] = copy.deepcopy(REGISTRATION)
+GP_REGISTRATION['filing']['registration']['startDate'] = now
+
+SP_REGISTRATION = copy.deepcopy(FILING_HEADER)
+SP_REGISTRATION['filing']['header']['name'] = 'registration'
+SP_REGISTRATION['filing']['business']['legalType'] = 'SP'
+SP_REGISTRATION['filing']['registration'] = copy.deepcopy(REGISTRATION)
+SP_REGISTRATION['filing']['registration']['startDate'] = now
+SP_REGISTRATION['filing']['registration']['nameRequest']['legalType'] = 'SP'
+SP_REGISTRATION['filing']['registration']['businessType'] = 'SP'
+SP_REGISTRATION['filing']['registration']['parties'][0]['roles'] = [
+    {
+        'roleType': 'Completing Party',
+        'appointmentDate': '2022-01-01'
+
+    },
+    {
+        'roleType': 'Proprietor',
+        'appointmentDate': '2022-01-01'
+
+    }
+]
+del SP_REGISTRATION['filing']['registration']['parties'][1]
+
+
+DBA_REGISTRATION = copy.deepcopy(FILING_HEADER)
+DBA_REGISTRATION['filing']['header']['name'] = 'registration'
+DBA_REGISTRATION['filing']['business']['legalType'] = 'SP'
+DBA_REGISTRATION['filing']['registration'] = copy.deepcopy(REGISTRATION)
+DBA_REGISTRATION['filing']['registration']['startDate'] = now
+DBA_REGISTRATION['filing']['registration']['nameRequest']['legalType'] = 'SP'
+DBA_REGISTRATION['filing']['registration']['businessType'] = 'DBA'
+DBA_REGISTRATION['filing']['registration']['parties'][0]['roles'] = [
+    {
+        'roleType': 'Completing Party',
+        'appointmentDate': '2022-01-01'
+    }
+]
+DBA_REGISTRATION['filing']['registration']['parties'][1] = {
+    'officer': {
+        'id': 2,
+        'organizationName': 'Xyz Inc.',
+        'identifier': 'BC1234567',
+        'taxId': '123456789',
+        'email': 'peter@email.com',
+        'partyType': 'organization'
+    },
+    'mailingAddress': {
+        'streetAddress': 'mailing_address - address line one',
+        'streetAddressAdditional': '',
+        'addressCity': 'mailing_address city',
+        'addressCountry': 'CA',
+        'postalCode': 'H0H0H0',
+        'addressRegion': 'BC'
+    },
+    'roles': [
+        {
+            'roleType': 'Proprietor',
+            'appointmentDate': '2022-01-01'
+        }
+    ]
+}
+
+nr_response = {
+    'state': 'APPROVED',
+    'expirationDate': '',
+    'names': [{
+        'name': 'legal_name',
+        'state': 'APPROVED',
+        'consumptionDate': ''
+    }]
+}
+
+
+class MockResponse:
+    """Mock http response."""
+
+    def __init__(self, json_data):
+        """Initialize mock http response."""
+        self.json_data = json_data
+
+    def json(self):
+        """Return mock json data."""
+        return self.json_data
+
+
+def test_gp_registration(session):
+    """Assert that the general partnership registration is valid."""
+    with patch.object(NameXService, 'query_nr_number', return_value=MockResponse(nr_response)):
+        err = validate(GP_REGISTRATION)
+
+    assert not err
+
+
+def test_sp_registration(session):
+    """Assert that the general partnership registration is valid."""
+    with patch.object(NameXService, 'query_nr_number', return_value=MockResponse(nr_response)):
+        err = validate(SP_REGISTRATION)
+
+    assert not err
+
+
+def test_dba_registration(session):
+    """Assert that the general partnership registration is valid."""
+    with patch.object(NameXService, 'query_nr_number', return_value=MockResponse(nr_response)):
+        err = validate(DBA_REGISTRATION)
+
+    assert not err
+
+
+def test_invalid_nr_registration(session):
+    """Assert that nr is invalid."""
+    filing = copy.deepcopy(SP_REGISTRATION)
+    invalid_nr_response = {
+        'state': 'INPROGRESS',
+        'expirationDate': '',
+        'names': [{
+            'name': 'legal_name',
+            'state': 'INPROGRESS',
+            'consumptionDate': ''
+        }]
+    }
+    with patch.object(NameXService, 'query_nr_number', return_value=MockResponse(invalid_nr_response)):
+        err = validate(filing)
+
+    assert err
+
+
+def test_business_type_required(session):
+    """Assert that business type is required."""
+    filing = copy.deepcopy(SP_REGISTRATION)
+    del filing['filing']['registration']['businessType']
+    with patch.object(NameXService, 'query_nr_number', return_value=MockResponse(nr_response)):
+        err = validate(filing)
+
+    assert err
+
+
+@pytest.mark.parametrize(
+    'test_name, filing, expected_msg',
+    [
+        ('sp_invalid_party', copy.deepcopy(SP_REGISTRATION), '1 Proprietor and a Completing Party is required.'),
+        ('dba_invalid_party', copy.deepcopy(DBA_REGISTRATION), '1 Proprietor and a Completing Party is required.'),
+        ('gp_invalid_party', copy.deepcopy(GP_REGISTRATION), '2 Partners and a Completing Party is required.'),
+    ]
+)
+def test_invalid_party(session, test_name, filing, expected_msg):
+    """Assert that party is invalid."""
+    filing['filing']['registration']['parties'] = []
+    with patch.object(NameXService, 'query_nr_number', return_value=MockResponse(nr_response)):
+        err = validate(filing)
+
+    assert err
+    assert err.msg[0]['error'] == expected_msg
+
+
+@pytest.mark.parametrize(
+    'test_name, filing',
+    [
+        ('sp_invalid_business_address', copy.deepcopy(SP_REGISTRATION)),
+        ('dba_invalid_business_address', copy.deepcopy(DBA_REGISTRATION)),
+        ('gp_invalid_business_address', copy.deepcopy(GP_REGISTRATION)),
+    ]
+)
+def test_invalid_business_address(session, test_name, filing):
+    """Assert that delivery business address is invalid."""
+    filing['filing']['registration']['businessAddress']['deliveryAddress']['addressRegion'] = 'invalid'
+    filing['filing']['registration']['businessAddress']['deliveryAddress']['addressCountry'] = 'invalid'
+    with patch.object(NameXService, 'query_nr_number', return_value=MockResponse(nr_response)):
+        err = validate(filing)
+
+    assert err
+    assert err.msg[0]['error'] == "Address Region must be 'BC'."
+    assert err.msg[1]['error'] == "Address Country must be 'CA'."
+
+
+@pytest.mark.parametrize(
+    'test_name, delta_date, is_valid',
+    [
+        ('today', None, True),
+        ('greater', timedelta(days=90), True),
+        ('invalid_greater', timedelta(days=91), False),
+        ('lesser', relativedelta(years=-2), True),
+        ('invalid_lesser', relativedelta(years=-2, days=-1), False)
+    ]
+)
+def test_validate_start_date(session, test_name, delta_date, is_valid):
+    """Assert that start date is validated."""
+    start_date = LegislationDatetime.now()
+    if delta_date:
+        start_date = start_date + delta_date
+
+    filing = copy.deepcopy(SP_REGISTRATION)
+    filing['filing']['registration']['startDate'] = start_date.strftime('%Y-%m-%d')
+    with patch.object(NameXService, 'query_nr_number', return_value=MockResponse(nr_response)):
+        err = validate(filing)
+
+    if is_valid:
+        assert not err
+    else:
+        assert err
+
+
+@pytest.mark.parametrize(
+    'test_status, file_number, effect_of_order, expected_code, expected_msg',
+    [
+        ('FAIL', None, 'planOfArrangement', HTTPStatus.BAD_REQUEST, 'Court order file number is required.'),
+        ('FAIL', '12345678901234567890', 'invalid', HTTPStatus.BAD_REQUEST, 'Invalid effectOfOrder.'),
+        ('SUCCESS', '12345678901234567890', 'planOfArrangement', None, None)
+    ]
+)
+def test_registration_court_orders(session, test_status, file_number, effect_of_order, expected_code, expected_msg):
+    """Assert valid court orders."""
+    filing = copy.deepcopy(GP_REGISTRATION)
+
+    court_order = {'effectOfOrder': effect_of_order}
+    if file_number:
+        court_order['fileNumber'] = file_number
+    filing['filing']['registration']['courtOrder'] = court_order
+
+    with patch.object(NameXService, 'query_nr_number', return_value=MockResponse(nr_response)):
+        err = validate(filing)
+
+    # validate outcomes
+    if test_status == 'FAIL':
+        assert expected_code == err.code
+        assert expected_msg == err.msg[0]['error']
+    else:
+        assert not err
