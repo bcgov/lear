01fe29a83e3bcfa033698213b43937880ab5ab9a
vysakh.menon <vysakh.menon@aot-technologies.com>
2022-03-29 11:00:41 -0700

    11713 remove check before consume NR

--- queue_services/entity-filer/src/entity_filer/filing_processors/change_of_registration.py
+++ queue_services/entity-filer/src/entity_filer/filing_processors/change_of_registration.py
@@ -136,8 +136,7 @@ def post_process(business: Business, filing: Filing):
 
     THIS SHOULD NOT ALTER THE MODEL
     """
-    if name_request.has_new_nr_for_filing(business, filing.filing_json, 'changeOfRegistration'):
-        name_request.consume_nr(business, filing, '/filing/changeOfRegistration/nameRequest/nrNumber')
+    name_request.consume_nr(business, filing, '/filing/changeOfRegistration/nameRequest/nrNumber')
 
     with suppress(IndexError, KeyError, TypeError):
         if err := business_profile.update_business_profile(
--- queue_services/entity-filer/src/entity_filer/filing_processors/filing_components/name_request.py
+++ queue_services/entity-filer/src/entity_filer/filing_processors/filing_components/name_request.py
@@ -70,13 +70,3 @@ def has_new_nr_for_correction(filing: dict):
             get('nameRequest').get('nrNumber', None)
         return old_nr_number != new_nr_number
     return False
-
-
-def has_new_nr_for_filing(business: Business, filing: dict, filing_type: str):
-    """Return whether a alteration filing has new NR."""
-    nr_number = get_str(filing, f'/filing/{filing_type}/nameRequest/nrNumber')
-    legal_name = get_str(filing, f'/filing/{filing_type}/nameRequest/legalName')
-    if nr_number and legal_name and business.legal_name:
-        # legal api validates legal name in filing json, confirm both are different
-        return legal_name.lower() != business.legal_name.lower()
-    return False
