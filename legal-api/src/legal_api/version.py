f0c0b8f3d937cf945ea86f9819769c6d1d4bac3a
vysakh.menon <vysakh.menon@aot-technologies.com>
2022-04-29 10:45:17 -0700

    bump version num 2.43.0

--- legal-api/src/legal_api/version.py
+++ legal-api/src/legal_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.42.0'  # pylint: disable=invalid-name
+__version__ = '2.43.0'  # pylint: disable=invalid-name
