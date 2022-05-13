b2d8d7edcb6dd7bc06ddd35d104be1bbdc812e5a
Argus Chiu <argus@highwaythreesolutions.com>
2022-05-13 11:51:01 -0700

    12036 bump version num (#1503)

--- legal-api/src/legal_api/version.py
+++ legal-api/src/legal_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.43.0'  # pylint: disable=invalid-name
+__version__ = '2.44.0'  # pylint: disable=invalid-name
