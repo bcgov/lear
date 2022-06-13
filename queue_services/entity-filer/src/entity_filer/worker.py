05001efe7c1289c4cc37f3333ed25254c664c692
ritvick <ritvick.paliwal@gmail.com>
2022-06-13 13:43:22 -0400

    Final fixes

--- queue_services/entity-filer/src/entity_filer/worker.py
+++ queue_services/entity-filer/src/entity_filer/worker.py
@@ -350,4 +350,3 @@ async def cb_subscription_handler(msg: nats.aio.client.Msg):
         # Catch Exception so that any error is still caught and the message is removed from the queue
         capture_message('Queue Error:' + json.dumps(filing_msg), level='error')
         logger.error('Queue Error: %s', json.dumps(filing_msg), exc_info=True)
-
