b0e5f4f358eb5360e8ccc30c57c97c32c48882d3
ritvick <ritvick.paliwal@gmail.com>
2022-06-15 15:12:04 -0400

    display name

--- legal-api/src/legal_api/core/meta/filing.py
+++ legal-api/src/legal_api/core/meta/filing.py
@@ -300,9 +300,8 @@ class FilingMeta:  # pylint: disable=too-few-public-methods
                 name = f'{name} - {FilingMeta.display_name(business_revision, filing.children[0], False)}'
 
         elif filing.filing_type in ('dissolution') and filing.meta_data:
-            with suppress(Exception):
-                if filing.meta_data['dissolution'].get('dissolutionType') == 'administrative':
-                    name = 'Administrative Dissolution'
+            if filing.meta_data['dissolution'].get('dissolutionType') == 'administrative':
+                name = 'Administrative Dissolution'
 
         if full_name and filing.parent_filing_id and filing.status == FilingStorage.Status.CORRECTED:
             name = f'{name} - Corrected'
