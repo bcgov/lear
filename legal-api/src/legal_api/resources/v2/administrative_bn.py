1beb432250eacbb57780ef4a5c7cd8b2f9231bfb
vysakh.menon <vysakh.menon@aot-technologies.com>
2022-07-11 14:47:22 -0700

    no message

--- legal-api/src/legal_api/resources/v2/administrative_bn.py
+++ legal-api/src/legal_api/resources/v2/administrative_bn.py
@@ -69,3 +69,4 @@ def publish_entity_event(business: Business,
     except Exception as err:  # pylint: disable=broad-except; we don't want to fail out the filing, so ignore all.
         capture_message('Queue Publish Admin Event Error: business.id=' + str(business.id) + str(err), level='error')
         current_app.logger.error('Queue Publish Event Error: business.id=%s', business.id, exc_info=True)
+        raise err
