ef2c75466fb42ea8fcdd25b9e4a2210c7553d188
Vysakh Menon <vysakh.menon@aot-technologies.com>
2022-08-16 11:44:20 -0700

    13227 skip missing business from colin tax_id update (#1633)

--- legal-api/src/legal_api/resources/v1/business/business_filings.py
+++ legal-api/src/legal_api/resources/v1/business/business_filings.py
@@ -841,8 +841,9 @@ class InternalFilings(Resource):
                         (filing.filing_type != 'correction' or business.legal_type != business.LegalTypes.COOP.value):
                     filing_json['filingId'] = filing.id
                     filing_json['filing']['header']['learEffectiveDate'] = filing.effective_date.isoformat()
-                    if not filing_json['filing']['business'].get('legalName'):
-                        business = Business.find_by_internal_id(filing.business_id)
+                    if not filing_json['filing'].get('business'):
+                        filing_json['filing']['business'] = business.json()
+                    elif not filing_json['filing']['business'].get('legalName'):
                         filing_json['filing']['business']['legalName'] = business.legal_name
                     if filing.filing_type == 'correction':
                         colin_ids = \
