16baf4ede77061c004d9c231aa805acd7f406fa9
ritvick <ritvick.paliwal@gmail.com>
2022-08-25 10:54:29 -0400

    comment

--- queue_services/entity-filer/src/entity_filer/filing_processors/filing_components/rules_and_memorandum.py
+++ queue_services/entity-filer/src/entity_filer/filing_processors/filing_components/rules_and_memorandum.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Manages the share structure for a business."""
+"""Manages the rules and memorandum for a business."""
 from __future__ import annotations
 
 from tokenize import String
