8a20ad97e1828c405abbd10343f68c718d5d6711
Argus Chiu <argus@highwaythreesolutions.com>
2022-09-16 15:01:03 -0700

    12887 Create SP/GP affiliation script (#1678)

---
+++ data-tool/flows/common/affiliation_queries.py
@@ -0,0 +1,12 @@
+def get_unaffiliated_firms_query(data_load_env: str):
+    query = f"""
+            select ap.account_id, ap.corp_num, ap.contact_email, c.admin_email
+            from affiliation_processing ap
+                     join corporation c on ap.corp_num = c.corp_num
+            where environment = '{data_load_env}'
+               and processed_status is null
+              or processed_status <> 'COMPLETED'
+            limit 5
+            ;
+        """
+    return query
