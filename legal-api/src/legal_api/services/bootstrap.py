72d51c24bd34bfce1730d5eca0797fce99dfe862
Lekshmi <lekshmi.mallika@aot-technologies.com>
2022-10-17 21:01:44 -0700

    filer fix (#1730)
    * filer fix
    
    * no message

--- legal-api/src/legal_api/services/bootstrap.py
+++ legal-api/src/legal_api/services/bootstrap.py
@@ -126,7 +126,7 @@ class AccountService:
 
     @classmethod
     # pylint: disable=too-many-arguments;
-    def create_affiliation(cls, account: int,
+    def create_affiliation(cls, account: int,  # pylint: disable=invalid-name
                            business_registration: str,
                            business_name: str = None,
                            corp_type_code: str = 'TMP',
@@ -143,17 +143,19 @@ class AccountService:
             return HTTPStatus.UNAUTHORIZED
 
         # Create an entity record
-        entity_data = json.dumps({'businessIdentifier': business_registration,
-                                  'corpTypeCode': corp_type_code,
-                                  'name': business_name or business_registration
-                                  })
+        entity_data = {
+            'businessIdentifier': business_registration,
+            'corpTypeCode': corp_type_code,
+            'name': business_name or business_registration
+        }
         if details:
             entity_data['details'] = details
+        payload = json.dumps(entity_data)
         entity_record = requests.post(
             url=account_svc_entity_url,
             headers={**cls.CONTENT_TYPE_JSON,
                      'Authorization': cls.BEARER + token},
-            data=entity_data,
+            data=payload,
             timeout=cls.timeout
         )
 
