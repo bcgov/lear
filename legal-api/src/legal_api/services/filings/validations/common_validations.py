94df4ea28c0ad627fd583f072a912b3fcc2ff36f
vysakh.menon <vysakh.menon@aot-technologies.com>
2022-12-02 17:29:53 -0800

    no message

--- legal-api/src/legal_api/services/filings/validations/common_validations.py
+++ legal-api/src/legal_api/services/filings/validations/common_validations.py
@@ -228,10 +228,9 @@ def validate_party_name(legal_type: str, party: dict, party_path: str) -> list:
     return msg
 
 
-def validate_name_request(
-        filing_json: dict,
-        legal_type: str,
-        filing_type: str) -> list:  # pylint: disable=too-many-locals
+def validate_name_request(filing_json: dict,  # pylint: disable=too-many-locals
+                          legal_type: str,
+                          filing_type: str) -> list:
     """Validate name request section."""
     nr_path = f'/filing/{filing_type}/nameRequest'
     nr_number_path = f'{nr_path}/nrNumber'
