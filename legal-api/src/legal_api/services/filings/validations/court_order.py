45f2fcb0a5c2186bca31d551942c10c4963b412d
vysakh.menon <vysakh.menon@aot-technologies.com>
2023-01-04 17:26:49 -0800

    14604 uploaded court order

--- legal-api/src/legal_api/services/filings/validations/court_order.py
+++ legal-api/src/legal_api/services/filings/validations/court_order.py
@@ -19,6 +19,7 @@ from flask_babel import _ as babel  # noqa: N813, I004, I001; importing camelcas
 # noqa: I004
 from legal_api.errors import Error
 from legal_api.models import Business
+from legal_api.services.filings.validations.common_validations import validate_pdf
 
 from ...utils import get_str
 # noqa: I003; needed as the linter gets confused from the babel override above.
@@ -30,12 +31,23 @@ def validate(business: Business, court_order: Dict) -> Optional[Error]:
         return Error(HTTPStatus.BAD_REQUEST, [{'error': babel('A valid business and filing are required.')}])
     msg = []
 
-    if not get_str(court_order, '/filing/courtOrder/orderDetails'):
-        msg.append({'error': babel('Court Order is required.'), 'path': '/filing/courtOrder/orderDetails'})
-
     effect_of_order = get_str(court_order, '/filing/courtOrder/effectOfOrder')
     if effect_of_order and effect_of_order != 'planOfArrangement':
         msg.append({'error': babel('Invalid effectOfOrder.'), 'path': '/filing/courtOrder/effectOfOrder'})
+
+    file_key_path = '/filing/courtOrder/fileKey'
+    file_key = get_str(court_order, file_key_path)
+
+    order_details_path = '/filing/courtOrder/orderDetails'
+    order_details = get_str(court_order, order_details_path)
+
+    if not order_details and not file_key:
+        msg.append({'error': babel('Court Order is required (in orderDetails/fileKey).'), 'path': '/filing/courtOrder'})
+
+    if file_key:
+        file_err = validate_pdf(file_key, file_key_path)
+        if file_err:
+            msg.extend(file_err)
 
     if msg:
         return Error(HTTPStatus.BAD_REQUEST, msg)
