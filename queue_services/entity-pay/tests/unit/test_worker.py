fbed99db3fad79fa858e7b17e3f3328d12ad06d3
Argus Chiu <argus@highwaythreesolutions.com>
2023-01-30 11:42:58 -0800

    15114 Update entity pay to support payment queue message with filing id and corp type code fields (#1908)

--- queue_services/entity-pay/tests/unit/test_worker.py
+++ queue_services/entity-pay/tests/unit/test_worker.py
@@ -23,9 +23,10 @@ from tests.unit import create_business, create_filing  # noqa I001, E501;
 
 def test_extract_payment_token():
     """Assert that the payment token can be extracted from the Queue delivered Msg."""
-    from entity_pay.worker import extract_payment_token
-    from stan.aio.client import Msg
     import stan.pb.protocol_pb2 as protocol
+    from stan.aio.client import Msg
+
+    from entity_pay.worker import extract_payment_token
 
     # setup
     token = {'paymentToken': {'id': 1234, 'statusCode': 'COMPLETED'}}
@@ -37,24 +38,24 @@ def test_extract_payment_token():
     assert extract_payment_token(msg) == token
 
 
-def test_get_filing_by_payment_id(app, session):
-    """Assert that a unique filling gets retrieved for a payment_id."""
-    from entity_pay.worker import get_filing_by_payment_id
+def test_get_filing_by_id(app, session):
+    """Assert that a unique filling gets retrieved for a filing id."""
+    from entity_pay.worker import get_filing_by_id
 
     payment_id = str(random.SystemRandom().getrandbits(0x58))
+    new_filing = create_filing(payment_id)
 
-    create_filing(payment_id)
-
-    filing = get_filing_by_payment_id(str(payment_id))
+    filing = get_filing_by_id(new_filing.id)
 
     assert filing
-    assert filing.payment_token == payment_id
+    assert filing.id == new_filing.id
 
 
 async def test_process_payment_missing_app(app, session):
     """Assert that a filling will fail with no flask app supplied."""
-    from entity_pay.worker import process_payment
     from legal_api.models import Filing
+
+    from entity_pay.worker import process_payment
 
     # vars
     payment_id = str(random.SystemRandom().getrandbits(0x58))
@@ -62,8 +63,10 @@ async def test_process_payment_missing_app(app, session):
 
     # setup
     business = create_business(identifier)
-    create_filing(payment_id, None, business.id)
-    payment_token = {'paymentToken': {'id': payment_id, 'statusCode': Filing.Status.COMPLETED.value}}
+    filing = create_filing(payment_id, None, business.id)
+    payment_token = {'paymentToken': {'id': payment_id,
+                                      'statusCode': Filing.Status.COMPLETED.value,
+                                      'filingIdentifier': f'{filing.id}'}}
 
     # TEST
     with pytest.raises(Exception):
@@ -72,8 +75,9 @@ async def test_process_payment_missing_app(app, session):
 
 async def test_process_empty_filing(app, session):
     """Assert that an AR filling can be applied to the model correctly."""
-    from entity_pay.worker import get_filing_by_payment_id, process_payment
     from legal_api.models import Filing
+
+    from entity_pay.worker import get_filing_by_id, process_payment
 
     # vars
     payment_id = str(random.SystemRandom().getrandbits(0x58))
@@ -82,14 +86,16 @@ async def test_process_empty_filing(app, session):
     # setup
     business = create_business(identifier)
     business_id = business.id
-    create_filing(payment_id, None, business.id)
-    payment_token = {'paymentToken': {'id': payment_id, 'statusCode': Filing.Status.COMPLETED.value}}
+    initial_filing = create_filing(payment_id, None, business.id)
+    payment_token = {'paymentToken': {'id': payment_id,
+                                      'statusCode': Filing.Status.COMPLETED.value,
+                                      'filingIdentifier': f'{initial_filing.id}'}}
 
     # TEST
     await process_payment(payment_token, app)
 
     # Get modified data
-    filing = get_filing_by_payment_id(payment_id)
+    filing = get_filing_by_id(initial_filing.id)
 
     # check it out
     assert filing.business_id == business_id
@@ -98,9 +104,9 @@ async def test_process_empty_filing(app, session):
 
 async def test_process_payment_failed(app, session):
     """Assert that an AR filling status is set to error if payment transaction failed."""
-    from entity_pay.worker import process_payment
-    from entity_pay.worker import get_filing_by_payment_id
     from legal_api.models import Business, Filing
+
+    from entity_pay.worker import get_filing_by_id, process_payment
 
     # vars
     payment_id = str(random.SystemRandom().getrandbits(0x58))
@@ -109,14 +115,16 @@ async def test_process_payment_failed(app, session):
     # setup
     business = create_business(identifier)
     business_id = business.id
-    create_filing(payment_id, None, business.id)
-    payment_token = {'paymentToken': {'id': payment_id, 'statusCode': 'TRANSACTION_FAILED'}}
+    filing = create_filing(payment_id, None, business.id)
+    payment_token = {'paymentToken': {'id': payment_id,
+                                      'statusCode': 'TRANSACTION_FAILED',
+                                      'filingIdentifier': f'{filing.id}'}}
 
     # TEST
     await process_payment(payment_token, app)
 
     # Get modified data
-    filing = get_filing_by_payment_id(payment_id)
+    filing = get_filing_by_id(filing.id)
     business = Business.find_by_internal_id(business_id)
 
     # check it out
@@ -124,3 +132,33 @@ async def test_process_payment_failed(app, session):
     assert filing.status == Filing.Status.PENDING.value
     assert not business.last_agm_date
     assert not business.last_ar_date
+
+
+@pytest.mark.parametrize('name,filing_id,corp_type_code,expected_result', [
+    ('success', '1', 'BEN', True),
+    ('success', '1', 'CP', True),
+    ('success', '1', 'SP', True),
+    ('success', '1', 'GP', True),
+    ('success', '1', 'BC', True),
+    ('success', '1', 'ULC', True),
+    ('success', '1', 'CC', True),
+    ('fail_invalid_corp_type', '1', None, False),
+    ('fail_invalid_corp_type', '1', 'CSO', False),
+    ('fail_no_filing_id', None, 'BC', False),
+    ('fail_no_payment_token', '1', 'BC', False),
+])
+def test_is_processable_message(app, session, name, filing_id, corp_type_code, expected_result):
+    """Assert that the queue message is processable only when msg meets required criteria."""
+    from entity_pay.worker import is_processable_message
+
+    # setup
+    if name == 'fail_no_payment_token':
+        msg = {'paymentToken': None}
+    else:
+        msg = {'paymentToken': {'id': 1234,
+                                'statusCode': 'COMPLETED',
+                                'filingIdentifier': filing_id,
+                                'corpTypeCode': corp_type_code}}
+
+    # test and verify
+    assert is_processable_message(msg) == expected_result
