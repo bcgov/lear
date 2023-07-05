273f2b5f89831acf0383de4e840c2509cdffb0ad
Travis Semple <travis8814@gmail.com>
2023-07-05 14:19:00 -0700

    Minor fixes based on UX and testing.

--- legal-api/src/legal_api/core/meta/filing.py
+++ legal-api/src/legal_api/core/meta/filing.py
@@ -486,6 +486,7 @@ class FilingMeta:  # pylint: disable=too-few-public-methods
             if filing.meta_data.get('alteration', {}).get('toLegalName'):
                 outputs.add('certificateOfNameChange')
         elif filing.filing_type == 'specialResolution':
+            outputs.remove('certificateOfIncorporation')
             if 'changeOfName' in filing.meta_data.get('legalFilings', []):
                 outputs.add('certificateOfNameChange')
             if 'alteration' in filing.meta_data.get('legalFilings', []):
