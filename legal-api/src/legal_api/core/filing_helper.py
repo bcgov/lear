3fda09f682c9f1414ed3866361fab8d03f193f41
Travis Semple <travis8814@gmail.com>
2023-07-14 09:06:35 -0700

    Comment update

--- legal-api/src/legal_api/core/filing_helper.py
+++ legal-api/src/legal_api/core/filing_helper.py
@@ -6,8 +6,8 @@ from typing import Dict
 
 def is_special_resolution_correction_by_meta_data(filing):
     """Check whether it is a special resolution correction."""
-    # Check by using the Metadata, this is more permanent than the filing json.
-    # This is used by reports and email (after the filer).
+    # Check by using the meta_data, this is more permanent than the filing json.
+    # This is used by reports (after the filer).
     if filing.meta_data and (correction_meta_data := filing.meta_data.get('correction')):
         # Note these come from the corrections filer.
         sr_correction_meta_data_keys = ['hasResolution', 'memorandumInResolution', 'rulesInResolution',
@@ -21,7 +21,7 @@ def is_special_resolution_correction_by_meta_data(filing):
 def is_special_resolution_correction_by_filing_json(filing: Dict):
     """Check whether it is a special resolution correction."""
     # Note this relies on the filing data once. This is acceptable inside of the filer (which runs once)
-    # and emailer (runs on PAID and runs on COMPLETED).
+    # and emailer (runs on PAID which is before the filer and runs on COMPLETED).
     # For filing data that persists in the database, attempt to use the meta_data instead.
     sr_correction_keys = ['rulesInResolution', 'resolution', 'rulesFileKey',
                           'memorandumInResolution', 'associationType']
