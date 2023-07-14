6dda146b3b685f0f78babe339b5eeef848ad1912
Travis Semple <travis8814@gmail.com>
2023-07-14 10:47:47 -0700

    Remove certifiedMemorandum

--- legal-api/src/legal_api/core/meta/filing.py
+++ legal-api/src/legal_api/core/meta/filing.py
@@ -398,7 +398,7 @@ FILINGS: Final = {
             'CP': 'SPRLN'
         },
         'additional': [
-            {'types': 'CP', 'outputs': ['certifiedRules', 'certifiedMemorandum']},
+            {'types': 'CP', 'outputs': ['certifiedRules']},
         ]
     },
     'transition': {
