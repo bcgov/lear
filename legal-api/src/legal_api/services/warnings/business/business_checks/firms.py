2a8984081226813d1e50851300d69d769e8758a9
Paul <144158015+PaulGarewal@users.noreply.github.com>
2023-09-11 15:09:42 -0700

    Update firms.py

--- legal-api/src/legal_api/services/warnings/business/business_checks/firms.py
+++ legal-api/src/legal_api/services/warnings/business/business_checks/firms.py
@@ -256,10 +256,10 @@ def check_address(address: Address,
         result.append(get_address_business_warning(referer,
                                                    address_type,
                                                    BusinessWarnings.NO_ADDRESS_POSTAL_CODE))
-        
+
     if referer != BusinessWarningReferers.BUSINESS_OFFICE and not address.region:
         result.append(get_address_business_warning(referer,
                                                    address_type,
                                                    BusinessWarnings.NO_ADDRESS_REGION))
-            
+
     return result
