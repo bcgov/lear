4665ae2afb4caae630514e3533c882ec79c29035
Paul <144158015+PaulGarewal@users.noreply.github.com>
2023-09-13 14:25:21 -0700

    Update firms.py conditional on line 260

--- legal-api/src/legal_api/services/warnings/business/business_checks/firms.py
+++ legal-api/src/legal_api/services/warnings/business/business_checks/firms.py
@@ -257,8 +257,9 @@ def check_address(address: Address,
                                                    address_type,
                                                    BusinessWarnings.NO_ADDRESS_POSTAL_CODE))
 
-    if referer not in (BusinessWarningReferers.BUSINESS_OFFICE, BusinessWarningReferers.BUSINESS_PARTY,
-                       BusinessWarningReferers.COMPLETING_PARTY) and not address.region:
+    if (referer == BusinessWarningReferers.BUSINESS_OFFICE
+        and address_type == Address.DELIVERY
+        and not address.region):
         result.append(get_address_business_warning(referer,
                                                    address_type,
                                                    BusinessWarnings.NO_ADDRESS_REGION))
