28531edc8d0b4e9a010d227ff6d12c2ddb395550
Jia Xu <Jia.Xu@gov.bc.ca>
2023-09-21 14:27:28 -0700

    remove some unnessery changes

--- queue_services/entity-filer/src/entity_filer/filing_processors/correction.py
+++ queue_services/entity-filer/src/entity_filer/filing_processors/correction.py
@@ -53,7 +53,8 @@ def process(correction_filing: Filing, filing: Dict, filing_meta: FilingMeta, bu
     corrected_filing_type = filing['correction']['correctedFilingType']
     is_sr_correction = business.legal_type == 'CP' and is_special_resolution_correction_by_filing_json(filing)
     if (business.legal_type in ['SP', 'GP', 'BC', 'BEN', 'CC', 'ULC'] or
-            is_sr_correction) and corrected_filing_type != 'conversion':
+            is_sr_correction) and \
+            corrected_filing_type != 'conversion':
         correct_business_data(business, correction_filing, filing, filing_meta)
     else:
         # set correction filing to PENDING_CORRECTION, for manual intervention
