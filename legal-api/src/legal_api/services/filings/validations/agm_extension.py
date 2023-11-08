4efe526b698e4bf24cccd90a743a0cdca04daa5b
Hongjing Chen <Hongjing.Chen@gov.bc.ca>
2023-11-08 13:33:01 -0800

    update agm validation
    Signed-off-by: Hongjing Chen <Hongjing.Chen@gov.bc.ca>

--- legal-api/src/legal_api/services/filings/validations/agm_extension.py
+++ legal-api/src/legal_api/services/filings/validations/agm_extension.py
@@ -21,6 +21,8 @@ from flask_babel import _ as babel  # noqa: N813, I004, I001; importing camelcas
 from legal_api.errors import Error
 from legal_api.models import Business
 from legal_api.utils.legislation_datetime import LegislationDatetime
+# noqa: I003
+
 
 def validate(business: Business, filing: Dict) -> Optional[Error]:
     """Validate the Agm Extension filing."""
@@ -29,83 +31,79 @@ def validate(business: Business, filing: Dict) -> Optional[Error]:
 
     msg = []
 
-    agm_extension_path = '/filing/agmExtension'
-
     is_first_agm = filing['filing']['agmExtension']['isFirstAgm']
-    has_ext_req_for_agm_year = filing['filing']['agmExtension']['extReqForAgmYear']
 
     if is_first_agm:
-        # first agm
-        founding_date = business.founding_date
-        if not has_ext_req_for_agm_year:
-            # first AGM, first extension
-            now = LegislationDatetime.now()
-            latest_ext_date = founding_date + relativedelta(months=18, days=5)
-            if now > latest_ext_date:
-                msg.append({'error': 'Company failed to request the extension in time.', 'path': f'{agm_extension_path}/isFirstAgm'})
-            # else:
-            #     pass
-                # total_extension_approved = 6
-                # extension_left_to_be_granted = 6
-
-        else:
-            # first AGM, second extension or more
-            curr_ext_expire_date_str = filing['filing']['agmExtension']['expireDateCurrExt']
-            curr_ext_expire_date = LegislationDatetime.as_legislation_timezone_from_date_str(curr_ext_expire_date_str)
-            allowable_ext_date = founding_date + relativedelta(months=30)
-
-            if curr_ext_expire_date >= allowable_ext_date:
-                msg.append({'error': 'Company has received the maximum 12 months of allowable extensions.', 'path': f'{agm_extension_path}/expireDateCurrExt'})
-            else:
-                now = LegislationDatetime.now()
-                if now > curr_ext_expire_date + relativedelta(days=5):
-                    msg.append({'error': 'Company failed to request the extension in time.', 'path': f'{agm_extension_path}/expireDateCurrExt'})
-                # else:
-                #     pass
-                    # delta = founding_date + relativedelta(months=18)
-                    # total_extension_approved = total_extension_approved.year * 12 + total_extension_approved.month
-                    # extension_approved = min(12-total_extension_approved, 6)
-                    # total_extension_approved += extension_approved
-                    # extension_left_to_be_granted = 12 - total_extension_approved
-
+        msg.extend(first_agm_validation(business, filing))
     else:
-        # subsequent AGM
-        prev_agm_ref_date_str = filing['filing']['agmExtension']['prevAgmRefDate']
-        prev_agm_ref_date = LegislationDatetime.as_legislation_timezone_from_date_str(prev_agm_ref_date_str)
+        msg.extend(subsequent_agm_validation(filing))
 
-        if not has_ext_req_for_agm_year:
-            # subsequent AGM, first extension
-
-            now = LegislationDatetime.now()
-            latest_ext_date = prev_agm_ref_date + relativedelta(months=15, days=5)
-            if now > latest_ext_date:
-                msg.append({'error': 'Company failed to request the extension in time.', 'path': f'{agm_extension_path}/prevAgmRefDate'})
-            # else:
-            #     pass
-                # total_extension_approved = 6
-                # extension_left_to_be_granted = 6
-        else:
-            # subsequent AGM, second extension or more
-            curr_ext_expire_date_str = filing['filing']['agmExtension']['expireDateCurrExt']
-            curr_ext_expire_date = LegislationDatetime.as_legislation_timezone_from_date_str(curr_ext_expire_date_str)
-
-            allowable_ext_date = prev_agm_ref_date + relativedelta(months=12)
-
-            if curr_ext_expire_date >= allowable_ext_date:
-                msg.append({'error': 'Company has received the maximum 12 months of allowable extensions.', 'path': f'{agm_extension_path}/expireDateCurrExt'})
-            else:
-                now = LegislationDatetime.now()
-
-                if now > curr_ext_expire_date + relativedelta(days=5):
-                    msg.append({'error': 'Company failed to request the extension in time.', 'path': f'{agm_extension_path}/expireDateCurrExt'})
-                # else:
-                #     pass
-                    # total_extension_approved = total_extension_approved.year * 12 + total_extension_approved.month
-                    # total_extension_approved
-                    # extension_approved = min(12-total_extension_approved, 6)
-                    # total_extension_approved += extension_approved
-                    # extension_left_to_be_granted = 12 - total_extension_approved
     if msg:
         return Error(HTTPStatus.BAD_REQUEST, msg)
 
     return None
+
+
+def first_agm_validation(business: Business, filing: Dict) -> list:
+    """Validate filing for first AGM Extension."""
+    msg = []
+
+    agm_extension_path = '/filing/agmExtension'
+    has_ext_req_for_agm_year = filing['filing']['agmExtension']['extReqForAgmYear']
+    founding_date = business.founding_date
+
+    if not has_ext_req_for_agm_year:
+        # first AGM, first extension
+        now = LegislationDatetime.now()
+        latest_ext_date = founding_date + relativedelta(months=18, days=5)
+        if now > latest_ext_date:
+            msg.append({'error': 'Company failed to request the extension in time.',
+                        'path': f'{agm_extension_path}/isFirstAgm'})
+    else:
+        # first AGM, second extension or more
+        curr_ext_expire_date_str = filing['filing']['agmExtension']['expireDateCurrExt']
+        curr_ext_expire_date = LegislationDatetime.as_legislation_timezone_from_date_str(curr_ext_expire_date_str)
+        allowable_ext_date = founding_date + relativedelta(months=30)
+        now = LegislationDatetime.now()
+        if curr_ext_expire_date >= allowable_ext_date:
+            msg.append({'error': 'Company has received the maximum 12 months of allowable extensions.',
+                        'path': f'{agm_extension_path}/expireDateCurrExt'})
+        elif now > curr_ext_expire_date + relativedelta(days=5):
+            msg.append({'error': 'Company failed to request the extension in time.',
+                        'path': f'{agm_extension_path}/expireDateCurrExt'})
+
+    return msg
+
+
+def subsequent_agm_validation(filing: Dict) -> list:
+    """Validate filing for subsequent AGM Extension."""
+    msg = []
+    agm_extension_path = '/filing/agmExtension'
+
+    has_ext_req_for_agm_year = filing['filing']['agmExtension']['extReqForAgmYear']
+    prev_agm_ref_date_str = filing['filing']['agmExtension']['prevAgmRefDate']
+    prev_agm_ref_date = LegislationDatetime.as_legislation_timezone_from_date_str(prev_agm_ref_date_str)
+
+    if not has_ext_req_for_agm_year:
+        # subsequent AGM, first extension
+        now = LegislationDatetime.now()
+        latest_ext_date = prev_agm_ref_date + relativedelta(months=15, days=5)
+        if now > latest_ext_date:
+            msg.append({'error': 'Company failed to request the extension in time.',
+                        'path': f'{agm_extension_path}/prevAgmRefDate'})
+    else:
+        # subsequent AGM, second extension or more
+        curr_ext_expire_date_str = filing['filing']['agmExtension']['expireDateCurrExt']
+        curr_ext_expire_date = LegislationDatetime.as_legislation_timezone_from_date_str(curr_ext_expire_date_str)
+
+        allowable_ext_date = prev_agm_ref_date + relativedelta(months=12)
+        now = LegislationDatetime.now()
+
+        if curr_ext_expire_date >= allowable_ext_date:
+            msg.append({'error': 'Company has received the maximum 12 months of allowable extensions.',
+                        'path': f'{agm_extension_path}/expireDateCurrExt'})
+        elif now > curr_ext_expire_date + relativedelta(days=5):
+            msg.append({'error': 'Company failed to request the extension in time.',
+                        'path': f'{agm_extension_path}/expireDateCurrExt'})
+
+    return msg
