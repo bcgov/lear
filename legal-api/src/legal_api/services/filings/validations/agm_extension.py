5e266d03781df2ba898e169bc7f867eb88aa85b2
Hongjing Chen <Hongjing.Chen@gov.bc.ca>
2023-11-09 16:32:56 -0800

    more validations
    Signed-off-by: Hongjing Chen <Hongjing.Chen@gov.bc.ca>

--- legal-api/src/legal_api/services/filings/validations/agm_extension.py
+++ legal-api/src/legal_api/services/filings/validations/agm_extension.py
@@ -21,8 +21,11 @@ from flask_babel import _ as babel  # noqa: N813, I004, I001; importing camelcas
 from legal_api.errors import Error
 from legal_api.models import Business
 from legal_api.utils.legislation_datetime import LegislationDatetime
+from ...utils import get_bool, get_int, get_str
 # noqa: I003
 
+
+AGM_EXTENSION_PATH = '/filing/agmExtension'
 
 def validate(business: Business, filing: Dict) -> Optional[Error]:
     """Validate the AGM Extension filing."""
@@ -31,12 +34,19 @@ def validate(business: Business, filing: Dict) -> Optional[Error]:
 
     msg = []
 
-    is_first_agm = filing['filing']['agmExtension']['isFirstAgm']
+    if get_bool(filing, f'{AGM_EXTENSION_PATH}/isFirstAgm') is None:
+        return Error(HTTPStatus.BAD_REQUEST, [{'error': babel('isFirstAgm is required.'), 'path': f'{AGM_EXTENSION_PATH}/isFirstAgm'}])
+
+    if get_bool(filing, f'{AGM_EXTENSION_PATH}/extReqForAgmYear') is None:
+        return Error(HTTPStatus.BAD_REQUEST, [{'error': babel('extReqForAgmYear is required.'), 'path': f'{AGM_EXTENSION_PATH}/extReqForAgmYear'}])
+
+    is_first_agm = get_bool(filing, f'{AGM_EXTENSION_PATH}/isFirstAgm')
 
     if is_first_agm:
         msg.extend(first_agm_validation(business, filing))
     else:
         msg.extend(subsequent_agm_validation(filing))
+    msg.extend(intended_agm_date_validation(filing))
 
     if msg:
         return Error(HTTPStatus.BAD_REQUEST, msg)
@@ -48,9 +58,8 @@ def first_agm_validation(business: Business, filing: Dict) -> list:
     """Validate filing for first AGM Extension."""
     msg = []
 
-    agm_extension_path = '/filing/agmExtension'
-    has_ext_req_for_agm_year = filing['filing']['agmExtension']['extReqForAgmYear']
-    founding_date = business.founding_date
+    has_ext_req_for_agm_year = get_bool(filing, f'{AGM_EXTENSION_PATH}/extReqForAgmYear')
+    founding_date = LegislationDatetime.as_legislation_timezone_from_date(business.founding_date)
 
     if not has_ext_req_for_agm_year:
         # first AGM, first extension
@@ -58,7 +67,12 @@ def first_agm_validation(business: Business, filing: Dict) -> list:
         latest_ext_date = founding_date + relativedelta(months=18, days=5)
         if now > latest_ext_date:
             msg.append({'error': 'Allotted period to request extension has expired.',
-                        'path': f'{agm_extension_path}/isFirstAgm'})
+                        'path': f'{AGM_EXTENSION_PATH}/isFirstAgm'})
+        else:
+            total_approved_ext = get_int(filing, f'{AGM_EXTENSION_PATH}/totalApprovedExt')
+            extension_duration = get_int(filing, f'{AGM_EXTENSION_PATH}/extensionDuration')
+            if total_approved_ext != 6 or extension_duration != 6:
+                msg.append({'error': babel('Fail to grant extension.')})
     else:
         # first AGM, second extension or more
         curr_ext_expire_date_str = filing['filing']['agmExtension']['expireDateCurrExt']
@@ -67,10 +81,20 @@ def first_agm_validation(business: Business, filing: Dict) -> list:
         now = LegislationDatetime.now()
         if curr_ext_expire_date >= allowable_ext_date:
             msg.append({'error': 'Company has received the maximum 12 months of allowable extensions.',
-                        'path': f'{agm_extension_path}/expireDateCurrExt'})
+                        'path': f'{AGM_EXTENSION_PATH}/expireDateCurrExt'})
         elif now > curr_ext_expire_date + relativedelta(days=5):
             msg.append({'error': 'Allotted period to request extension has expired.',
-                        'path': f'{agm_extension_path}/expireDateCurrExt'})
+                        'path': f'{AGM_EXTENSION_PATH}/expireDateCurrExt'})
+        else:
+            total_approved_ext = get_int(filing, f'{AGM_EXTENSION_PATH}/totalApprovedExt')
+            extension_duration = get_int(filing, f'{AGM_EXTENSION_PATH}/extensionDuration')
+
+            baseline = founding_date + relativedelta(months=18)
+            expected_total_approved_ext, expected_extension_duration = _calculate_granted_ext(curr_ext_expire_date, baseline)
+
+            if expected_total_approved_ext != total_approved_ext or\
+                expected_extension_duration != extension_duration:
+                msg.append({'error': babel('Fail to grant extension.')})
 
     return msg
 
@@ -78,7 +102,6 @@ def first_agm_validation(business: Business, filing: Dict) -> list:
 def subsequent_agm_validation(filing: Dict) -> list:
     """Validate filing for subsequent AGM Extension."""
     msg = []
-    agm_extension_path = '/filing/agmExtension'
 
     has_ext_req_for_agm_year = filing['filing']['agmExtension']['extReqForAgmYear']
     prev_agm_ref_date_str = filing['filing']['agmExtension']['prevAgmRefDate']
@@ -90,7 +113,12 @@ def subsequent_agm_validation(filing: Dict) -> list:
         latest_ext_date = prev_agm_ref_date + relativedelta(months=15, days=5)
         if now > latest_ext_date:
             msg.append({'error': 'Allotted period to request extension has expired.',
-                        'path': f'{agm_extension_path}/prevAgmRefDate'})
+                        'path': f'{AGM_EXTENSION_PATH}/prevAgmRefDate'})
+        else:
+            total_approved_ext = get_int(filing, f'{AGM_EXTENSION_PATH}/totalApprovedExt')
+            extension_duration = get_int(filing, f'{AGM_EXTENSION_PATH}/extensionDuration')
+            if total_approved_ext != 6 or extension_duration != 6:
+                msg.append({'error': babel('Fail to grant extension.')})
     else:
         # subsequent AGM, second extension or more
         curr_ext_expire_date_str = filing['filing']['agmExtension']['expireDateCurrExt']
@@ -101,9 +129,43 @@ def subsequent_agm_validation(filing: Dict) -> list:
 
         if curr_ext_expire_date >= allowable_ext_date:
             msg.append({'error': 'Company has received the maximum 12 months of allowable extensions.',
-                        'path': f'{agm_extension_path}/expireDateCurrExt'})
+                        'path': f'{AGM_EXTENSION_PATH}/expireDateCurrExt'})
         elif now > curr_ext_expire_date + relativedelta(days=5):
             msg.append({'error': 'Allotted period to request extension has expired.',
-                        'path': f'{agm_extension_path}/expireDateCurrExt'})
+                        'path': f'{AGM_EXTENSION_PATH}/expireDateCurrExt'})
+        else:
+            total_approved_ext = get_int(filing, f'{AGM_EXTENSION_PATH}/totalApprovedExt')
+            extension_duration = get_int(filing, f'{AGM_EXTENSION_PATH}/extensionDuration')
+
+            expected_total_approved_ext, expected_extension_duration = _calculate_granted_ext(curr_ext_expire_date, prev_agm_ref_date)
+
+            if expected_total_approved_ext != total_approved_ext or\
+                expected_extension_duration != extension_duration:
+                msg.append({'error': babel('Fail to grant extension.')})
 
     return msg
+
+
+def intended_agm_date_validation(filing: Dict) -> list:
+    """Validate intended AGM date"""
+    msg = []
+    intended_agm_date_str = get_str(filing, f'{AGM_EXTENSION_PATH}/intendedAgmDate')
+    curr_ext_expire_date_str = get_str(filing, f'{AGM_EXTENSION_PATH}/expireDateCurrExt')
+    if intended_agm_date_str and curr_ext_expire_date_str:
+        intended_agm_date = LegislationDatetime.as_legislation_timezone_from_date_str(intended_agm_date_str)
+        curr_ext_expire_date = LegislationDatetime.as_legislation_timezone_from_date_str(curr_ext_expire_date_str)
+
+        if intended_agm_date > curr_ext_expire_date:
+            msg.append({'error': 'Intended AGM date should not be greater than current extension expiry date.',
+                    'path': f'{AGM_EXTENSION_PATH}/intendedAgmDate'})
+
+    return msg
+
+
+def _calculate_granted_ext(curr_ext_expire_date, baseline_date) -> tuple:
+    """Calculate expected total approved extension and extension duration."""
+    total_approved_ext = relativedelta(curr_ext_expire_date, baseline_date).months
+    extension_duration = min(12-total_approved_ext, 6)
+    total_approved_ext += extension_duration
+
+    return total_approved_ext, extension_duration
--- legal-api/tests/unit/services/filings/validations/test_agm_extension.py
+++ legal-api/tests/unit/services/filings/validations/test_agm_extension.py
@@ -18,7 +18,7 @@ from http import HTTPStatus
 from unittest.mock import patch
 
 import pytest
-from registry_schemas.example_data import AGM_EXTENSION, FILING_HEADER
+from registry_schemas.example_data import FILING_HEADER
 
 from legal_api.services.filings.validations.validation import validate
 from legal_api.utils.legislation_datetime import LegislationDatetime
@@ -30,19 +30,26 @@ from tests.unit.models import factory_business
 @pytest.mark.parametrize(
     'test_name, founding_date, agm_ext_json, expected_code, message',
     [
-        ('SUCCESS_FIRST_AGM_FIRST_EXT', '2023-10-01', {'year': '2023','isFirstAgm': True, 'extReqForAgmYear': False}, None, None),
+        ('SUCCESS_FIRST_AGM_FIRST_EXT', '2023-10-01', 
+         {'year': '2023','isFirstAgm': True, 'extReqForAgmYear': False, 'totalApprovedExt': 6, 'extensionDuration': 6},
+         None, None),
         ('FAIL_FIRST_AGM_FIRST_EXT_TOO_LATE', '2020-10-01', {'year': '2023','isFirstAgm': True, 'extReqForAgmYear': False},
          HTTPStatus.BAD_REQUEST, 'Allotted period to request extension has expired.'),
-        ('SUCCESS_FIRST_AGM_MORE_EXT', '2022-10-01', {'year': '2023','isFirstAgm': True, 'extReqForAgmYear': True, 'expireDateCurrExt': '2024-10-01'}, None, None),
+        ('SUCCESS_FIRST_AGM_MORE_EXT', '2022-10-01', 
+         {'year': '2023','isFirstAgm': True, 'extReqForAgmYear': True, 'expireDateCurrExt': '2024-10-01', 'totalApprovedExt': 12, 'extensionDuration': 6},
+           None, None),
         ('FAIL_FIRST_AGM_MORE_EXT_TOO_LATE', '2022-10-01', {'year': '2023','isFirstAgm': True, 'extReqForAgmYear': True, 'expireDateCurrExt': '2023-12-01'},
          HTTPStatus.BAD_REQUEST, 'Allotted period to request extension has expired.'),
         ('FAIL_FIRST_AGM_MORE_EXT_EXCEED_LIMIT', '2021-10-01', {'year': '2023','isFirstAgm': True, 'extReqForAgmYear': True, 'expireDateCurrExt': '2024-10-01'},
          HTTPStatus.BAD_REQUEST, 'Company has received the maximum 12 months of allowable extensions.'),
-        ('SUCCESS_SUBSEQUENT_AGM_FIRST_EXT', '2020-10-01', {'year': '2023','isFirstAgm': False, 'extReqForAgmYear': False, 'prevAgmRefDate':'2023-10-01'}, None, None),
+        ('SUCCESS_SUBSEQUENT_AGM_FIRST_EXT', '2020-10-01', 
+         {'year': '2023','isFirstAgm': False, 'extReqForAgmYear': False, 'prevAgmRefDate':'2023-10-01', 'totalApprovedExt': 6, 'extensionDuration': 6},
+           None, None),
         ('FAIL_SUBSEQUENT_AGM_FIRST_EXT_TOO_LATE', '2020-10-01', {'year': '2023','isFirstAgm': False, 'extReqForAgmYear': False, 'prevAgmRefDate':'2022-06-01'},
          HTTPStatus.BAD_REQUEST, 'Allotted period to request extension has expired.'),
         ('SUCCESS_SUBSEQUENT_AGM_MORE_EXT', '2022-10-01',
-         {'year': '2023','isFirstAgm': False, 'extReqForAgmYear': True, 'prevAgmRefDate':'2023-06-01', 'expireDateCurrExt':'2024-03-01'}, None, None),
+         {'year': '2023','isFirstAgm': False, 'extReqForAgmYear': True, 'prevAgmRefDate':'2023-06-01', 'expireDateCurrExt':'2024-05-01', 'totalApprovedExt': 12, 'extensionDuration': 1},
+         None, None),
         ('FAIL_SUBSEQUENT_AGM_MORE_EXT_TOO_LATE', '2022-10-01',
          {'year': '2023','isFirstAgm': False, 'extReqForAgmYear': True, 'prevAgmRefDate':'2023-06-01', 'expireDateCurrExt':'2023-12-01'},
           HTTPStatus.BAD_REQUEST, 'Allotted period to request extension has expired.'),
