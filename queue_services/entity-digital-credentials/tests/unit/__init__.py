0d61a2aa400632c5ab5b4c29ce96b67165ba8183
Akiff Manji <amanji@petridish.dev>
2023-11-28 10:18:56 -0800

    Feature digital credentials (#2334)
    * feat: digital credentials helper tests
    
    Signed-off-by: Akiff Manji <amanji@petridish.dev>
    
    * fix: code smells
    
    Signed-off-by: Akiff Manji <amanji@petridish.dev>
    
    * fix: code smells
    
    Signed-off-by: Akiff Manji <amanji@petridish.dev>
    
    ---------
    
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

--- queue_services/entity-digital-credentials/tests/unit/__init__.py
+++ queue_services/entity-digital-credentials/tests/unit/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 """The Unit Tests and the helper routines."""
 
-from legal_api.models import Business, Filing
+from legal_api.models import Business, DCConnection, DCDefinition, DCIssuedCredential, Filing
 from sqlalchemy_continuum import versioning_manager
 
 
@@ -47,3 +47,56 @@ def create_filing(session,  business_id=None,
 
     filing.save()
     return filing
+
+
+def create_dc_definition():
+    """Create new dc_definition object."""
+    definition = DCDefinition(
+        credential_type=DCDefinition.CredentialType.business.name,
+        schema_name='test_business_schema',
+        schema_version='1.0.0',
+        schema_id='test_schema_id',
+        credential_definition_id='test_credential_definition_id'
+    )
+    definition.save()
+    return definition
+
+
+def create_dc_connection(business, is_active=False):
+    """Create new dc_connection object."""
+    connection = DCConnection(
+        connection_id='0d94e18b-3a52-4122-8adf-33e2ccff681f',
+        invitation_url="""http://192.168.65.3:8020?c_i=eyJAdHlwZSI6ICJodHRwczovL2RpZGNvbW0ub3JnL2Nvbm5lY3Rpb
+25zLzEuMC9pbnZpdGF0aW9uIiwgIkBpZCI6ICIyZjU1M2JkZS01YWJlLTRkZDctODIwZi1mNWQ2Mjc1OWQxODgi
+LCAicmVjaXBpZW50S2V5cyI6IFsiMkFHSjVrRDlVYU45OVpSeUFHZVZKNDkxclZhNzZwZGZYdkxXZkFyc2lKWjY
+iXSwgImxhYmVsIjogImZhYmVyLmFnZW50IiwgInNlcnZpY2VFbmRwb2ludCI6ICJodHRwOi8vMTkyLjE2OC42NS4zOjgwMjAifQ==""",
+        is_active=is_active,
+        connection_state='active' if is_active else 'invitation',
+        business_id=business.id
+    )
+    connection.save()
+    return connection
+
+
+def create_dc_issued_credential(business=None,
+                                credential_exchange_id='test_credential_exchange_id',
+                                credential_revocation_id='123',
+                                revocation_registry_id='123',
+                                is_issued=True, is_revoked=False):
+    """Create new dc_issued_credential object."""
+    if not business:
+        identifier = 'FM1234567'
+        business = create_business(identifier)
+    definition = create_dc_definition()
+    connection = create_dc_connection(business, is_active=True)
+    issued_credential = DCIssuedCredential(
+        dc_definition_id=definition.id,
+        dc_connection_id=connection.id,
+        credential_exchange_id=credential_exchange_id,
+        credential_revocation_id=credential_revocation_id,
+        revocation_registry_id=revocation_registry_id,
+        is_issued=is_issued,
+        is_revoked=is_revoked
+    )
+    issued_credential.save()
+    return issued_credential
