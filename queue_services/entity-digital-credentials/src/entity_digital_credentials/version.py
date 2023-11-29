9c3b7758f465e482b0173e5918e0b7b9c4eb8643
Karim El Jazzar <122301442+JazzarKarim@users.noreply.github.com>
2023-11-29 11:58:57 -0800

    bump version numbers for release 19.5b (#2338)

--- queue_services/entity-digital-credentials/src/entity_digital_credentials/version.py
+++ queue_services/entity-digital-credentials/src/entity_digital_credentials/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '1.0.0'  # pylint: disable=invalid-name
+__version__ = '2.96.0'  # pylint: disable=invalid-name
