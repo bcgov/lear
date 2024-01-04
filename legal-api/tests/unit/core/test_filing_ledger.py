e65de482df64fabea82e6887eda4a47ee8f06a1f
Kevin Zhang <kevinzhang940420@gmail.com>
2024-01-03 16:01:51 -0800

    add display_ledger test

--- legal-api/tests/unit/core/test_filing_ledger.py
+++ legal-api/tests/unit/core/test_filing_ledger.py
@@ -105,3 +105,10 @@ def test_common_ledger_items(session):
         factory_completed_filing(business, filing, filing_date=founding_date + datedelta.datedelta(months=1))
     common_ledger_items = CoreFiling.common_ledger_items(identifier, completed_filing)
     assert common_ledger_items['documentsLink'] is not None
+    assert common_ledger_items['displayLedger'] is True
+
+    filing['filing']['header']['name'] = 'adminFreeze'
+    completed_filing = \
+        factory_completed_filing(business, filing, filing_date=founding_date + datedelta.datedelta(months=1), filing_type='adminFreeze')
+    common_ledger_items = CoreFiling.common_ledger_items(identifier, completed_filing)
+    assert common_ledger_items['displayLedger'] is False
