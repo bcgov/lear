00296c9bc3352064ae2f3aca56b55aa90b0d303f
PaulGarewal <paul.garewal@gov.bc.ca>
2024-01-22 10:44:51 -0800

    fixed lint issue

--- legal-api/src/legal_api/reports/report.py
+++ legal-api/src/legal_api/reports/report.py
@@ -712,7 +712,7 @@ class Report:  # pylint: disable=too-few-public-methods, too-many-lines
 
     def _format_certificate_of_amalgamation_data(self, filing):
         amalgamation = filing['amalgamationApplication']
-        
+
         filing['nameRequest'] = amalgamation.get('nameRequest', {})
         filing['amalgamatingBusinesses'] = amalgamation.get('amalgamatingBusinesses', [])
 
@@ -1117,6 +1117,8 @@ class Report:  # pylint: disable=too-few-public-methods, too-many-lines
 
         # Appears in the Description section of the PDF Document Properties as Subject.
         if self._report_key == 'noticeOfArticles':
+            print("******")
+            print()
             filing['meta_subject'] = '{} ({})'.format(self._business.legal_name, self._business.identifier)
         else:
             legal_name = self._filing.filing_json['filing'].get('business', {}).get('legalName', 'NA')
