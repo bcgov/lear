499c60da3816261a7d80c67a39ab667687b5eba2
vysakh.menon <vysakh.menon@aot-technologies.com>
2024-02-06 15:39:37 -0800

    19649 filer - short form amalgamation

--- queue_services/entity-filer/tests/unit/__init__.py
+++ queue_services/entity-filer/tests/unit/__init__.py
@@ -19,7 +19,7 @@ from freezegun import freeze_time
 from sqlalchemy_continuum import versioning_manager
 from legal_api.utils.datetime import datetime, timezone
 from tests import EPOCH_DATETIME, FROZEN_DATETIME
-from legal_api.models import db, Filing
+from legal_api.models import db, Filing, ShareClass, ShareSeries
 from legal_api.models.colin_event_id import ColinEventId
 
 AR_FILING = {
@@ -528,6 +528,36 @@ def create_party_role(business, party, roles, appointment_date):
         business.party_roles.append(party_role)
 
     return business
+
+
+
+def create_share_class(business, no_of_shares=1, no_of_series_in_each_share=2):
+    """Create a new share class and associated series."""
+    for i in range(no_of_shares):
+        share_class = ShareClass(
+            name=f'{business.identifier} Share Class {i}',
+            priority=1,
+            max_share_flag=True,
+            max_shares=100,
+            par_value_flag=True,
+            par_value=10,
+            currency='CAD',
+            special_rights_flag=False
+        )
+
+        share_class.series = []
+        for j in range(no_of_series_in_each_share):
+            share_series = ShareSeries(
+                name=f'{business.identifier} Share {i} Series {j}',
+                priority=1,
+                max_share_flag=True,
+                max_shares=50,
+                special_rights_flag=False
+            )
+            share_class.series.append(share_series)
+
+    business.share_classes.append(share_class)
+    business.save()
 
 
 def factory_completed_filing(business, data_dict, filing_date=FROZEN_DATETIME, payment_token=None, colin_id=None):
