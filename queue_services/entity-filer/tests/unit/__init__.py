de5e17b7c6a3cfee465ab95a7131f4803cdc79e1
Vysakh Menon <vysakh.menon@aot-technologies.com>
2024-02-07 13:12:08 -0800

    19649 filer - short form amalgamation (#2445)

--- queue_services/entity-filer/tests/unit/__init__.py
+++ queue_services/entity-filer/tests/unit/__init__.py
@@ -23,6 +23,8 @@ from freezegun import freeze_time
 
 from entity_filer.utils.datetime import datetime, timezone
 from tests import EPOCH_DATETIME, FROZEN_DATETIME
+from legal_api.models import db, Filing, ShareClass, ShareSeries
+from legal_api.models.colin_event_id import ColinEventId
 
 AR_FILING = {
     "filing": {
@@ -564,6 +566,66 @@ def create_entity_role(business, party, roles, appointment_date):
         business.entity_roles.append(party_role)
 
     return business
+
+
+
+def create_share_class(business, no_of_shares=1, no_of_series_in_each_share=2):
+    """Create a new share class and associated series."""
+    for i in range(no_of_shares):
+        share_class = ShareClass(
+            name=f'{business.identifier} Share Class {i}',
+            priority=1,
+            max_share_flag=True,
+            max_shares=100,
+            par_value_flag=True,
+            par_value=10,
+            currency='CAD',
+            special_rights_flag=False
+        )
+
+        share_class.series = []
+        for j in range(no_of_series_in_each_share):
+            share_series = ShareSeries(
+                name=f'{business.identifier} Share {i} Series {j}',
+                priority=1,
+                max_share_flag=True,
+                max_shares=50,
+                special_rights_flag=False
+            )
+            share_class.series.append(share_series)
+
+    business.share_classes.append(share_class)
+    business.save()
+
+
+
+def create_share_class(business, no_of_shares=1, no_of_series_in_each_share=2):
+    """Create a new share class and associated series."""
+    for i in range(no_of_shares):
+        share_class = ShareClass(
+            name=f'{business.identifier} Share Class {i}',
+            priority=1,
+            max_share_flag=True,
+            max_shares=100,
+            par_value_flag=True,
+            par_value=10,
+            currency='CAD',
+            special_rights_flag=False
+        )
+
+        share_class.series = []
+        for j in range(no_of_series_in_each_share):
+            share_series = ShareSeries(
+                name=f'{business.identifier} Share {i} Series {j}',
+                priority=1,
+                max_share_flag=True,
+                max_shares=50,
+                special_rights_flag=False
+            )
+            share_class.series.append(share_series)
+
+    business.share_classes.append(share_class)
+    business.save()
 
 
 def factory_completed_filing(
