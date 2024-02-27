3c04dbb9719ecac3b80ea7337630c68b4fe7757e
Vysakh Menon <vysakh.menon@aot-technologies.com>
2024-02-27 09:58:37 -0800

    20034 bump up version 2.106.0 (#2477)

--- legal-api/src/legal_api/version.py
+++ legal-api/src/legal_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.105.0'  # pylint: disable=invalid-name
+__version__ = '2.106.0'  # pylint: disable=invalid-name
