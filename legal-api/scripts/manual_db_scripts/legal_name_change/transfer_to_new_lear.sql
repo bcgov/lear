4e38ee48b5a1fb7373a7e0e78ba79b4a3b377831
tshyun24 <shaoyun.tong@gov.bc.ca>
2024-02-27 19:28:53 -0800

    update table name

--- legal-api/scripts/manual_db_scripts/legal_name_change/transfer_to_new_lear.sql
+++ legal-api/scripts/manual_db_scripts/legal_name_change/transfer_to_new_lear.sql
@@ -924,10 +924,10 @@ SELECT stg.filing_id,
 FROM public.sent_to_gazette stg;
 
 
--- amalgamation -> amalgamation
+-- amalgamation -> amalgamations
 CREATE CAST (varchar AS amalgamation_type) WITH INOUT AS IMPLICIT;
 
-transfer public.amalgamation from lear_old using
+transfer public.amalgamations from lear_old using
 SELECT  id,
         business_id as legal_entity_id,
         filing_id,
@@ -937,17 +937,17 @@ SELECT  id,
 FROM public.amalgamation;
 
 
--- amalgamating_business -> amalgamating_business
+-- amalgamating_business -> amalgamating_businesses
 CREATE CAST (varchar AS amalgamating_business_role) WITH INOUT AS IMPLICIT;
 
-transfer public.amalgamating_business from lear_old using
+transfer public.amalgamating_businesses from lear_old using
 SELECT id,
        business_id as legal_entity_id,
        amalgamation_id,
        foreign_jurisdiction,
        foreign_jurisdiction_region,
        foreign_name,
-       foreign_corp_num,
+       foreign_corp_num as foreign_identifier,
        role :: amalgamating_business_role
 FROM public.amalgamating_business;
 
