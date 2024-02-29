67243b13527d514fc2f6612205fbe9b76314da6f
Kevin Zhang <kevinzhang940420@gmail.com>
2024-02-29 09:00:57 -0800

    update appointment_date

--- legal-api/src/legal_api/models/alternate_name.py
+++ legal-api/src/legal_api/models/alternate_name.py
@@ -187,7 +187,7 @@ class AlternateName(Versioned, db.Model, BusinessCommon):
         """Return if owner data."""
         json = {
             "officer": {},
-            "roles": [{"appointmentDate": "2000-01-03", "cessationDate": None, "roleType": "Proprietor"}],
+            "roles": [{"appointmentDate": self.startDate, "cessationDate": None, "roleType": "Proprietor"}],
         }
 
         if self.is_owned_by_legal_entity_person:
