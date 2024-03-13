c0dec235100670f6d99fa7058c61bcba7108ea61
JazzarKarim <karim.jazzar@gov.bc.ca>
2024-03-13 15:34:08 -0700

    fixed linting

--- legal-api/src/legal_api/resources/v2/business/business_filings/business_filings.py
+++ legal-api/src/legal_api/resources/v2/business/business_filings/business_filings.py
@@ -609,9 +609,9 @@ class ListFilingResource:
             else:
                 filing = Filing()
                 if business.identifier.startswith("FM"):
-                  filing.alternate_name_id = business.id
+                    filing.alternate_name_id = business.id
                 else:
-                  filing.legal_entity_id = business.id  
+                    filing.legal_entity_id = business.id  
 
         try:
             filing.submitter_id = user.id
