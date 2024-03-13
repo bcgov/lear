168432f5441b0a32a28886c83e09d7312f41b114
Karim El Jazzar <122301442+JazzarKarim@users.noreply.github.com>
2024-03-13 16:08:15 -0700

    fixed change of registration (#2514)
    * fixed change of registration
    
    * fixed linting
    
    * fixed lint again
    
    * refactored code
    
    * updated in response to Hongjing's comment

--- legal-api/src/legal_api/resources/v2/business/business_filings/business_filings.py
+++ legal-api/src/legal_api/resources/v2/business/business_filings/business_filings.py
@@ -608,7 +608,10 @@ class ListFilingResource:
                 filing = rv[1]
             else:
                 filing = Filing()
-                filing.legal_entity_id = business.id
+                if business.is_alternate_name_entity:
+                    filing.alternate_name_id = business.id
+                else:
+                    filing.legal_entity_id = business.id
 
         try:
             filing.submitter_id = user.id
