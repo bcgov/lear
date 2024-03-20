3cafef665dd5e8a92f7d96042e2f5de66bf21b1d
leodube-aot <leo.dube@aot-technologies.com>
2024-03-20 10:53:11 -0700

    Get unit tests running

--- python/common/business-registry-model/src/business_model_migrations/versions/3d8eb786f4c2_add_sequences.py
+++ python/common/business-registry-model/src/business_model_migrations/versions/3d8eb786f4c2_add_sequences.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = "3d8eb786f4c2"
-down_revision = "2b08acebfed5"
+revision = '3d8eb786f4c2'
+down_revision = '2b08acebfed5'
 branch_labels = None
 depends_on = None
 
