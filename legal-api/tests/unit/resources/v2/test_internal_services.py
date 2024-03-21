f003134fed9f1de2e958ca39c4e457d3827011be
ketaki-deodhar <ketaki.deodhar@gov.bc.ca>
2024-03-21 15:02:39 -0700

    20016 - update unit tests for bnmove

--- legal-api/tests/unit/resources/v2/test_internal_services.py
+++ legal-api/tests/unit/resources/v2/test_internal_services.py
@@ -18,17 +18,18 @@ from unittest.mock import patch
 
 import pytest
 
-from legal_api.models import LegalEntity, UserRoles
+from legal_api.models import AlternateName, LegalEntity, UserRoles
 from legal_api.resources.v2 import internal_services
 from legal_api.resources.v2.internal_services import ListFilingResource
-from tests.unit.models import factory_legal_entity
+from legal_api.utils.datetime import datetime
+from tests.unit.models import factory_alternate_name, factory_legal_entity
 from tests.unit.services.utils import create_header
 
 
-def test_update_bn_move(session, client, jwt):
+def test_update_bn_move_GP(session, client, jwt):
     """Assert that the endpoint updates tax_id."""
     identifier = "FM0000001"
-    legal_entity = factory_legal_entity(identifier, entity_type=LegalEntity.EntityTypes.SOLE_PROP.value)
+    legal_entity = factory_legal_entity(identifier, entity_type=LegalEntity.EntityTypes.PARTNERSHIP.value)
     legal_entity.tax_id = "993775204BC0001"
     legal_entity.save()
 
@@ -44,6 +45,39 @@ def test_update_bn_move(session, client, jwt):
             )
             assert rv.status_code == HTTPStatus.OK
             assert LegalEntity.find_by_tax_id(new_bn)
+
+
+def test_update_bn_move_SP(session, client, jwt):
+    """Assert that the endpoint updates bn15."""
+    identifier = "FM0000001"
+    owner_identifier = "BC0000001"
+    owner_legal_type = "BEN"
+
+    legal_entity = factory_legal_entity(entity_type=owner_legal_type, identifier=owner_identifier)
+    alternate_name = factory_alternate_name(
+        identifier=identifier,
+        name="SP TESTING1212",
+        name_type=AlternateName.NameType.DBA,
+        bn15="111111100BC1111",
+        start_date=datetime.utcnow(),
+        legal_entity_id=legal_entity.id,
+    )
+    alternate_name.bn15 = "993775204BC0001"
+    alternate_name.save()
+    legal_entity.alternate_names.append(alternate_name)
+
+    new_bn = "993777399BC0001"
+    with patch.object(internal_services, "publish_event"):
+        with patch.object(
+            ListFilingResource, "create_invoice", return_value=({"isPaymentActionRequired": False}, HTTPStatus.CREATED)
+        ):
+            rv = client.post(
+                "/api/v2/internal/bnmove",
+                headers=create_header(jwt, [UserRoles.system], identifier),
+                json={"oldBn": alternate_name.bn15, "newBn": new_bn},
+            )
+            assert rv.status_code == HTTPStatus.OK
+            assert AlternateName.find_by_tax_id(new_bn)
 
 
 @pytest.mark.parametrize(
