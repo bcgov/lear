174553ebea9435b23ae8cacf122dd5e7f2b35130
James <james.paolo.p.garcia@gmail.com>
2024-03-25 18:47:22 -0700

    Fixed lindt error

--- legal-api/src/legal_api/services/authz.py
+++ legal-api/src/legal_api/services/authz.py
@@ -344,7 +344,7 @@ def get_allowable_filings_dict():
                     'legalTypes': ['BC', 'BEN', 'ULC', 'CC'],
                     'blockerChecks': {
                         'business': [BusinessBlocker.DEFAULT],
-                        'invalidStateFilings': ['restoration.limitedRestoration', 
+                        'invalidStateFilings': ['restoration.limitedRestoration',
                                                 'restoration.limitedRestorationExtension']
                     }
                 },
