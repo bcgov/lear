4c9222996ac010ef6db8f21fbbadbda4c7a3c875
Steven Chen <steven.chen@gov.bc.ca>
2024-03-27 09:28:23 -0700

    Gazette Messaging for Incorporation Application Filings#6870
    Signed-off-by: Steven Chen <steven.chen@gov.bc.ca>

--- jobs/sftp-gazette/config.py
+++ jobs/sftp-gazette/config.py
@@ -11,22 +11,31 @@ CONFIGURATION = {
     'default': 'config.ProdConfig'
 }
 
+
 class Config(object):
-    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    APP_FILE = os.getenv('APP_FILE', '')
+    PROJECT_ROOT = os.getcwd()
     SENDER_EMAIL = os.getenv('SENDER_EMAIL', '')
     ERROR_EMAIL_RECIPIENTS = os.getenv('ERROR_EMAIL_RECIPIENTS', '')
-    REPORT_RECIPIENTS = os.getenv('REPORT_RECIPIENTS', '')
+    SFTP_GAZETTE_RECIPIENTS = os.getenv('SFTP_GAZETTE_RECIPIENTS', '')
     EMAIL_SMTP = os.getenv('EMAIL_SMTP', '')
     ENVIRONMENT = os.getenv('ENVIRONMENT', '')
-    MONTH_REPORT_DATES = os.getenv('MONTH_REPORT_DATES', '')
-
+    SFTP_ARCHIVE_DIRECTORY = os.getenv('SFTP_ARCHIVE_DIRECTORY', '/opt/app-root/archive/')
+    TEMPLATE_PATH = os.getenv('TEMPLATE_PATH', None)
+   
     # POSTGRESQL
     PG_USER = os.getenv('PG_USER', '')
     PG_PASSWORD = os.getenv('PG_PASSWORD', '')
     PG_NAME = os.getenv('PG_DB_NAME', '')
     PG_HOST = os.getenv('PG_HOST', '')
     PG_PORT = os.getenv('PG_PORT', '5432')
+    SFTP_HOST = os.getenv('SFTP_HOST', 'drive.kp.gov.bc.ca')
+    SFTP_PORT = os.getenv('SFTP_PORT', 22)
+    SFTP_VERIFY_HOST = os.getenv('SFTP_VERIFY_HOST')
+    SFTP_HOST_KEY = os.getenv('SFTP_HOST_KEY', '')
+    SFTP_ARCHIVE_DIRECTORY = os.getenv('SFTP_ARCHIVE_DIRECTORY', '/opt/app-root/archieve/')
+    BCREG_FTP_PRIVATE_KEY = os.getenv('BCREG_FTP_PRIVATE_KEY', '')
+    SFTP_USERNAME = os.getenv('SFTP_USERNAME', 'foo')
+    BCREG_FTP_PRIVATE_KEY_PASSPHRASE = os.getenv('BCREG_FTP_PRIVATE_KEY_PASSPHRASE', '')
     SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{host}:{port}/{name}'.format(
         user=PG_USER,
         password=PG_PASSWORD,
@@ -34,4 +43,3 @@ class Config(object):
         port=int(PG_PORT),
         name=PG_NAME,
     )
-
--- jobs/sftp-gazette/sftpgazette.py
+++ jobs/sftp-gazette/sftpgazette.py
@@ -26,6 +26,7 @@ setup_logging(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'logging.
 # This script helps with the automated processing of Jupyter Notebooks via
 # papermill (https://github.com/nteract/papermill/)
 
+
 def create_app(config=Config):
     """Create app."""
     app = Flask(__name__)
@@ -36,68 +37,103 @@ def create_app(config=Config):
 
     return app
 
+
 def findfiles(directory, pattern):
     """Find files matched."""
     for filename in os.listdir(directory):
         if fnmatch.fnmatch(filename.lower(), pattern):
             yield os.path.join(directory, filename)
 
+
 def archive_files(data_dir, dest_dir):
-    for file in os.listdir(data_dir): 
-        shutil.move(data_dir + file, dest_dir + file)      
-    logging.info("Data files were archived successfully!")           
+    for file in os.listdir(data_dir):
+        shutil.move(data_dir + file, dest_dir + file)
+    logging.info("Data files were archived successfully!")
+
 
 def send_email(note_book, emailtype, errormessage):
     """Send email for results."""
-    message = MIMEMultipart()
-    date = datetime.strftime(datetime.now(), '%Y%m%d')
-    
-    ext = ''
-    if not os.getenv('ENVIRONMENT', '') == 'prod':
-        ext = ' on ' + os.getenv('ENVIRONMENT', '')
+    date = datetime.strftime(datetime.now(), '%Y-%m-%d')
 
-    subject = "SFTP Gazette Error Notification from LEAR for processing '" \
-        + note_book + "' on " + date + ext
-    recipients = os.getenv('ERROR_EMAIL_RECIPIENTS', '')
-    message.attach(MIMEText('ERROR!!! \n' + errormessage, 'plain'))
-        
+    ext = ''
+    if not Config.ENVIRONMENT == 'prod':
+        ext = ' on ' + Config.ENVIRONMENT
+
+    message = MIMEMultipart()
+
+    if emailtype == 'ERROR':
+        subject = "SFTP Gazette Error Notification from LEAR for processing '" \
+            + note_book + "' on " + date + ext
+        recipients = Config.ERROR_EMAIL_RECIPIENTS
+        message.attach(MIMEText('ERROR!!! \n' + errormessage, 'plain'))
+    else:
+        subject = 'SFTP Gazette Report ' + note_book + "' on " + date + ext
+        filename = 'sent_to_gazette.xml'
+        recipients = Config.SFTP_GAZETTE_RECIPIENTS
+        # Add body to email
+        message.attach(MIMEText('Please see the attachment(s).', 'plain'))
+
+        # Open file in binary mode
+        with open(os.path.join(os.getcwd(), r'data/')+filename, 'rb') as attachment:
+            # Add file as application/octet-stream
+            # Email client can usually download this automatically as attachment
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(attachment.read())
+
+        # Encode file in ASCII characters to send by email
+        encoders.encode_base64(part)
+
+        # Add header as key/value pair to attachment part
+        part.add_header(
+            'Content-Disposition',
+            f'attachment; filename= {filename}',
+        )
+
+        # Add attachment to message and convert message to string
+        message.attach(part)
+
     message['Subject'] = subject
-    server = smtplib.SMTP(os.getenv('EMAIL_SMTP', ''))
+    server = smtplib.SMTP(Config.EMAIL_SMTP)
     email_list = recipients.strip('][').split(', ')
     logging.info('Email recipients list is: %s', email_list)
-    server.sendmail(os.getenv('SENDER_EMAIL', ''), email_list, message.as_string())
+    server.sendmail(Config.SENDER_EMAIL, email_list, message.as_string())
     logging.info("Email with subject \'%s\' has been sent successfully!", subject)
     server.quit()
 
 
-def processnotebooks(notebookdirectory):
+def processnotebooks(notebookdirectory, data_dir):
     """Process data."""
-    status = False
-         
-    logging.info('Start processing directory: %s', notebookdirectory)        
-    data_dir = os.getenv('DATA_DIR', '/opt/app-root/data') 
-    dest_dir = os.getenv('SFTP_ARCHIVE_DIRECTORY', '/opt/app-root/archive/')            
-    
-    try: 
-        pm.execute_notebook(os.path.join(notebookdirectory, "generate_files.ipynb") , data_dir +'temp.ipynb', parameters=None)   
-        os.remove(data_dir+'temp.ipynb')   
 
-        FtpProcessor.process_ftp(data_dir)                        
-                    
-        pm.execute_notebook(os.path.join(notebookdirectory, "update_database.ipynb") , data_dir +'temp.ipynb', parameters=None)   
-        os.remove(data_dir+'temp.ipynb')      
+    logging.info('Start processing directory: %s', notebookdirectory)
 
-        archive_files (data_dir, dest_dir)          
-        status = True                    
-    except Exception:            
+    try:
+        pm.execute_notebook(os.path.join(notebookdirectory, "generate_files.ipynb"),
+                            data_dir + 'temp.ipynb', parameters=None)
+        os.remove(data_dir+'temp.ipynb')
+
+        # FtpProcessor.process_ftp(data_dir)
+
+        pm.execute_notebook(os.path.join(notebookdirectory, "update_database.ipynb"),
+                            data_dir + 'temp.ipynb', parameters=None)
+        send_email(notebookdirectory, '', '')
+        os.remove(data_dir+'temp.ipynb')
+
+    except Exception:
         logging.exception('Error processing notebook %s.', notebookdirectory)
-        send_email(notebookdirectory, 'ERROR', traceback.format_exc())       
-    return status;    
-   
+        send_email(notebookdirectory, 'ERROR', traceback.format_exc())
+
 
 if __name__ == '__main__':
     start_time = datetime.utcnow()
-    processnotebooks('notebook')
+
+    temp_dir = os.path.join(os.getcwd(), r'data/')
+    # Check if the subfolders for notebooks exist, and create them if they don't
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+
+    for subdir in ['weekly']:
+        processnotebooks(subdir, temp_dir)
+
     end_time = datetime.utcnow()
     logging.info('job - jupyter notebook report completed in: %s', end_time - start_time)
     sys.exit()
