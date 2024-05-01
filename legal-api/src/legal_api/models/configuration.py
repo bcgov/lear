f6381878e65912443cefcaa973b56152de93cd8e
leodube-aot <leo.dube@aot-technologies.com>
2024-05-01 10:42:58 -0700

    Update description

--- legal-api/src/legal_api/models/configuration.py
+++ legal-api/src/legal_api/models/configuration.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""This module holds data for aliases."""
+"""This module holds data for configurations."""
 from __future__ import annotations
 from croniter import croniter
 from sqlalchemy import event
