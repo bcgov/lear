01bd4f14b148aa4e1f4acf94289a134146d2a51b
argush3 <argus@highwaythreesolutions.com>
2024-05-13 14:45:01 -0700

    Example PUT configuration structure

--- legal-api/src/legal_api/resources/v2/configuration.py
+++ legal-api/src/legal_api/resources/v2/configuration.py
@@ -16,6 +16,7 @@ from http import HTTPStatus
 
 from flask import Blueprint, jsonify, request
 from flask_cors import cross_origin
+from sqlalchemy import any_
 
 from legal_api.models import Configuration, UserRoles, db
 from legal_api.services.utils import get_duplicate_keys
@@ -48,30 +49,16 @@ def update_configurations():
         return {'message': 'Request body cannot be blank'}, HTTPStatus.BAD_REQUEST
 
     configurations = json_input.get('configurations', [])
-    if not configurations:
-        return {'message': 'Configurations list cannot be empty'}, HTTPStatus.BAD_REQUEST
+    error_message = validate_configurations(configurations)
+    if error_message:
+        return {'message': error_message}, HTTPStatus.BAD_REQUEST
 
-    duplicated_keys = get_duplicate_keys(data_list=configurations, key_attr='name')
-    if len(duplicated_keys) > 0:
-        duplicated_keys_str = ', '.join(duplicated_keys)
-        return {'message': f'{duplicated_keys_str} are duplicated.'}, HTTPStatus.BAD_REQUEST
-
-    numeric_names = {'NUM_DISSOLUTIONS_ALLOWED', 'MAX_DISSOLUTIONS_ALLOWED'}
     response = []
-
     try:
         for config_data in configurations:
             name = config_data.get('name')
             value = config_data.get('value')
-
-            if name in numeric_names:
-                if not is_valid_numeric_value(name, value):
-                    raise ValueError(f'Invalid value for {name}.')
-
             config = Configuration.find_by_name(name)
-            if not config:
-                raise ValueError(f'{name} is an invalid key.')
-
             config.val = str(value)
             db.session.add(config)
             response.append(config.json)
@@ -81,15 +68,90 @@ def update_configurations():
         db.session.rollback()
         return {'message': str(e)}, HTTPStatus.BAD_REQUEST
 
-    return {'results': response}, HTTPStatus.OK
+    return {'configurations': response}, HTTPStatus.OK
 
 
-def is_valid_numeric_value(name, value):
-    """Check if the numeric value is valid."""
-    if name == 'NUM_DISSOLUTIONS_ALLOWED':
-        max_value = Configuration.find_by_name('MAX_DISSOLUTIONS_ALLOWED').val
-        return int(value) <= int(max_value)
-    elif name == 'MAX_DISSOLUTIONS_ALLOWED':
-        min_value = Configuration.find_by_name('NUM_DISSOLUTIONS_ALLOWED').val
-        return int(value) >= int(min_value)
-    return False
+
+def validate_configurations(configurations):
+    """Validate the configurations before updating."""
+    if not configurations:
+        return 'Configurations list cannot be empty'
+
+    # Extract names from the requested configuration updates
+    names = [config['name'] for config in configurations]
+    if len(names) != len(set(names)):
+        return 'Duplicate names error.'
+
+    if err := validate_invalid_names(names):
+        return err
+
+    if err := validate_data_types(configurations):
+        return err
+
+    if err := validate_dissolutions_config(configurations):
+        return err
+
+    return None  # No errors found
+
+
+def validate_data_types(configurations):
+    """Validate the data types of the configurations."""
+
+    try:
+        for config in configurations:
+            name = config.get('name')
+            value = config.get('value')
+            Configuration.validate_configuration_value(name, value)
+    except ValueError as e:
+        return str(e)
+
+    return None
+
+
+def validate_invalid_names(names):
+    """Validate if there are any invalid names in configurations to be updated."""
+
+    # Query the database for these names
+    existing_configs = Configuration.query.filter(Configuration.name.ilike(any_(names))).all()
+
+    # Check if the number of unique names in the request matches the number of names found in the database
+    if len(existing_configs) != len(set(names)):
+        return 'Invalid name error.'
+
+    return None
+
+
+def validate_dissolutions_config(configurations):
+    """Validate the dissolutions configuration."""
+
+    num_dissolutions_match = find_config_by_name(configurations, 'NUM_DISSOLUTIONS_ALLOWED')
+    max_dissolutions_match = find_config_by_name(configurations, 'MAX_DISSOLUTIONS_ALLOWED')
+
+    # don't need to validate
+    if not num_dissolutions_match and not max_dissolutions_match:
+        return None
+
+    if num_dissolutions_match:
+        num_dissolutions_allowed = int(num_dissolutions_match.get('value'))
+    else:
+        num_dissolutions_allowed = Configuration.find_by_name('NUM_DISSOLUTIONS_ALLOWED').val
+    num_dissolutions_allowed = int(num_dissolutions_allowed)
+
+
+    if max_dissolutions_match:
+        max_dissolutions_allowed = int(max_dissolutions_match.get('value'))
+    else:
+        max_dissolutions_allowed = Configuration.find_by_name('MAX_DISSOLUTIONS_ALLOWED').val
+    max_dissolutions_allowed = int(max_dissolutions_allowed)
+
+    if num_dissolutions_allowed > max_dissolutions_allowed:
+        return 'NUM_DISSOLUTIONS_ALLOWED is greater than MAX_DISSOLUTIONS_ALLOWED.'
+
+    return None
+
+
+
+def find_config_by_name(configurations, name):
+    """Search for a specific configuration by name from configuration payload."""
+    return next((config for config in configurations if config['name'] == name), None)
+
