75765f63fbbb005b328b84278488a75c8baaae32
Argus Chiu <argus@daxiom.com>
2024-05-17 15:58:44 -0700

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0MTc5MQ/spectral.js?branch_slug=main&token=d7659600-7823-4746-b1e4-0d25a8bba915";
+export default { extends: ruleset };
