02f77425b8e75e39ec4c4e95f3d10d4c8ac838df
leodube-aot <leo.dube@aot-technologies.com>
2024-05-24 11:12:36 -0500

    initial quick pass at unit tests

--- jobs/involuntary-dissolutions/tests/unit/__init__.py
+++ jobs/involuntary-dissolutions/tests/unit/__init__.py
@@ -1,3 +1,45 @@
+import datetime
+from legal_api.models import (
+    Batch,
+    BatchProcessing,
+    Business,
+)
+
+EPOCH_DATETIME = datetime.datetime.utcfromtimestamp(0).replace(tzinfo=datetime.timezone.utc)
+FROZEN_DATETIME = datetime.datetime(2001, 8, 5, 7, 7, 58, 272362).replace(tzinfo=datetime.timezone.utc)
+
+def factory_business(identifier,
+                     founding_date=EPOCH_DATETIME,
+                     last_ar_date=None,
+                     entity_type=Business.LegalTypes.COOP.value,
+                     state=Business.State.ACTIVE,
+                     naics_code=None,
+                     naics_desc=None,
+                     admin_freeze=False,
+                     no_dissolution=False):
+    """Create a business entity with a versioned business."""
+    last_ar_year = None
+    if last_ar_date:
+        last_ar_year = last_ar_date.year
+    business = Business(legal_name=f'legal_name-{identifier}',
+                        founding_date=founding_date,
+                        last_ar_date=last_ar_date,
+                        last_ar_year=last_ar_year,
+                        last_ledger_timestamp=EPOCH_DATETIME,
+                        # dissolution_date=EPOCH_DATETIME,
+                        identifier=identifier,
+                        tax_id='BN123456789',
+                        fiscal_year_end_date=FROZEN_DATETIME,
+                        legal_type=entity_type,
+                        state=state,
+                        naics_code=naics_code,
+                        naics_description=naics_desc,
+                        admin_freeze=admin_freeze,
+                        no_dissolution=no_dissolution)
+
+    business.save()
+    return business
+
 def factory_batch(batch_type=Batch.BatchType.INVOLUNTARY_DISSOLUTION,
                   status=Batch.BatchStatus.HOLD,
                   size=3,
--- jobs/involuntary-dissolutions/tests/unit/test_job.py
+++ jobs/involuntary-dissolutions/tests/unit/test_job.py
@@ -1,7 +1,13 @@
 import os
 import psycopg2
-from unittest.mock import patch
 import pytest
+
+from datetime import datetime
+
+from involuntary_dissolutions import create_app, initiate_dissolution_process
+from legal_api.models import Configuration, Batch, BatchProcessing, Business
+from tests import factory_business, factory_batch, factory_batch_processing
+from unittest.mock import patch
 
 def test_connection_failed():
     status = False
@@ -36,12 +42,31 @@ def test_database_connection_succeed():
         assert status == True
 
 
-@pytest.mark.parametrize(
-    'test_name, filing, expected_msg',
-    [
-        ('test_batch_has_already_run'),
-        ('test_job_cron_valid'),
-    ]
-)
-def test_initiate_dissolution_process(mocker, app, session, test_name):
-    pass
+def test_batch_already_ran_today():
+    application = create_app()
+    with application.app_context():
+        factory_business(identifier='BC1234567', entity_type=Business.LegalTypes.COMP.value)
+
+        # first run
+        initiate_dissolution_process(application)
+        batches = Batch.find_by(batch_type=Batch.BatchType.INVOLUNTARY_DISSOLUTION)
+        assert batches.count() == 1
+
+        # second run
+        initiate_dissolution_process(application)
+        batches = Batch.find_by(batch_type=Batch.BatchType.INVOLUNTARY_DISSOLUTION)
+        assert not batches.count() > 1
+
+
+def test_batch_cron_invalid():
+    application = create_app()
+    with application.app_context():
+        factory_business(identifier='BC1234567', entity_type=Business.LegalTypes.COMP.value)
+
+        # shouldn't run on a friday
+        with patch.object(datetime, 'today', return_value=datetime.datetime(2024, 5, 24)):
+            initiate_dissolution_process(application)
+            batches = Batch.find_by(batch_type=Batch.BatchType.INVOLUNTARY_DISSOLUTION)
+            assert batches.count() == 0
+
+# TODO: test batch and batch_processing entries are created successfully for different scenarios
