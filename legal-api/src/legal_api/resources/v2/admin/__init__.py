b737e8eafb08a3826224043e5d6cbec86aa88cdc
Eason Pan <Eason.Pan@gov.bc.ca>
2024-05-24 15:43:43 -0700

    create admin module and move 3 files into it

---
+++ legal-api/src/legal_api/resources/v2/admin/__init__.py
@@ -0,0 +1,20 @@
+# Copyright © 2021 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Admin Entity End-Points.
+
+Provides all admin services.
+"""
+from flask import Blueprint
+
+bp_admin = Blueprint('ADMIN', __name__, url_prefix='api/v2/admin')
