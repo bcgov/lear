8c64af72f99b48cf11cf0b5f5f04514abecd0524
leodube-aot <leo.dube@aot-technologies.com>
2024-05-31 05:07:40 -0600

    Remove tests to include in future PR

--- jobs/involuntary-dissolutions/tests/unit/test_job.py
+++ jobs/involuntary-dissolutions/tests/unit/test_job.py
@@ -1,13 +1,5 @@
 import os
 import psycopg2
-import pytest
-
-from datetime import datetime
-
-from involuntary_dissolutions import create_app, run
-from legal_api.models import Configuration, Batch, BatchProcessing, Business
-from tests import factory_business, factory_batch, factory_batch_processing
-from unittest.mock import patch
 
 def test_connection_failed():
     status = False
@@ -40,33 +32,3 @@ def test_database_connection_succeed():
         status = False
     finally:
         assert status == True
-
-
-def test_batch_already_ran_today():
-    application = create_app()
-    with application.app_context():
-        factory_business(identifier='BC1234567', entity_type=Business.LegalTypes.COMP.value)
-
-        # first run
-        run(application)
-        batches = Batch.find_by(batch_type=Batch.BatchType.INVOLUNTARY_DISSOLUTION)
-        assert batches.count() == 1
-
-        # second run
-        run(application)
-        batches = Batch.find_by(batch_type=Batch.BatchType.INVOLUNTARY_DISSOLUTION)
-        assert not batches.count() > 1
-
-
-def test_batch_cron_invalid():
-    application = create_app()
-    with application.app_context():
-        factory_business(identifier='BC1234567', entity_type=Business.LegalTypes.COMP.value)
-
-        # shouldn't run on a friday
-        with patch.object(datetime, 'today', return_value=datetime.datetime(2024, 5, 24)):
-            initiate_dissolution_process(application)
-            batches = Batch.find_by(batch_type=Batch.BatchType.INVOLUNTARY_DISSOLUTION)
-            assert batches.count() == 0
-
-# TODO: test batch and batch_processing entries are created successfully for different scenarios
