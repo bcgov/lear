8514a5663d97c3647c0e843a9ad755f24865755b
leodube-aot <122323255+leodube-aot@users.noreply.github.com>
2024-05-31 17:14:10 -0600

    21092 Dissolution Job - add logic to initiate dissolution process for businesses (first pass) (#2714)

--- jobs/involuntary-dissolutions/involuntary_dissolutions.py
+++ jobs/involuntary-dissolutions/involuntary_dissolutions.py
@@ -12,17 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Involuntary dissolutions job."""
+import asyncio
 import logging
 import os
+from datetime import datetime
 
+import pytz
 import sentry_sdk  # noqa: I001, E501; pylint: disable=ungrouped-imports; conflicts with Flake8
+from croniter import croniter
 from flask import Flask
-from legal_api.models import db  # noqa: I001
+from legal_api.models import Batch, BatchProcessing, Configuration, db  # noqa: I001
 from legal_api.services.flags import Flags
+from legal_api.services.involuntary_dissolution import InvoluntaryDissolutionService
 from sentry_sdk.integrations.logging import LoggingIntegration
 
 import config  # pylint: disable=import-error
 from utils.logging import setup_logging  # pylint: disable=import-error
+
+
 # noqa: I003
 
 setup_logging(
@@ -65,11 +72,63 @@ def register_shellcontext(app):
     app.shell_context_processor(shell_context)
 
 
-if __name__ == '__main__':
-    application = create_app()
+def initiate_dissolution_process(app: Flask):  # pylint: disable=redefined-outer-name
+    """Initiate dissolution process for new businesses that meet dissolution criteria"""
+    try:
+        # check if batch has already run today
+        batch_today = Batch.find_by(batch_type=Batch.BatchType.INVOLUNTARY_DISSOLUTION, start_date=datetime.today())
+        if batch_today:
+            app.logger.debug('Skipping job run since batch job has already run today.')
+            return
+
+        # get first NUM_DISSOLUTIONS_ALLOWED number of businesses
+        num_dissolutions_allowed = Configuration.find_by_name(config_name='NUM_DISSOLUTIONS_ALLOWED').val
+        businesses = InvoluntaryDissolutionService.get_businesses_eligible(num_dissolutions_allowed)
+
+        # create new entry in batches table
+        batch = Batch(batch_type=Batch.BatchType.INVOLUNTARY_DISSOLUTION,
+                      status=Batch.BatchStatus.PROCESSING,
+                      size=len(businesses),
+                      start_date=datetime.now())
+        batch.save()
+
+        # create batch processing entries for each business being dissolved
+        for business in businesses:
+            batch_processing = BatchProcessing(business_identifier=business.identifier,
+                                               step=BatchProcessing.BatchProcessingStep.WARNING_LEVEL_1,
+                                               status=BatchProcessing.BatchProcessingStatus.PROCESSING,
+                                               created_date=datetime.now(),
+                                               batch_id=batch.id,
+                                               business_id=business.id)
+            batch_processing.save()
+
+    except Exception as err:  # pylint: disable=redefined-outer-name; noqa: B902
+        app.logger.error(err)
+
+
+async def run(loop, application: Flask = None):  # pylint: disable=redefined-outer-name
+    """Run the stage 1-3 methods for dissolving businesses."""
+    if application is None:
+        application = create_app()
+
     with application.app_context():
         flag_on = flags.is_on('enable-involuntary-dissolution')
         application.logger.debug(f'enable-involuntary-dissolution flag on: {flag_on}')
         if flag_on:
-            # TODO: detailed implementation
-            application.logger.debug('Running involuntary dissolutions job')
+            # check if batch can be run today
+            new_dissolutions_schedule_config = Configuration.find_by_name(config_name='DISSOLUTIONS_STAGE_1_SCHEDULE')
+            tz = pytz.timezone('US/Pacific')
+            cron_valid = croniter.match(new_dissolutions_schedule_config.val, tz.localize(datetime.today()))
+            if cron_valid:
+                initiate_dissolution_process(application)
+            else:
+                application.logger.debug('Skipping job run since current day of the week does not match the cron schedule.')  # noqa: E501
+
+if __name__ == '__main__':
+    application = create_app()
+    try:
+        event_loop = asyncio.get_event_loop()
+        event_loop.run_until_complete(run(event_loop, application))
+    except Exception as err:  # pylint: disable=broad-except; Catching all errors from the frameworks
+        application.logger.error(err)  # pylint: disable=no-member
+        raise err
