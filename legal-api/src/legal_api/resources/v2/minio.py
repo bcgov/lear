41d4915cebc0d2405c6cd4432185d81f25306fee
PaulGarewal <paul.garewal@gov.bc.ca>
2024-06-10 07:44:29 -0700

    misc fixes

--- legal-api/src/legal_api/resources/v2/minio.py
+++ legal-api/src/legal_api/resources/v2/minio.py
@@ -1,8 +1,22 @@
-from flask import Blueprint, request, jsonify, current_app
-from flask_cors import cross_origin
+# Copyright © 2024 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module for handling Minio document operations."""
 from http import HTTPStatus
-from legal_api.services.minio import MinioService
+from flask import Blueprint, jsonify, current_app
+from flask_cors import cross_origin
 from minio.error import S3Error
+from legal_api.services.minio import MinioService
 from legal_api.utils.auth import jwt
 from legal_api.models import Filing
 
