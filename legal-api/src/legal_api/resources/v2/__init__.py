5580f646808c345e9f12e6b6d9462751341e5386
PaulG <144158015+PaulGarewal@users.noreply.github.com>
2024-06-11 12:04:53 -0700

    21480 Created delete and get endpointn for minio documents (#2747)
    * 21480 Created delete endpoint for minio documents
    
    * misc fixes
    
    * lint fix
    
    * renamed document_signature to document, placed changes in that file
    
    * Changed init to account for changes
    
    * added document.py
    
    * Added logic to check if is draft filing
    
    * fixed changes to document.py, readded signature endpoint
    
    * fix: attempted simple secure http change
    
    * added get request
    
    * updated draft filing status

--- legal-api/src/legal_api/resources/v2/__init__.py
+++ legal-api/src/legal_api/resources/v2/__init__.py
@@ -19,7 +19,7 @@ from flask import Flask
 from .admin import bp_admin as admin_bp
 from .business import bp as businesses_bp
 from .business.business_digital_credentials import bp_dc as digital_credentials_bp
-from .document_signature import bp as document_signature_bp
+from .document import bp as document_bp
 from .internal_services import bp as internal_bp
 from .meta import bp as meta_bp
 from .naics import bp as naics_bp
@@ -45,7 +45,7 @@ class V2Endpoint:
         self.app.register_blueprint(admin_bp)
         self.app.register_blueprint(businesses_bp)
         self.app.register_blueprint(digital_credentials_bp)
-        self.app.register_blueprint(document_signature_bp)
+        self.app.register_blueprint(document_bp)
         self.app.register_blueprint(namerequest_bp)
         self.app.register_blueprint(naics_bp)
         self.app.register_blueprint(request_tracker_bp)
