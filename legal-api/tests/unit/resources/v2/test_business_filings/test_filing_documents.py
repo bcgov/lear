5e9a2bdbb7ee226c0499f91a709717e25015b3cd
vysakh.menon <vysakh.menon@aot-technologies.com>
2024-06-12 16:53:32 -0700

    no message

--- legal-api/tests/unit/resources/v2/test_business_filings/test_filing_documents.py
+++ legal-api/tests/unit/resources/v2/test_business_filings/test_filing_documents.py
@@ -144,7 +144,7 @@ def test_unpaid_filing(session, client, jwt):
     assert rv.json == {}
 
 
-base_url = 'http://localhost:5000'
+base_url = 'https://LEGAL_API_BASE_URL'
 
 ALTERATION_WITHOUT_NR = copy.deepcopy(ALTERATION)
 del ALTERATION_WITHOUT_NR['nameRequest']['nrNumber']
