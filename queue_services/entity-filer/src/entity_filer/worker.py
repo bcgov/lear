ac6b4cb528810fcca35cb713dd07c897cf88df4c
thor <thor@wolpert.ca>
2024-06-12 23:05:24 -0700

    fix: add in temp identifier to gcp event call

--- queue_services/entity-filer/src/entity_filer/worker.py
+++ queue_services/entity-filer/src/entity_filer/worker.py
@@ -140,6 +140,18 @@ def publish_gcp_queue_event(business: Business, filing: Filing):
     """Publish the filing message onto the NATS filing subject."""
     try:
         subject = APP_CONFIG.BUSINESS_EVENTS_TOPIC
+        data= {
+                'filing': {
+                    'header': {'filingId': filing.id,
+                               'effectiveDate': filing.effective_date.isoformat()
+                               },
+                    'business': {'identifier': business.identifier},
+                    'legalFilings': get_filing_types(filing.filing_json)
+                }
+            }
+        if filing.temp_reg:
+            data['tempidentifier'] = filing.temp_reg
+
         ce = SimpleCloudEvent(
             id=str(uuid.uuid4()),
             source=''.join([
@@ -151,15 +163,7 @@ def publish_gcp_queue_event(business: Business, filing: Filing):
                 subject=subject,
             time=datetime.now(timezone.utc),
             type='bc.registry.business.' + filing.filing_type,
-            data= {
-                'filing': {
-                    'header': {'filingId': filing.id,
-                               'effectiveDate': filing.effective_date.isoformat()
-                               },
-                    'business': {'identifier': business.identifier},
-                    'legalFilings': get_filing_types(filing.filing_json)
-                }
-            }
+            data=data
         )
         
         gcp_queue.publish(subject,to_queue_message(ce))
