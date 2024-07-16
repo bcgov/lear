5d95abc9cb1ab4d272f15212f29d147ba2ddabb7
ketaki-deodhar <ketaki.deodhar@gov.bc.ca>
2024-07-16 10:55:29 -0700

    21929 - update unit tests

--- legal-api/tests/unit/resources/v2/test_reviews.py
+++ legal-api/tests/unit/resources/v2/test_reviews.py
@@ -16,11 +16,42 @@
 
 Test-Suite to ensure that admin/reviews endpoints are working as expected.
 """
-import pytest
+import copy
+from registry_schemas.example_data import (
+    CONTINUATION_IN,
+)
 from http import HTTPStatus
 
 from legal_api.services.authz import BASIC_USER, STAFF_ROLE
 from tests.unit.services.utils import create_header
+
+from legal_api.models import Review, ReviewStatus
+
+from tests.unit.models import factory_filing
+
+
+def basic_test_review():
+    filing_dict = {
+        'filing': {
+            'header': {
+                'name': 'continuationIn',
+                'date': '2019-04-08',
+                'certifiedBy': 'full name',
+                'email': 'no_one@never.get',
+            }
+        }
+    }
+    filing_dict['filing']['continuationIn'] = copy.deepcopy(CONTINUATION_IN)
+    filing = factory_filing(None, filing_dict)
+
+    review = Review()
+    review.filing_id = filing.id
+    review.nr_number = filing_dict['filing']['continuationIn']['nameRequest']['nrNumber']
+    review.identifier = filing_dict['filing']['continuationIn']['foreignJurisdiction']['identifier']
+    review.completing_party = 'completing party'
+    review.status = ReviewStatus.AWAITING_REVIEW
+
+    return review
 
 
 def test_get_reviews_with_invalid_user(app, session, client, jwt):
@@ -34,6 +65,15 @@ def test_get_reviews_with_invalid_user(app, session, client, jwt):
 def test_get_reviews_with_valid_user(app, session, client, jwt):
     """Assert review object returned for STAFF role."""
 
+    review_one = basic_test_review()
+    review_one.save()
+
+    review_two = basic_test_review()
+    review_two.save()
+
+    review_three = basic_test_review()
+    review_three.save()
+
     rv = client.get(f'/api/v2/admin/reviews',
                     headers=create_header(jwt, [STAFF_ROLE], 'user'))
 
@@ -41,3 +81,5 @@ def test_get_reviews_with_valid_user(app, session, client, jwt):
     assert 'reviews' in rv.json
     assert 1 == rv.json.get('page')
     assert 10 == rv.json.get('limit')
+    assert 3 == rv.json.get('total')
+
