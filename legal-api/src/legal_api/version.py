cb11a4f2f4de91bec91e61a3f334cc8ba2beed34
leodube-aot <leo.dube@aot-technologies.com>
2024-07-17 13:19:37 -0700

    bump version numbers up for release 22.2a

--- legal-api/src/legal_api/version.py
+++ legal-api/src/legal_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.117.0'  # pylint: disable=invalid-name
+__version__ = '2.118.0'  # pylint: disable=invalid-name
