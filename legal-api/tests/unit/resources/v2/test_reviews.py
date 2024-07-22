743edcb60e9fb8c920047302b7cd27682f61566d
vysakh.menon <vysakh.menon@aot-technologies.com>
2024-07-22 15:33:31 -0700

    21931 submit staff review

--- legal-api/tests/unit/resources/v2/test_reviews.py
+++ legal-api/tests/unit/resources/v2/test_reviews.py
@@ -17,22 +17,28 @@
 Test-Suite to ensure that admin/reviews endpoints are working as expected.
 """
 import copy
-from registry_schemas.example_data import (
-    CONTINUATION_IN,
-)
+import pytest
 from http import HTTPStatus
 
 from flask import current_app
+from registry_schemas.example_data import CONTINUATION_IN
 
+from legal_api.models import Filing, RegistrationBootstrap, Review, ReviewStatus
 from legal_api.services.authz import BASIC_USER, STAFF_ROLE
-from tests.unit.services.utils import create_header
-
-from legal_api.models import Review, ReviewStatus
 
 from tests.unit.models import factory_filing
+from tests.unit.services.utils import create_header
 
 
-def create_test_review(no_of_reviews=1):
+def create_reviews(no_of_reviews=1):
+    reviews = []
+    for index in range(no_of_reviews):
+        review = create_review(f'T1z3a56{index}')
+        reviews.append(review)
+    return reviews
+
+
+def create_review(identifier, status=ReviewStatus.AWAITING_REVIEW):
     filing_dict = {
         'filing': {
             'header': {
@@ -45,20 +51,22 @@ def create_test_review(no_of_reviews=1):
     }
     filing_dict['filing']['continuationIn'] = copy.deepcopy(CONTINUATION_IN)
 
-    reviews = []
-    for _ in range(no_of_reviews):
-        filing = factory_filing(None, filing_dict)
+    filing = factory_filing(None, filing_dict)
+    temp_reg = RegistrationBootstrap()
+    temp_reg._identifier = identifier
+    temp_reg.save()
+    filing.temp_reg = identifier
+    filing.save()
 
-        review = Review()
-        review.filing_id = filing.id
-        review.nr_number = filing_dict['filing']['continuationIn']['nameRequest']['nrNumber']
-        review.identifier = filing_dict['filing']['continuationIn']['foreignJurisdiction']['identifier']
-        review.completing_party = 'completing party'
-        review.status = ReviewStatus.AWAITING_REVIEW
-        review.save()
-        reviews.append(review)
+    review = Review()
+    review.filing_id = filing.id
+    review.nr_number = filing_dict['filing']['continuationIn']['nameRequest']['nrNumber']
+    review.identifier = filing_dict['filing']['continuationIn']['foreignJurisdiction']['identifier']
+    review.completing_party = 'completing party'
+    review.status = status
+    review.save()
 
-    return reviews
+    return review
 
 
 def test_get_reviews_with_invalid_user(app, session, client, jwt):
@@ -71,7 +79,7 @@ def test_get_reviews_with_invalid_user(app, session, client, jwt):
 def test_get_reviews_with_valid_user(app, session, client, jwt):
     """Assert review object returned for STAFF role."""
     no_of_reviews = 11
-    create_test_review(no_of_reviews)
+    create_reviews(no_of_reviews)
 
     rv = client.get(f'/api/v2/admin/reviews',
                     headers=create_header(jwt, [STAFF_ROLE], 'user'))
@@ -85,15 +93,10 @@ def test_get_reviews_with_valid_user(app, session, client, jwt):
 
 def test_get_specific_review_with_valid_user(app, session, client, jwt, mocker):
     """Assert specific review object returned for STAFF role."""
-    review = create_test_review(1)[0]
+    review = create_review('T1z3a567')
+    filing = Filing.find_by_id(review.filing_id)
 
     base_url = current_app.config.get('LEGAL_API_BASE_URL')
-
-    mock_filing = mocker.Mock()
-    mock_filing.temp_reg = 'BC1234567'
-    mock_filing.id = 1
-    mocker.patch('legal_api.models.Filing.find_by_id', return_value=mock_filing)
-
     mocker.patch('legal_api.resources.v2.admin.reviews.current_app.config.get', return_value=base_url)
 
     rv = client.get(f'/api/v2/admin/reviews/{review.id}',
@@ -102,12 +105,12 @@ def test_get_specific_review_with_valid_user(app, session, client, jwt, mocker):
     assert rv.status_code == HTTPStatus.OK
     assert rv.json['id'] == review.id
     assert 'filingLink' in rv.json
-    assert rv.json['filingLink'] == f'{base_url}/{mock_filing.temp_reg}/filings/{mock_filing.id}'
+    assert rv.json['filingLink'] == f'{base_url}/{filing.temp_reg}/filings/{filing.id}'
 
 
 def test_get_specific_review_with_invalid_user(app, session, client, jwt):
     """Assert unauthorized for BASIC_USER role when getting a specific review."""
-    review = create_test_review(1)[0]
+    review = create_review('T1z3a567')
 
     rv = client.get(f'/api/v2/admin/reviews/{review.id}',
                     headers=create_header(jwt, [BASIC_USER], 'user'))
@@ -122,4 +125,98 @@ def test_get_nonexistent_review(app, session, client, jwt):
 
     assert rv.status_code == HTTPStatus.NOT_FOUND
     assert 'message' in rv.json
+    assert rv.json['message'] == 'Review not found.'
+
+
+@pytest.mark.parametrize('status', [
+    ReviewStatus.CHANGE_REQUESTED,
+    ReviewStatus.APPROVED,
+    ReviewStatus.REJECTED,
+])
+def test_save_review(app, session, client, jwt, mocker, status):
+    """Assert that a review can be saved."""
+    review = create_review('T1z3a567')
+
+    data = {
+        'status': status.name,
+        'comment': 'Upload all documents'
+    }
+
+    subjects_in_queue = {}
+
+    def publish_json(payload, subject):
+        subjects_in_queue[subject] = payload
+
+    mocker.patch('legal_api.resources.v2.admin.reviews.queue.publish_json', side_effect=publish_json)
+
+    rv = client.post(f'/api/v2/admin/reviews/{review.id}',
+                     json=data,
+                     headers=create_header(jwt, [STAFF_ROLE], 'user'))
+    assert rv.status_code == HTTPStatus.CREATED
+    review = Review.find_by_id(review.id)
+    assert review.status == status
+    result = review.review_results.all()[0]
+    assert result
+    assert result.status == status
+    assert result.comments == data['comment']
+
+    status_mapping = {
+        ReviewStatus.CHANGE_REQUESTED: Filing.Status.CHANGE_REQUESTED.value,
+        ReviewStatus.APPROVED: Filing.Status.APPROVED.value,
+        ReviewStatus.REJECTED: Filing.Status.REJECTED.value,
+    }
+    filing = Filing.find_by_id(review.filing_id)
+    assert filing.status == status_mapping[status]
+
+    assert current_app.config.get('NATS_EMAILER_SUBJECT') in subjects_in_queue
+    if filing.status == Filing.Status.APPROVED.value:
+        assert current_app.config.get('NATS_FILER_SUBJECT') in subjects_in_queue
+    else:
+        assert current_app.config.get('NATS_FILER_SUBJECT') not in subjects_in_queue
+
+
+@pytest.mark.parametrize('data, message, response_code', [
+    ({'status': ReviewStatus.APPROVED.name}, 'Status and Comment are required.', HTTPStatus.BAD_REQUEST),
+    ({'status': ReviewStatus.RESUBMITTED.name, 'comment': 'all docs'}, 'Invalid Status.', HTTPStatus.BAD_REQUEST),
+])
+def test_save_review_validation(app, session, client, jwt, mocker, data, message, response_code):
+    """Assert that a save review can be validated."""
+    review = create_review('T1z3a567')
+
+    mocker.patch('legal_api.resources.v2.admin.reviews.queue.publish_json', return_value=None)
+    rv = client.post(f'/api/v2/admin/reviews/{review.id}',
+                     json=data,
+                     headers=create_header(jwt, [STAFF_ROLE], 'user'))
+    assert rv.status_code == response_code
+    assert rv.json['message'] == message
+
+
+@pytest.mark.parametrize('status', [
+    ReviewStatus.APPROVED,
+    ReviewStatus.REJECTED,
+    ReviewStatus.CHANGE_REQUESTED,
+])
+def test_save_review_not_allowed(app, session, client, jwt, mocker, status):
+    """Assert that a save review can be validated."""
+    review = create_review('T1z3a567', status)
+
+    data = {
+        'status': status.name,
+        'comment': 'Upload all documents'
+    }
+
+    mocker.patch('legal_api.resources.v2.admin.reviews.queue.publish_json', return_value=None)
+    rv = client.post(f'/api/v2/admin/reviews/{review.id}',
+                     json=data,
+                     headers=create_header(jwt, [STAFF_ROLE], 'user'))
+    assert rv.status_code == HTTPStatus.BAD_REQUEST
+    assert rv.json['message'] == 'No changes allowed.'
+
+
+def test_save_no_review_validation(app, session, client, jwt):
+    """Assert that a save review can be validated."""
+    rv = client.post(f'/api/v2/admin/reviews/{857}',
+                     json={},
+                     headers=create_header(jwt, [STAFF_ROLE], 'user'))
+    assert rv.status_code == HTTPStatus.NOT_FOUND
     assert rv.json['message'] == 'Review not found.'
