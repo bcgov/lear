3edea9724337e81747da8d76b935d6eab737c618
Hongjing Chen <Hongjing.Chen@gov.bc.ca>
2024-07-23 17:00:39 +0000

    fix typo
    Signed-off-by: Hongjing Chen <Hongjing.Chen@gov.bc.ca>

--- legal-api/src/legal_api/resources/v2/business/business_furnishings/furnishing_documents.py
+++ legal-api/src/legal_api/resources/v2/business/business_furnishings/furnishing_documents.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Retrieve the specified lettter for the furnishing entry."""
+"""Retrieve the specified letter for the furnishing entry."""
 from http import HTTPStatus
 from typing import Final
 
