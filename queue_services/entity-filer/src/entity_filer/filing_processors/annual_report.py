517b58effe4f90617ca1b067906bcf7bc4a7b0a8
Kevin Zhang <kevinzhang940420@gmail.com>
2024-09-04 08:33:20 -0700

    22681_remove_notes_ar_filer

--- queue_services/entity-filer/src/entity_filer/filing_processors/annual_report.py
+++ queue_services/entity-filer/src/entity_filer/filing_processors/annual_report.py
@@ -54,7 +54,6 @@ def process(business: Business, filing: Dict, filing_meta: FilingMeta, flag_on):
         if not eligibility:
             batch_processing, _ = InvoluntaryDissolutionService.get_in_dissolution_batch_processing(business.id)
             batch_processing.status = BatchProcessing.BatchProcessingStatus.WITHDRAWN.value
-            batch_processing.notes = 'Moved back to good standing'
             batch_processing.last_modified = datetime.datetime.utcnow()
 
     # save the annual report date to the filing meta info
