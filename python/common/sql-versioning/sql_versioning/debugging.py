a1be15f72a687c91a30725c7aae9840f1118682f
Hongjing Chen <Hongjing.Chen@gov.bc.ca>
2024-10-11 08:50:32 -0700

    add TODO for sql-versioning
    Signed-off-by: Hongjing Chen <Hongjing.Chen@gov.bc.ca>

--- python/common/sql-versioning/sql_versioning/debugging.py
+++ python/common/sql-versioning/sql_versioning/debugging.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Utilities used for debugging."""
+# TODO: remove this debugging utility file
 import functools
 
 
