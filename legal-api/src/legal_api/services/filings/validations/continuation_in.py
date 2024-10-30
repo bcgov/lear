e96c0a44081c9eb1c4ba7fe91b73cb0735afdd29
Qin <Arwen.Qin@gov.bc.ca>
2024-10-29 17:00:47 -0700

    fix linting
    Signed-off-by: Qin <Arwen.Qin@gov.bc.ca>

--- legal-api/src/legal_api/services/filings/validations/continuation_in.py
+++ legal-api/src/legal_api/services/filings/validations/continuation_in.py
@@ -200,7 +200,7 @@ def validate_business_in_colin(filing_json: dict, filing_type: str) -> list:
                     'path': business_founding_date_path
                 })
             else:
-                if dt.fromisoformat(founding_date) !=  dt.fromisoformat(response_json['business']['foundingDate']):
+                if dt.fromisoformat(founding_date) != dt.fromisoformat(response_json['business']['foundingDate']):
                     msg.append({
                         'error': 'Founding date does not match with founding date from Colin.',
                         'path': business_founding_date_path
