d2aa4fd6b5551118f6828965030e972ad6cfb0dd
BrandonSharratt <brandon@highwaythreesolutions.com>
2024-11-19 13:02:47 -0800

    remove T type check

--- legal-api/src/legal_api/resources/v2/business/business.py
+++ legal-api/src/legal_api/resources/v2/business/business.py
@@ -223,8 +223,6 @@ def get_allowable_for_business_type(business_type: str, business_state: str):
     """Return a JSON object with information about what a user could theoretically file for a business type."""
     business_state = business_state.upper()
     business_type = business_type.upper()
-    if business_type.startswith('T'):
-        return {'message': babel('No information on temp registrations.')}, 200
     
     bs_state = getattr(Business.State, business_state, False)
     bs_type = getattr(Business.LegalTypes, business_type, False)
