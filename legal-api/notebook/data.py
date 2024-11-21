8eecf68f8d50b373b977daf3d147104dec7742cc
ketaki-deodhar <ketaki.deodhar@gov.bc.ca>
2024-11-20 16:17:41 -0800

    24378 - jupyter notebook to add registrars notation filing for existing BENs

---
+++ legal-api/notebook/data.py
@@ -0,0 +1,14 @@
+# Populate this list with the existing BEN business identifiers from specified environment
+# This works as a data file for Jupyter notebook used to add Registrar's Notation
+ben_businesses = [
+    "BC0871277",
+    "BC0871062",
+    "BC0871261",
+    "BC0871183",
+    "BC0871181",
+    "BC0871152",
+    "BC0871062",
+    "BC0871139",
+    "BC0871073",
+    "BC0870819"
+]
