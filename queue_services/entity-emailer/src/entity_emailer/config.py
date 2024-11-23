660dc681a6261d5b9daba9b8e4360f54352cf8c2
eason-pan-bc <Eason.Pan@gov.bc.ca>
2024-11-22 17:56:55 -0800

    add service name to config.py

--- queue_services/entity-emailer/src/entity_emailer/config.py
+++ queue_services/entity-emailer/src/entity_emailer/config.py
@@ -57,7 +57,7 @@ class _Config():  # pylint: disable=too-few-public-methods
 
     Used as the base for all the other configurations.
     """
-
+    SERVICE_NAME = 'emailer'
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
     MSG_RETRY_NUM = int(os.getenv('MSG_RETRY_NUM', '5'))
