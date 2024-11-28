e04923ab50c25364a2510a3099c064031a4c8804
ketaki-deodhar <ketaki.deodhar@gov.bc.ca>
2024-11-28 13:03:29 -0800

    24378 - update data

--- jobs/correction-ben-statement/data.py
+++ jobs/correction-ben-statement/data.py
@@ -2,15 +2,6 @@
 # This works as a data file for Jupyter notebook used to add Registrar's Notation
 ben_businesses = [
     "BC0871277",
-    "BC0871062",
-    "BC0871261",
-    "BC0871183",
-    "BC0871181",
-    "BC0871152",
-    "BC0871062",
-    "BC0871139",
-    "BC0871073",
-    "BC0870819",
-    "BC0870818"
+    "BC0871062"
 ]
 
