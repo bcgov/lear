3d6252f269b64551cb0bbadebfa93d5326f9abbc
Qin <Arwen.Qin@gov.bc.ca>
2024-12-02 15:30:13 -0800

    restore amalg
    Signed-off-by: Qin <Arwen.Qin@gov.bc.ca>

--- legal-api/src/legal_api/resources/v2/business/colin_sync.py
+++ legal-api/src/legal_api/resources/v2/business/colin_sync.py
@@ -290,10 +290,7 @@ def _set_shares(primary_or_holding_business, amalgamation_filing, transaction_id
     amalgamation_filing['shareStructure'] = {'shareClasses': share_classes}
     business_dates = [item.resolution_date.isoformat() for item in primary_or_holding_business.resolutions]
     if business_dates:
-        formatted_dates = [
-            datetime.strptime(date, '%Y-%m-%d').strftime('%B %-d, %Y').replace(' 0', ' ') for date in business_dates
-        ]
-        amalgamation_filing['shareStructure']['resolutionDates'] = formatted_dates
+        amalgamation_filing['shareStructure']['resolutionDates'] = business_dates
 
 
 @bp.route('/internal/filings/<int:filing_id>', methods=['PATCH'])
