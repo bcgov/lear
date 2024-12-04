50e41da592f0b7b3da81077e7e3568424b6700da
ketaki-deodhar <116035339+ketaki-deodhar@users.noreply.github.com>
2024-12-04 10:00:00 -0800

    24378 - script to add Registrar's Notation to all existing companies (#3088)
    * 24378 - jupyter notebook to add registrars notation filing for existing BENs
    
    * 24378 - update notebook
    
    * 24378 - update date
    
    * 24378 - move files
    
    * 24378 - update data
    
    * 24378 - updat order details text

---
+++ jobs/correction-ben-statement/data.py
@@ -0,0 +1,7 @@
+# Populate this list with the existing BEN business identifiers from specified environment
+# This works as a data file for Jupyter notebook used to add Registrar's Notation
+ben_businesses = [
+    "BC0871277",
+    "BC0871062"
+]
+
