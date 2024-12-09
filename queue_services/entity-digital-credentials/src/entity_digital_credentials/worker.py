44763453d994a18404d6352678f980f87bc9a9bd
Kevin Zhang <kevinzhang940420@gmail.com>
2024-12-09 12:08:04 -0800

    update unit test, fix lint issue

--- queue_services/entity-digital-credentials/src/entity_digital_credentials/worker.py
+++ queue_services/entity-digital-credentials/src/entity_digital_credentials/worker.py
@@ -33,7 +33,7 @@ import nats
 from entity_queue_common.service import QueueServiceManager
 from entity_queue_common.service_utils import QueueException, logger
 from flask import Flask
-from legal_api import db, init_db
+from legal_api import init_db
 from legal_api.core import Filing as FilingCore
 from legal_api.models import Business
 from legal_api.services import digital_credentials, flags
