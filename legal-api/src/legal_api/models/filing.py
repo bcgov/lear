3e79e14da7859f746b1fd9f5dc2dfaa4c0faa1d8
meganwong <megan.a.wong@gov.bc.ca>
2025-01-27 17:52:50 +0000

    25328-Optimize-query-for-temp-reg-filing

--- legal-api/src/legal_api/models/filing.py
+++ legal-api/src/legal_api/models/filing.py
@@ -862,18 +862,25 @@ class Filing(db.Model):  # pylint: disable=too-many-instance-attributes,too-many
     @staticmethod
     def get_temp_reg_filing(temp_reg_id: str, filing_id: str = None):
         """Return a filing by the temp id and filing id (if applicable)."""
-        # fetch temp filing
-        q = db.session.query(Filing).filter(Filing.temp_reg == temp_reg_id)
-        if filing_id:
-            filing = db.session.query(Filing).filter(Filing.id == filing_id).one_or_none()
-            if filing:
-                # Special case for NoW
-                is_now_filing = filing.filing_type == 'noticeOfWithdrawal'
-                temp_filing_id = q.one_or_none().id
-                if is_now_filing and filing.withdrawn_filing_id == temp_filing_id:
-                    return filing
-            return q.filter(Filing.id == filing_id).one_or_none()
-        return q.one_or_none()
+        if not filing_id:
+            return db.session.query(Filing).filter(Filing.temp_reg == temp_reg_id).one_or_none()
+
+        return (
+            db.session.query(Filing).filter(
+                db.or_(
+                    db.and_(
+                        Filing.id == filing_id,
+                        Filing.temp_reg == temp_reg_id
+                    ),
+                    db.and_(  # special case for NoW
+                        Filing.id == filing_id,
+                        Filing._filing_type == 'noticeOfWithdrawal',
+                        Filing.withdrawn_filing_id == db.session.query(Filing.id)
+                        .filter(Filing.temp_reg == temp_reg_id)
+                        .scalar_subquery()
+                    )
+                )
+            ).one_or_none())
 
     @staticmethod
     def get_filing_by_payment_token(token: str):
