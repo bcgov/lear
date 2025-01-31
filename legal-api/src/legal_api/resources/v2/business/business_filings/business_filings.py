c5d132437a7dd320929b3d7a09b3df17bf77270e
meganwong <megan.a.wong@gov.bc.ca>
2025-01-31 18:07:19 +0000

    25641-Embed-NoW-once-available-and-update-unit-tests

--- legal-api/src/legal_api/resources/v2/business/business_filings/business_filings.py
+++ legal-api/src/legal_api/resources/v2/business/business_filings/business_filings.py
@@ -307,7 +307,7 @@ class ListFilingResource():  # pylint: disable=too-many-public-methods
         filing_json = rv.json
         if rv.status == Filing.Status.PENDING.value:
             ListFilingResource.get_payment_update(filing_json)
-        if rv.status == Filing.Status.WITHDRAWN.value and identifier.startswith('T'):
+        if (rv.status == Filing.Status.WITHDRAWN.value or rv.storage.withdrawal_pending) and identifier.startswith('T'):
             now_filing = ListFilingResource.get_notice_of_withdrawal(filing_json['filing']['header']['filingId'])
             filing_json['filing']['noticeOfWithdrawal'] = now_filing.json
         elif (rv.status in [Filing.Status.CHANGE_REQUESTED.value,
