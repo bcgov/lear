f7bbc58e1aaab8d70a66f618c72b43075dfbc757
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2025-02-07 14:20:17 -0800

    Update version.py

--- colin-api/src/colin_api/version.py
+++ colin-api/src/colin_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.137.0'  # pylint: disable=invalid-name
+__version__ = '2.139.0'  # pylint: disable=invalid-name
