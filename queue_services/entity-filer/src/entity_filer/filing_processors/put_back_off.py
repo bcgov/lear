26d941d7bd4dcda4b467be7e130c07ba09ed97b8
meawong <megan.a.wong@gov.bc.ca>
2025-02-11 16:03:06 -0800

    25473 - Add Short Reason for Limited Restoration Expiry (#3222)
    * 25473-Add-reason-and-expiry-date-for-put-back-off
    
    * 25473-Remove-unused-import-from-test
    
    * 25473 - Changes from comments
    
    * 25473 - Fix missing initialization

--- queue_services/entity-filer/src/entity_filer/filing_processors/put_back_off.py
+++ queue_services/entity-filer/src/entity_filer/filing_processors/put_back_off.py
@@ -19,6 +19,7 @@ from typing import Dict
 import dpath
 from entity_queue_common.service_utils import QueueException, logger
 from legal_api.models import Business, Filing
+from legal_api.utils.legislation_datetime import LegislationDatetime
 
 from entity_filer.filing_meta import FilingMeta
 from entity_filer.filing_processors.filing_components import filings
@@ -32,12 +33,21 @@ def process(business: Business, filing: Dict, filing_rec: Filing, filing_meta: F
 
     logger.debug('processing putBackOff: %s', filing)
 
+    filing_meta.put_back_off = {}
+
     # update court order, if any is present
     with suppress(IndexError, KeyError, TypeError):
         court_order_json = dpath.util.get(put_back_off_filing, '/courtOrder')
         filings.update_filing_court_order(filing_rec, court_order_json)
 
     filing_rec.order_details = put_back_off_filing.get('details')
+
+    if business.restoration_expiry_date:
+        filing_meta.put_back_off = {
+          **filing_meta.put_back_off,
+          'reason': 'Limited Restoration Expired',
+          'expiryDate': LegislationDatetime.format_as_legislation_date(business.restoration_expiry_date)
+        }
 
     # change business state to historical
     business.state = Business.State.HISTORICAL
