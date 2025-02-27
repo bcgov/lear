2a462375d8d11a2fe4e5cbed462ed671fbece9fd
EasonPan <Eason.Pan@gov.bc.ca>
2025-02-27 11:45:10 -0800

    remove deleted debugging decorator import (#3261)

--- python/common/sql-versioning/sql_versioning/__init__.py
+++ python/common/sql-versioning/sql_versioning/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Versioning extension for SQLAlchemy."""
-from .debugging import debug
 from .versioning import (Base, TransactionFactory, TransactionManager,
                          Versioned, disable_versioning, enable_versioning)
 from .utils import version_class
