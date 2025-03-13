697d0029c05f19c08ac6e749f7dee9eac8dc3f07
ketaki-deodhar <ketaki.deodhar@gov.bc.ca>
2025-03-13 16:20:41 -0700

    25959 - cease receiver filer emailer and outputs

--- queue_services/entity-emailer/src/entity_emailer/message_tracker/tracker.py
+++ queue_services/entity-emailer/src/entity_emailer/message_tracker/tracker.py
@@ -106,7 +106,8 @@ def get_message_context_properties(queue_msg: nats.aio.client.Msg):
             return create_message_context_properties(etype, message_id, None, None, False)
 
         if etype in ('dissolution', 'registration', 'changeOfRegistration',
-                     'restoration', 'specialResolution', 'correction', 'amalgamationApplication', 'continuationIn') \
+                     'restoration', 'specialResolution', 'correction', 'amalgamationApplication', 'continuationIn',
+                     'ceaseReceiver') \
                 and (option := email.get('option', None)) \
                 and (filing_id := email.get('filingId', None)):
             # option contains current status of filing - PAID or COMPLETED or CHANGE_REQUESTED, etc
