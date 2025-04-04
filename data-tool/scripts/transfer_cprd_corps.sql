6ca386f973a7c0d0d55db27ef43397cd1e07e4eb
leodube-aot <leo.dube@aot-technologies.com>
2025-04-04 08:52:43 -0700

    Fix corp_involved_cont_in query

--- data-tool/scripts/transfer_cprd_corps.sql
+++ data-tool/scripts/transfer_cprd_corps.sql
@@ -72,7 +72,7 @@ with corporation_cte as (
             where e.CORP_NUM = c.CORP_NUM
                 and f.EVENT_ID = e.EVENT_ID
                 and f.FILING_TYP_CD = 'BEINC'
-          )
+       )
       and c.CORP_TYP_CD in ('BC', 'C', 'ULC', 'CUL', 'CC', 'CCC', 'QA', 'QB', 'QC', 'QD', 'QE')
           -- and c.corp_num in ('1396310', '1396309', '1396308', '1396307', '1396306', '1396890', '1396889', '1396885', '1396883', '1396878','1396597', '1396143', '1395925', '1395116', '1394990', '1246445', '1216743', '1396508', '1396505', '1396488', '1396401', '1396387', '1396957', '1355943', '1340611', '1335427', '1327193', '1393945', '1208648', '1117024', '1120292', '1127373', '1135492')
           -- and rownum <= 5
@@ -900,6 +900,7 @@ from event e
 where e.event_id = ci.event_id
   and c.corp_num = e.corp_num
   and e.event_id = f.event_id
+  and filing_typ_cd in ('CONTI', 'CONTU', 'CONTC')
 order by e.event_id;
 
 
