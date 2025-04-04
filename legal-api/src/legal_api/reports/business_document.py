aedabb7f6bb65233aadb9ca87bec3bac764b3709
Aimee Gao <Aimee.Gao@gov.bc.ca>
2025-04-04 10:02:19 -0700

    Update business document

--- legal-api/src/legal_api/reports/business_document.py
+++ legal-api/src/legal_api/reports/business_document.py
@@ -26,6 +26,7 @@ from legal_api.reports.registrar_meta import RegistrarInfo
 from legal_api.resources.v2.business import get_addresses, get_directors
 from legal_api.resources.v2.business.business_parties import get_parties
 from legal_api.services import VersionedBusinessDetailsService
+from legal_api.services import flags
 from legal_api.utils.auth import jwt
 from legal_api.utils.legislation_datetime import LegislationDatetime
 
@@ -99,13 +100,13 @@ class BusinessDocument:
             'common/nameTranslation',
             'common/style',
             'common/styleLetterOverride',
-            'common/styleWithWatermark',
             'common/certificateFooter',
             'common/certificateLogo',
             'common/certificateRegistrarSignature',
             'common/certificateSeal',
             'common/certificateStyle',
             'common/courtOrder',
+            'common/watermark',
             'footer',
             'logo',
             'macros',
@@ -124,6 +125,9 @@ class BusinessDocument:
             # get document data
             business_json['reportType'] = self._document_key
             business_json['business'] = self._business.json()
+
+            # set FFs config
+            business_json['enable_sandbox'] = flags.is_on('enable-sandbox')
 
             # legal name easy fix
             if not get_json:
