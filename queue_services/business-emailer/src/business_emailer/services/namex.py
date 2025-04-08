100548ca258b94de709cd2248d2d20c868f533ac
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2025-04-08 14:37:58 -0700

    26309 - moving filing util into the business_emailer

--- queue_services/business-emailer/src/business_emailer/services/namex.py
+++ queue_services/business-emailer/src/business_emailer/services/namex.py
@@ -16,16 +16,52 @@
 import http
 from datetime import datetime
 from enum import Enum
+from typing import Dict, Optional
 
 import datedelta
+import dpath.util
 import pytz
 import requests
 from business_account.AccountService import AccountService
-from business_common.utils.filing import get_str_from_json_filing
 from business_model.models import Filing
 from flask import current_app
 
+def get_str_from_json_filing(filing: Dict, path: str) -> Optional[str]:
+    """Extract a str from the JSON filing, at the provided path.
 
+    Args:
+        data (Dict): A valid registry_schema filing.
+        path (str): The path to the date, which is in ISO Format.
+
+    Examples:
+        >>>get_str_from_json_filing(
+            filing={'filing':{'header':{'name': 'annualReport'}}},
+            path='filing/header/name')
+        'annualReport'
+
+    """
+    get_str(filing, path)
+
+@DeprecationWarning
+def get_str(filing: Dict, path: str) -> Optional[str]:
+    """Extract a str from the JSON filing, at the provided path.
+
+    Args:
+        filing (Dict): A valid registry_schema filing.
+        path (str): The path to the date, which is in ISO Format.
+
+    Examples:
+        >>>get_str(
+            filing={'filing':{'header':{'name': 'annualReport'}}},
+            path='filing/header/name')
+        'annualReport'
+
+    """
+    try:
+        raw = dpath.util.get(filing, path)
+        return str(raw)
+    except (IndexError, KeyError, TypeError, ValueError):
+        return None
 class NameXService:
     """Provides services to use the namex-api."""
 
