0c1688dbd1db142f1817c8a71ea21fa11351d037
BrandonSharratt <brandon@highwaythreesolutions.com>
2025-04-14 12:46:53 -0700

    common updates

---
+++ python/common/business-registry-common/src/business_common/core/involuntary_dissolution_types.py
@@ -0,0 +1,20 @@
+class DissolutionTypes(str, Enum):
+    """Dissolution types."""
+
+    ADMINISTRATIVE = 'administrative'
+    COURT_ORDERED_LIQUIDATION = 'courtOrderedLiquidation'
+    INVOLUNTARY = 'involuntary'
+    VOLUNTARY = 'voluntary'
+    VOLUNTARY_LIQUIDATION = 'voluntaryLiquidation'
+
+
+class DissolutionStatementTypes(str, Enum):
+    """Dissolution statement types."""
+
+    NO_ASSETS_NO_LIABILITIES_197 = '197NoAssetsNoLiabilities'
+    NO_ASSETS_PROVISIONS_LIABILITIES_197 = '197NoAssetsProvisionsLiabilities'
+
+    @classmethod
+    def has_value(cls, value):
+        """Check if enum contains specific value provided via input param."""
+        return value in cls._value2member_map_  # pylint: disable=no-member
---
+++ python/common/business-registry-common/src/business_common/errors/errors.py
@@ -0,0 +1,24 @@
+# Copyright © 2019 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Common error."""
+from typing import Dict, List
+
+
+class Error():  # pylint: disable=too-few-public-methods; convenience class
+    """A convenience class for managing errors as code outside of Exceptions."""
+
+    def __init__(self, code: int, message: List[Dict]):
+        """Initialize the error object."""
+        self.code = code
+        self.msg = message
