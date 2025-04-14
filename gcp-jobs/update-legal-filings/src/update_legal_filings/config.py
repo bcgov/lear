d2a193d0783192271d94dee29f1665876ba667db
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2025-04-14 12:24:12 -0700

    feat: cleanup config; add GCP queue insetad of QSM

--- gcp-jobs/update-legal-filings/src/update_legal_filings/config.py
+++ gcp-jobs/update-legal-filings/src/update_legal_filings/config.py
@@ -21,52 +21,45 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 CONFIGURATION = {
-    'development': 'config.DevConfig',
-    'testing': 'config.TestConfig',
-    'production': 'config.ProdConfig',
-    'default': 'config.ProdConfig'
+    "development": "config.DevConfig",
+    "testing": "config.TestConfig",
+    "production": "config.ProdConfig",
+    "default": "config.ProdConfig"
 }
 
 
-def get_named_config(config_name: str = 'production'):
+def get_named_config(config_name: str = "production"):
     """Return the configuration object based on the name."""
-    if config_name in ['production', 'staging', 'default']:
+    if config_name in ["production", "staging", "default"]:
         config = ProdConfig()
-    elif config_name == 'testing':
+    elif config_name == "testing":
         config = TestConfig()
-    elif config_name == 'development':
+    elif config_name == "development":
         config = DevConfig()
     else:
         raise KeyError(f"Unknown configuration '{config_name}'")
     return config
 
 
-class _Config(object):  # pylint: disable=too-few-public-methods
+class _Config:  # pylint: disable=too-few-public-methods
     """Base class configuration."""
 
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-    COLIN_URL = os.getenv('COLIN_URL', '')
-    LEGAL_API_URL = os.getenv('LEGAL_API_URL', '')
-    SENTRY_DSN = os.getenv('SENTRY_DSN') or ''
-    SENTRY_DSN = '' if SENTRY_DSN.lower() == 'null' else SENTRY_DSN
+    COLIN_URL = os.getenv("COLIN_URL", "")
+    LEGAL_API_URL = os.getenv("LEGAL_API_URL", "")
 
-    ACCOUNT_SVC_AUTH_URL = os.getenv('ACCOUNT_SVC_AUTH_URL', None)
-    ACCOUNT_SVC_CLIENT_ID = os.getenv('ACCOUNT_SVC_CLIENT_ID', None)
-    ACCOUNT_SVC_CLIENT_SECRET = os.getenv('ACCOUNT_SVC_CLIENT_SECRET', None)
-    ACCOUNT_SVC_TIMEOUT = os.getenv('ACCOUNT_SVC_TIMEOUT', 20)
+    BUSINESS_EVENTS_TOPIC = os.getenv("BUSINESS_EVENTS_TOPIC", "business-bn")
+    BUSINESS_EMAILER_TOPIC = os.getenv("BUSINESS_EMAILER_TOPIC", None)
 
-    NATS_SERVERS = os.getenv('NATS_SERVERS', None)
-    NATS_CLUSTER_ID = os.getenv('NATS_CLUSTER_ID', None)
-    NATS_CLIENT_NAME = os.getenv('NATS_LEGAL_UPDATER_CLIENT_NAME', 'entity.updatelegalfilings')
-    NATS_EMAILER_SUBJECT = os.getenv('NATS_EMAILER_SUBJECT', 'entity.email')
-    NATS_ENTITY_EVENTS_SUBJECT = os.getenv('NATS_ENTITY_EVENTS_SUBJECT', 'entity.events')
+    ACCOUNT_SVC_AUTH_URL = os.getenv("ACCOUNT_SVC_AUTH_URL", None)
+    ACCOUNT_SVC_CLIENT_ID = os.getenv("ACCOUNT_SVC_CLIENT_ID", None)
+    ACCOUNT_SVC_CLIENT_SECRET = os.getenv("ACCOUNT_SVC_CLIENT_SECRET", None)
+    ACCOUNT_SVC_TIMEOUT = os.getenv("ACCOUNT_SVC_TIMEOUT", 20)
 
-    SECRET_KEY = 'a secret'
+    SECRET_KEY = "a secret"
 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    ALEMBIC_INI = 'migrations/alembic.ini'
+    ENVIRONMENT = os.getenv("ENVIRONMENT", "production")
 
     TESTING = False
     DEBUG = False
@@ -85,28 +78,13 @@ class TestConfig(_Config):  # pylint: disable=too-few-public-methods
     DEBUG = True
     TESTING = True
 
-    COLIN_URL = os.getenv('COLIN_URL_TEST', '')
-    LEGAL_API_URL = os.getenv('LEGAL_API_URL_TEST', '')
-
-    # POSTGRESQL
-    DB_USER = os.getenv('DATABASE_TEST_USERNAME', '')
-    DB_PASSWORD = os.getenv('DATABASE_TEST_PASSWORD', '')
-    DB_NAME = os.getenv('DATABASE_TEST_NAME', '')
-    DB_HOST = os.getenv('DATABASE_TEST_HOST', '')
-    DB_PORT = os.getenv('DATABASE_TEST_PORT', '5432')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{host}:{port}/{name}'.format(
-        user=DB_USER,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        port=int(DB_PORT),
-        name=DB_NAME,
-    )
-
+    COLIN_URL = os.getenv("COLIN_URL_TEST", "")
+    LEGAL_API_URL = os.getenv("LEGAL_API_URL_TEST", "")
 
 class ProdConfig(_Config):  # pylint: disable=too-few-public-methods
     """Production environment configuration."""
 
-    SECRET_KEY = os.getenv('SECRET_KEY', None)
+    SECRET_KEY = os.getenv("SECRET_KEY", None)
 
     if not SECRET_KEY:
         SECRET_KEY = os.urandom(24)
