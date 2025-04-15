4f4f1f453b6e31699add8fd6983fbb3cc985a897
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2025-04-15 11:53:17 -0700

    fix: typo; pr comments.

--- gcp-jobs/update-legal-filings/tests/unit/test_create_app.py
+++ gcp-jobs/update-legal-filings/tests/unit/test_create_app.py
@@ -2,7 +2,6 @@ import pytest
 from dotenv import find_dotenv, load_dotenv
 
 from update_legal_filings import create_app
-from update_legal_filings.worker import update_business_nos
 
 # this will load all the envars from a .env file located in the project root (api)
 load_dotenv(find_dotenv())
@@ -12,7 +11,3 @@ def test_create_app():
         create_app("testing")
     except Exception:
         pytest.fail("Failed to create app")
-
-
-def test_update_business_nos(app):  # pylint: disable=redefined-outer-name
-    update_business_nos()
