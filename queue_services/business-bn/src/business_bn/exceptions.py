6c7154d2262d176958b1a0c309353562f72cc12b
Peinan Wang <peinanwang@RiddleMBP.local>
2025-04-14 21:31:40 -0700

    remove unused files

--- queue_services/business-bn/src/business_bn/exceptions.py
+++ queue_services/business-bn/src/business_bn/exceptions.py
@@ -1,17 +1,17 @@
-
 # BNExceptionn
 # BNRetryExceededException
 # QueueException
 
 """Exceptions defined for the business_bn service."""
 
-class QueueException(Exception):
+
+class QueueException(Exception): # noqa: N818
     """Base exception for the Queue Services."""
 
 
-class BNException(Exception):
+class BNException(Exception): # noqa: N818
     """BN exception for the Queue Services."""
 
 
-class BNRetryExceededException(Exception):
+class BNRetryExceededException(Exception): # noqa: N818
     """BN retry exceeded exception for the Queue Services."""
