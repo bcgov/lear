55cf0d71182b0fbf5467c8e1f990e72e4f9424ac
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2025-04-17 09:48:39 -0700

    26711 - Upgrading Expired limited restoration job (#3414)
    * chore: copy job files; update-legal-filings
    
    * chore: remove unused files.
    
    * feat: move code to poetry, organize directories.
    
    * feat: cleanup config; add GCP queue insetad of QSM
    
    * feat: remove async stuff; move to run as 'worker' from flask.
    
    * feat: copy colin_api stuff into helper; add tests
    
    * chore: ruff fixes.
    
    * fix: update source path.
    
    * fix: update source path.
    
    * fix: typo; pr comments.
    
    * 26711 - expired limited restoration;copy paste base
    
    * refactor: update package structure, purge logging and; add poetry; upgrade versions.
    
    * refactor: setup how app is run; add tests.
    
    * chore: ruff fixes.
    
    * feat: adding ci file.
    
    * fix: fix tests aftere moving the file.
    
    * chore: syncing with new changes in original job.

---
+++ gcp-jobs/expired-limited-restoration/src/expired_limited_restoration/config.py
@@ -0,0 +1,72 @@
+# Copyright © 2025 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""All of the configuration for the service is captured here.
+
+All items are loaded, or have Constants defined here that are loaded into the Flask configuration.
+All modules and lookups get their configuration from the Flask config, rather than reading environment variables
+directly or by accessing this configuration directly.
+"""
+import os
+import sys
+
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+class _Config:  # pylint: disable=too-few-public-methods
+    """Base class configuration that should set reasonable defaults for all the other configurations."""
+
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+    LEGAL_API_URL = os.getenv("LEGAL_API_URL", "")
+
+    ACCOUNT_SVC_AUTH_URL = os.getenv("ACCOUNT_SVC_AUTH_URL", None)
+    ACCOUNT_SVC_CLIENT_ID = os.getenv("ACCOUNT_SVC_CLIENT_ID", None)
+    ACCOUNT_SVC_CLIENT_SECRET = os.getenv("ACCOUNT_SVC_CLIENT_SECRET", None)
+    ACCOUNT_SVC_TIMEOUT = os.getenv("ACCOUNT_SVC_TIMEOUT", "20")
+
+    SECRET_KEY = "a secret"
+
+    TESTING = False
+    DEBUG = False
+
+
+class DevConfig(_Config):  # pylint: disable=too-few-public-methods
+    """Config for local development."""
+
+    TESTING = False
+    DEBUG = True
+
+
+class TestConfig(_Config):  # pylint: disable=too-few-public-methods
+    """In support of testing only used by the py.test suite."""
+
+    DEBUG = True
+    TESTING = True
+
+    LEGAL_API_URL = os.getenv("LEGAL_API_URL_TEST", "")
+
+
+class ProdConfig(_Config):  # pylint: disable=too-few-public-methods
+    """Production environment configuration."""
+
+    SECRET_KEY = os.getenv("SECRET_KEY", None)
+
+    if not SECRET_KEY:
+        SECRET_KEY = os.urandom(24)
+        print("WARNING: SECRET_KEY being set as a one-shot", file=sys.stderr) # noqa: T201
+
+    TESTING = False
+    DEBUG = False
