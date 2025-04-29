eb46e38b265d9c42504a7cd7f859d3f3d7581c9f
ketaki-deodhar <ketaki.deodhar@gov.bc.ca>
2025-04-29 16:05:35 -0700

    27440 - update script and query

--- jobs/correction-ben-statement/queries.sql
+++ jobs/correction-ben-statement/queries.sql
@@ -5,10 +5,10 @@ where b.legal_type = 'BEN'
 order by b.identifier asc;
 
 -- query to get all ACTIVE businesses (BENs) for Corrections
-select b.identifier, f.id
+select b.identifier, f.id, f.effective_date
 from businesses b join filings f on b.id = f.business_id
 where b.legal_type = 'BEN' and f.filing_type = 'incorporationApplication' and b.state = 'ACTIVE'
-order by b.identifier asc;
+order by f.effective_date desc;
 
 -- query to get all ACTIVE businesses (BENs) which have "in progress" drafts
 select b.identifier, f.id, f.filing_type
