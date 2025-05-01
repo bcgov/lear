32517f7843777b012f14dbb4927f3eaa6a880e98
Hongjing <60866283+chenhongjing@users.noreply.github.com>
2025-05-01 15:52:36 -0700

    27278&27279 - tombstone - amend liquidator & correct liquidator (#3450)
    Signed-off-by: Hongjing Chen <Hongjing.Chen@gov.bc.ca>

--- data-tool/flows/tombstone/tombstone_mappings.py
+++ data-tool/flows/tombstone/tombstone_mappings.py
@@ -18,6 +18,7 @@ class EventFilings(str, Enum):
     FILE_AM_RM = 'FILE_AM_RM'
     FILE_AM_SS = 'FILE_AM_SS'
     FILE_AM_AR = 'FILE_AM_AR'
+    FILE_AM_LQ = 'FILE_AM_LQ'
 
     FILE_IAMGO = 'FILE_IAMGO'
     FILE_AMALO = 'FILE_AMALO'
@@ -86,6 +87,7 @@ class EventFilings(str, Enum):
     FILE_CO_DI = 'FILE_CO_DI'
     FILE_CO_DO = 'FILE_CO_DO'
     FILE_CO_LI = 'FILE_CO_LI'
+    FILE_CO_LQ = 'FILE_CO_LQ'
     FILE_CO_RM = 'FILE_CO_RM'
     FILE_CO_RR = 'FILE_CO_RR'
     FILE_CO_SS = 'FILE_CO_SS'
@@ -186,6 +188,7 @@ EVENT_FILING_LEAR_TARGET_MAPPING = {
     EventFilings.FILE_NOALC: 'alteration',
     EventFilings.FILE_AM_BC: 'alteration',
     EventFilings.FILE_AM_LI: 'alteration',
+    EventFilings.FILE_AM_LQ: 'alteration',
     EventFilings.FILE_AM_RM: 'alteration',
     EventFilings.FILE_AM_SS: 'alteration',
     EventFilings.FILE_AM_AR: 'alteration',
@@ -243,6 +246,7 @@ EVENT_FILING_LEAR_TARGET_MAPPING = {
     EventFilings.FILE_CO_DI: 'correction',
     EventFilings.FILE_CO_DO: 'correction',
     EventFilings.FILE_CO_LI: 'correction',
+    EventFilings.FILE_CO_LQ: 'correction',
     EventFilings.FILE_CO_RM: 'correction',
     EventFilings.FILE_CO_RR: 'correction',
     EventFilings.FILE_CO_SS: 'correction',
@@ -318,6 +322,7 @@ EVENT_FILING_DISPLAY_NAME_MAPPING = {
     EventFilings.FILE_NOALC: 'Notice of Alteration from a BC Company to Become a Community Contribution Company',
     EventFilings.FILE_AM_BC: 'Amendment - Translated Name',
     EventFilings.FILE_AM_LI: 'Amendment - Ledger Information',
+    EventFilings.FILE_AM_LQ: 'Amendment - Liquidator',
     EventFilings.FILE_AM_RM: 'Amendment - Receiver or Receiver Manager',
     EventFilings.FILE_AM_SS: 'Amendment - Share Structure',
     EventFilings.FILE_AM_AR: 'Amendment - Annual Report',
@@ -358,6 +363,7 @@ EVENT_FILING_DISPLAY_NAME_MAPPING = {
     EventFilings.FILE_CO_DI: 'Correction - Director',
     EventFilings.FILE_CO_DO: 'Correction - Dissolved Office',
     EventFilings.FILE_CO_LI: 'Correction - Ledger Information',
+    EventFilings.FILE_CO_LQ: 'Correction - Liquidator',
     EventFilings.FILE_CO_RM: 'Correction - Receiver or Receiver Manager',
     EventFilings.FILE_CO_RR: 'Correction - Registered and Records Offices',
     EventFilings.FILE_CO_SS: 'Correction - Share Structure',
@@ -411,8 +417,6 @@ EVENT_FILING_DISPLAY_NAME_MAPPING = {
     # ADVLQ: 'Application for Dissolution (Voluntary Liquidation)'
     # AM_LR: 'Amendment - Liquidation Report'
     # CO_LR: 'Correction - Liquidation Report'
-    # AM_LQ: 'Amendment - Liquidator'
-    # CO_LQ: 'Correction - Liquidator'
 
     EventFilings.FILE_NWITH: 'Notice of Withdrawal',
 
