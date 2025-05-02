60d68e7d37ae8490f5abcab4cf4f9fee853f365b
Arwen Qin <122495122+ArwenQin@users.noreply.github.com>
2025-05-02 09:39:36 -0700

    27128 - Record of Amalgamation out Emailer (#3449)
    * add email processor
    
    Signed-off-by: Qin <Arwen.Qin@gov.bc.ca>
    
    * add email worker
    
    Signed-off-by: Qin <Arwen.Qin@gov.bc.ca>
    
    * add unit tests
    
    Signed-off-by: Qin <Arwen.Qin@gov.bc.ca>
    
    * update templates 1
    
    Signed-off-by: Qin <Arwen.Qin@gov.bc.ca>
    
    * update CAO email template
    
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>
    
    * update AO email template
    
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>
    
    * fix linting, unit tests
    
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>
    
    * fix linting, unit tests 2
    
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>
    
    * try fix unit test 1
    
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>
    
    * try fix unit test 2
    
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>
    
    * try fix unit test 3
    
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>
    
    * update new name in New Jurisdiction
    
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>
    
    * try to fix unit tests 1
    
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>
    
    * try to fix unit tests 2
    
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>
    
    * try to fix unit tests 3
    
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>
    
    * fix lint
    
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>
    
    ---------
    
    Signed-off-by: Qin <Arwen.Qin@gov.bc.ca>
    Signed-off-by: Arwen Qin <Arwen.Qin@gov.bc.ca>

--- queue_services/entity-emailer/src/entity_emailer/email_processors/amalgamation_out_notification.py
+++ queue_services/entity-emailer/src/entity_emailer/email_processors/amalgamation_out_notification.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Email processing rules and actions for CAO notifications."""
+"""Email processing rules and actions for Amalgamation Out notifications."""
 from __future__ import annotations
 
 import base64
@@ -26,14 +26,8 @@ from entity_queue_common.service_utils import logger
 from flask import current_app
 from jinja2 import Template
 from legal_api.models import Business, Filing, UserRoles
-from legal_api.utils.legislation_datetime import LegislationDatetime
 
-from entity_emailer.email_processors import (
-    get_filing_document,
-    get_filing_info,
-    get_recipient_from_auth,
-    substitute_template_parts,
-)
+from entity_emailer.email_processors import get_filing_info, get_recipient_from_auth, substitute_template_parts
 
 
 def _get_pdfs(
@@ -43,27 +37,13 @@ def _get_pdfs(
         filing_date_time: str,
         effective_date: str) -> list:
     # pylint: disable=too-many-locals, too-many-branches, too-many-statements, too-many-arguments
-    """Get the pdfs for the Consent Amalgamation Out output."""
+    """Get the pdfs for the Amalgamation Out output."""
     pdfs = []
     attach_order = 1
     headers = {
         'Accept': 'application/pdf',
         'Authorization': f'Bearer {token}'
     }
-
-    # add Letter of Consent pdf
-    filing_pdf_type = 'letterOfConsentAmalgamationOut'
-    filing_pdf_encoded = get_filing_document(business['identifier'], filing.id, filing_pdf_type, token)
-    if filing_pdf_encoded:
-        pdfs.append(
-            {
-                'fileName': 'Letter of Consent.pdf',
-                'fileBytes': filing_pdf_encoded.decode('utf-8'),
-                'fileUrl': '',
-                'attachOrder': str(attach_order)
-            }
-        )
-        attach_order += 1
 
     # add receipt pdf
     corp_name = business.get('legalName')
@@ -97,8 +77,8 @@ def _get_pdfs(
 
 
 def process(email_info: dict, token: str) -> dict:  # pylint: disable=too-many-locals, too-many-branches
-    """Build the email for Consent Amalgamation Out notification."""
-    logger.debug('consent_amalgamation_out_notification: %s', email_info)
+    """Build the email for Amalgamation Out notification."""
+    logger.debug('amalgamation_out_notification: %s', email_info)
     # get template and fill in parts
     filing_type, status = email_info['type'], email_info['option']
     # get template vars from filing
@@ -106,15 +86,16 @@ def process(email_info: dict, token: str) -> dict:  # pylint: disable=too-many-l
     filing_name = filing.filing_type[0].upper() + ' '.join(re.findall('[a-zA-Z][^A-Z]*', filing.filing_type[1:]))
 
     template = Path(
-        f'{current_app.config.get("TEMPLATE_PATH")}/CAO-{status}.html'
+        f'{current_app.config.get("TEMPLATE_PATH")}/AO-{status}.html'
     ).read_text()
     filled_template = substitute_template_parts(template)
     # render template with vars
     jnja_template = Template(filled_template, autoescape=True)
     filing_data = (filing.json)['filing'][f'{filing_type}']
-    expiry_date_time = datetime.fromisoformat(filing.meta_data[f'{filing_type}']['expiry'])
-    expiry_date = LegislationDatetime.as_legislation_timezone(expiry_date_time)
-    expiry_date = expiry_date.strftime('%B %d, %Y')
+    effective_date = leg_tmz_effective_date.split(' at ')[0]
+    historical_date = datetime.strptime(filing.meta_data[f'{filing_type}']['amalgamationOutDate'], '%Y-%m-%d')
+    historical_date = historical_date.strftime('%B %d, %Y')
+    new_name = filing.meta_data[f'{filing_type}']['legalName']
     jurisdiction_region_code = filing.meta_data[f'{filing_type}']['region']
     jurisdiction_country_code = filing.meta_data[f'{filing_type}']['country']
     jurisdiction_country = pycountry.countries.get(alpha_2=jurisdiction_country_code).name
@@ -128,7 +109,9 @@ def process(email_info: dict, token: str) -> dict:  # pylint: disable=too-many-l
         business=business,
         filing=filing_data,
         header=(filing.json)['filing']['header'],
-        expiry_date=expiry_date,
+        historical_date=historical_date,
+        effective_date=effective_date,
+        new_name=new_name,
         jurisdiction_region=jurisdiction_region,
         jurisdiction_country=jurisdiction_country,
         entity_dashboard_url=current_app.config.get('DASHBOARD_URL') +
@@ -146,14 +129,14 @@ def process(email_info: dict, token: str) -> dict:  # pylint: disable=too-many-l
     recipients.append(get_recipient_from_auth(identifier, token))
 
     if filing.submitter_roles and UserRoles.staff in filing.submitter_roles:
-        # when staff file a CAO documentOptionalEmail may contain completing party email
+        # when staff file a AO documentOptionalEmail may contain completing party email
         recipients.append(filing.filing_json['filing']['header'].get('documentOptionalEmail'))
 
     recipients = list(set(recipients))
     recipients = ', '.join(filter(None, recipients)).strip()
 
     # assign subject
-    subject = 'Six Month Consent to Amalgamate Out'
+    subject = 'Amalgamation Out'
 
     legal_name = business.get('legalName', None)
     subject = f'{legal_name} - {subject}' if legal_name else subject
