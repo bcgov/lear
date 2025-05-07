dc06ddbd7a0ee37f33a53c8309b91f0470195dd7
ketaki-deodhar <116035339+ketaki-deodhar@users.noreply.github.com>
2025-05-07 09:55:50 -0700

    Ben correction statements (#3472)
    * 27440 - update script and query
    
    * 27440 - date change in script and output fix
    
    * 27440 - fix
    
    * update queries and fix lint issues
    
    * update queries and waive fees

--- jobs/correction-ben-statement/convert_registrar_notation_data.py
+++ jobs/correction-ben-statement/convert_registrar_notation_data.py
@@ -1,24 +1,27 @@
 import csv
 
+
 # Function to read CSV and convert to a Python array
 def csv_to_python_array(file_path):
     array = []
-    
+
     # Open the CSV file and read its contents
     with open(file_path, mode='r', newline='') as file:
         reader = csv.reader(file)
         for row in reader:
             array.extend(row)  # Add each element from the row to the array
-    
+
     return array
+
 
 # Function to write the Python array to a file, with each element on a new row
 def write_to_python_file(array, output_file):
     with open(output_file, 'w') as file:
         file.write('rn_businesses = [\n')  # Start the array in Python format
         for element in array:
-            file.write(f"    '{element}',\n")  # Write each element in the array
+            file.write(f"    '{element}',\n")  # Write each element in array
         file.write(']\n')  # End the array in Python format
+
 
 input_csv = 'registrar_notation_result.csv'
 output_python_file = 'rn_output.py'
--- jobs/correction-ben-statement/queries.sql
+++ jobs/correction-ben-statement/queries.sql
@@ -1,17 +1,54 @@
--- query to get all businesses (BENs) for Registrar's Notation
-select b.identifier
-from businesses b
-where b.legal_type = 'BEN'
-order by b.identifier asc;
+-- Queries changed a bit as FF was enabled first and scripts will be run later
 
--- query to get all ACTIVE businesses (BENs) for Corrections
-select b.identifier, f.id, f.filing_date
-from businesses b join filings f on b.id = f.business_id
-where b.legal_type = 'BEN' and f.filing_type = 'incorporationApplication' and b.state = 'ACTIVE'
-order by b.identifier asc;
+-- query to get all businesses (BENs with No In-progress DRAFTS) for Registrar's Notation until 2025-05-02 3:16pm
+select 
+	b.identifier
+from businesses b 
+	 join filings f 
+	 	on b.id = f.business_id
+where
+	b.legal_type = 'BEN' 
+	and f.filing_type = 'incorporationApplication'
+    and f.filing_date < TIMESTAMP '2025-05-02 15:16:00' AT TIME ZONE 'America/Los_Angeles'
+	and (
+		b.identifier NOT IN (
+			select DISTINCT(bb.identifier) 
+			from businesses bb
+				join filings ff
+					on bb.id = ff.business_id 
+			where
+				bb.legal_type = 'BEN'
+				and ff.status = 'DRAFT'
+		)
+	)
+order by
+	b.identifier desc;
 
--- query to get all ACTIVE businesses (BENs) which have "in progress" drafts
-select distinct(b.identifier), f.id, f.filing_type
-from businesses b join filings f on b.id = f.business_id
-where b.legal_type = 'BEN' and b.state = 'ACTIVE' and f.status = 'DRAFT'
-order by b.identifier asc;
+-- query to get ACTIVE businesses (BENs with No In-progress DRAFTS) for Corrections until 2025-05-02 3:16pm
+select 
+	b.identifier,
+	f.id,
+    f.filing_date
+from businesses b 
+	 join filings f 
+	 	on b.id = f.business_id
+where
+	b.legal_type = 'BEN' 
+	and f.filing_type = 'incorporationApplication'
+    and f.filing_date < TIMESTAMP '2025-05-02 15:16:00' AT TIME ZONE 'America/Los_Angeles'
+	and b.state = 'ACTIVE'
+	and (
+		b.identifier NOT IN (
+			select DISTINCT(bb.identifier) 
+			from businesses bb
+				join filings ff
+					on bb.id = ff.business_id 
+			where
+				bb.legal_type = 'BEN' 
+				and bb.state = 'ACTIVE'
+				and ff.status = 'DRAFT'
+		)
+	)
+order by
+	b.identifier desc;
+
