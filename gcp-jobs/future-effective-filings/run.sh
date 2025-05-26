c34292a86297db83d6d5b5ba018537eeb8ed7ec2
Kial <kialj876@gmail.com>
2025-05-26 11:13:40 -0400

    GCP Migration: tweaks to jobs run.sh (#3510)
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- gcp-jobs/future-effective-filings/run.sh
+++ gcp-jobs/future-effective-filings/run.sh
@@ -1,3 +1,4 @@
+#! /bin/sh
 echo 'run future effective filings'
 python src/future_effective_filings/file_future_effective.py
 echo 'run future effective filings done'
--- gcp-jobs/involuntary-dissolutions/run.sh
+++ gcp-jobs/involuntary-dissolutions/run.sh
@@ -1,3 +1,4 @@
+#! /bin/sh
 echo 'run involuntary dissolutions'
 python src/involuntary_dissolutions/involuntary_dissolutions.py
 echo 'run involuntary dissolutions done'
