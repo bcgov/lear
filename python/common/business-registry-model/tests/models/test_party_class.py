560934fa461e1a83eedaba90ffda75861f4b963a
deetz99 <dietrich@wolpert.ca>
2025-06-04 16:02:02 -0700

    update tests to use data from bulk insert

--- python/common/business-registry-model/tests/models/test_party_class.py
+++ python/common/business-registry-model/tests/models/test_party_class.py
@@ -17,6 +17,7 @@
 Test-Suite to ensure that the PartyClass Model is working as expected.
 """
 import datetime
+import pytest
 
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -24,13 +25,8 @@ from sqlalchemy.orm import Session
 from business_model.models import PartyClass, PartyRole
 from business_model.models.types.party_class_type import PartyClassType
 
-party_class_data = [
-    {"class_type": PartyClassType.DIRECTOR, "short_description": "Director Desc", "full_description": "Full Desc 1"},
-    {"class_type": PartyClassType.AGENT, "short_description": "Agent Desc", "full_description": "Full Desc 2"},
-    {"class_type": PartyClassType.OFFICER, "short_description": "Officer Desc", "full_description": "Full Desc 3"},
-    {"class_type": PartyClassType.ATTORNEY, "short_description": "Attorney Desc", "full_description": "Full Desc 4"},
-]
 
+@pytest.mark.skip('All PartyClassTypes already set in bulk_insert migration file')
 def test_party_class_save(session: Session):
     """Assert that the party class saves correctly."""
     party_class = PartyClass(
@@ -46,40 +42,24 @@ def test_party_class_save(session: Session):
 
 def test_party_class_json(session: Session):
     """Assert the json format of party class."""
-    party_class = PartyClass(
-        class_type=PartyClassType.DIRECTOR,
-        short_description="Short Description",
-        full_description="Full Description"
-    )
+    party_class = PartyClass.find_by_class_type(PartyClassType.DIRECTOR)
 
-    session.add(party_class)
-    session.flush()
+    json = party_class.json
+    
+    assert json["id"]
+    assert json["classType"] == 'DIRECTOR'
+    assert isinstance(json["shortDescription"], str)
+    assert len(json["shortDescription"]) > 0
 
-    party_class_json = {
-        'id': 1,
-        'classType': 'director',
-        'shortDescription': 'Short Description',
-        'fullDescription': 'Full Description',
-    }
-
-    assert party_class.json == party_class_json
+    assert isinstance(json["fullDescription"], str)
+    assert len(json["fullDescription"]) > 0
 
 
 def test_party_class_find_by_internal_id(session: Session):
     """Assert the party class can be found by id."""
-    # load 1 of each class type into db
-    for data in party_class_data:
-        party_class = PartyClass(
-            class_type=data['class_type'],
-            short_description=data['short_description'],
-            full_description=data['full_description']
-        )
-        session.add(party_class)
-
-    session.flush()
-
+    # ONLY 4 PARTYCLASSES
     # assert 4 items can be found by id
-    for i in range(len(party_class_data)):
+    for i in range(4):
         target_id = i + 1
         found = PartyClass.find_by_internal_id(target_id)
 
@@ -92,54 +72,37 @@ def test_party_class_find_by_internal_id(session: Session):
 
 def test_party_class_find_by_class_type(session: Session):
     """Assert the party class can be found by its class type."""
-    # load 1 of each class type into db
-    for data in party_class_data:
-        party_class = PartyClass(
-            class_type=data['class_type'],
-            short_description=data['short_description'],
-            full_description=data['full_description']
-        )
-        session.add(party_class)
-
-    session.flush()
-
+    # ONLY 4 PARTYCLASSES
     # assert 4 items can be found by class type
-    for data in party_class_data:
-        target_class = data["class_type"]
-        found = PartyClass.find_by_class_type(target_class)
+    party_class_types = [PartyClassType.DIRECTOR, PartyClassType.AGENT, PartyClassType.ATTORNEY, PartyClassType.OFFICER]
+
+    for type in party_class_types:
+        found = PartyClass.find_by_class_type(type)
 
         assert found is not None
-        assert found.class_type == target_class
+        assert found.class_type == type
 
 
 def test_party_class_party_role_relationship(session: Session):
     """Assert the relationship between PartyClass and PartyRole"""
-    party_class = PartyClass(
-        class_type=PartyClassType.OFFICER,
-        short_description="Short Desc",
-        full_description="Full Desc"
-    )
-
     role_1 = PartyRole(
         role=PartyRole.RoleTypes.CEO.value,
         appointment_date=datetime.datetime(2022, 5, 17),
         cessation_date=None,
-        party_class_type=party_class.class_type
+        party_class_type=PartyClassType.OFFICER
     )
 
     role_2 = PartyRole(
         role=PartyRole.RoleTypes.CHAIR.value,
         appointment_date=datetime.datetime(2022, 5, 17),
         cessation_date=None,
-        party_class_type=party_class.class_type
+        party_class_type=PartyClassType.OFFICER
     )
 
-    session.add_all([party_class, role_1, role_2])
+    session.add_all([role_1, role_2])
     session.flush()
 
-    assert party_class.id
-
-    parent = session.execute(select(PartyClass).where(PartyClass.id == party_class.id)).scalar_one_or_none()
+    parent = PartyClass.find_by_class_type(PartyClassType.OFFICER)
 
     assert parent.id
 
