b33cbdfa061c7f381955e57bd65532c8b85b4f2a
Qin <Arwen.Qin@gov.bc.ca>
2025-06-14 19:14:28 -0700

    add intent_to_liquidate unit tests
    Signed-off-by: Qin <Arwen.Qin@gov.bc.ca>

---
+++ queue_services/entity-filer/tests/unit/test_worker/test_intent_to_liquidate.py
@@ -0,0 +1,74 @@
+# Copyright © 2025 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Unit Tests for the Intent to Liquidate filing."""
+
+import copy
+import random
+
+import pytest
+from legal_api.models import Business, Filing
+from registry_schemas.example_data import INTENT_TO_LIQUIDATE, FILING_TEMPLATE
+
+from entity_filer.worker import process_filing
+from tests.unit import create_business, create_filing
+
+
+@pytest.mark.parametrize(
+    'test_name, legal_type, has_court_order',
+    [
+        ('bc_company_with_court_order', 'BC', True),
+        ('bc_company_no_court_order', 'BC', False),
+        ('ben_company_with_court_order', 'BEN', True),
+        ('ben_company_no_court_order', 'BEN', False),
+    ]
+)
+async def test_intent_to_liquidate_filing_process(app, session, test_name, legal_type, has_court_order):
+    """Assert that the intent to liquidate object is correctly populated to model objects."""
+    # Setup
+    identifier = f'{legal_type}1234567'
+    business = create_business(identifier, legal_type=legal_type)
+    business.in_liquidation = False
+    business.save()
+
+    # Create filing
+    filing_json = copy.deepcopy(FILING_TEMPLATE)
+    filing_json['filing']['header']['name'] = 'intentToLiquidate'
+    filing_json['filing']['business']['identifier'] = identifier
+    filing_json['filing']['intentToLiquidate'] = copy.deepcopy(INTENT_TO_LIQUIDATE)
+
+    payment_id = str(random.SystemRandom().getrandbits(0x58))
+    filing = create_filing(payment_id, filing_json, business_id=business.id)
+    filing_msg = {'filing': {'id': filing.id}}
+
+    # Test
+    await process_filing(filing_msg, app)
+
+    # Assertions
+    filing = Filing.find_by_id(filing.id)
+    business = Business.find_by_internal_id(business.id)
+    
+    assert filing.status == Filing.Status.COMPLETED.value
+    assert business.in_liquidation is True
+    assert len(filing.comments) == 1
+    
+    # Check filing metadata
+    filing_meta = filing.meta_data.get('intentToLiquidate', {})
+    assert filing_meta['dateOfCommencementOfLiquidation'] == '2025-05-15'
+    assert filing_meta['liquidationOfficer'] is not None
+    assert filing_meta['liquidationOffice'] is not None
+    
+    # Check comment was added
+    comment = filing.comments[0]
+    assert 'Liquidation is scheduled to commence on 2025-05-15' in comment.comment
+    assert comment.staff_id == filing.submitter_id
