98e5bd6c0221b21f76a7a39972a075674563c5a0
BrandonSharratt <brandon@highwaythreesolutions.com>
2025-07-14 11:46:41 -0700

    fixes and feedback

--- legal-api/src/legal_api/scripts/document_service_import.py
+++ legal-api/src/legal_api/scripts/document_service_import.py
@@ -14,6 +14,7 @@ specific to colin ids in the system can be imported and put into the table.
 """
 import requests
 import click
+import sys
 from flask import Blueprint, current_app
 
 from legal_api.models import db, Filing
@@ -26,43 +27,45 @@ from legal_api.services import AccountService
 document_service_bp = Blueprint('document_service', __name__)
 
 @document_service_bp.cli.command('import')
-@click.option("business_identifier", default="", help="Business id to import documents for")
+@click.option("--business_identifier", default="", help="Business id to import documents for")
 def import_documents(business_identifier):
     """
     Import documents from document service api.
     """
+    # pylint: disable-msg=too-many-locals
     current_app.logger.info("Import documents started")
     init_db(current_app)
     url = current_app.config.get('DOCUMENT_API_URL')
     version = current_app.config.get('DOCUMENT_API_VERSION')
-    requestBaseUrl = f'{url}{version}/application-reports/events'
+    request_base_url = f'{url}{version}/application-reports/events'
     token = AccountService.get_bearer_token()
     api_key = current_app.config.get('DOCUMENT_API_KEY')
 
     # This value doesn't affect the results, and is used for auditing purposes on the DRS side
-    account_id = "LEAR-API"
+    account_id = "LEAR-IMPORT-SCRIPT"
 
     query = db.session\
         .query(Filing)\
         .filter(Filing.source == 'COLIN')\
-    
-    if business_identifier != "":
+
+    if business_identifier:
         business = Business.find_by_identifier(business_identifier)
         if business is None:
             current_app.logger.info(
               f"Business {business_identifier} not found"
             )
-            exit(1)
+            sys.exit(1)
         query = query.filter(Filing.business_id == business.id)
-    
+
     colin_filings = query.all()
     count = 0
     num_filings = len(colin_filings)
+    current_app.logger.info(f'Found {num_filings} filings to import documents for')
     imported = 0
     for filing in colin_filings:
         colin_event_id = ColinEventId.get_by_filing_id(filing.id)
         for event_id in colin_event_id:
-            req_url = f'{requestBaseUrl}/{event_id}'
+            req_url = f'{request_base_url}/{event_id}'
             headers = {
                 'X-ApiKey': api_key,
                 'Account-Id': account_id,
@@ -88,7 +91,15 @@ def import_documents(business_identifier):
                         new_document.save()
                     else:
                         pass # Already imported
+            else:
+                current_app.logger.info(
+                    f"Failed to import documents for filing {filing.id}, status code: {response.status_code}, {req_url}"
+                )
         count += 1
-        if (count % 100 == 0):
+        if count % 100 == 0:
             current_app.logger.info(f'Processed {count} of {num_filings}')
-    current_app.logger.info(f"Import documents completed, {imported} documents imported")
+    summary = "Import documents completed"
+    if business_identifier:
+        summary = summary + f" for business {business_identifier}"
+    summary = summary + f", {imported} documents imported from {num_filings} filings"
+    current_app.logger.info(summary)
