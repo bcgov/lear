4a6e4e44c3e2dd08dd49a306e4480a2054b2350a
BrandonSharratt <brandon@highwaythreesolutions.com>
2025-07-15 11:19:11 -0700

    fix lint

--- legal-api/src/legal_api/__init__.py
+++ legal-api/src/legal_api/__init__.py
@@ -27,9 +27,7 @@ from legal_api import config, models
 from legal_api.models import db
 from legal_api.models.db import init_db
 from legal_api.resources import endpoints
-from legal_api.scripts.document_service_import import (
-  document_service_bp
-)  # noqa: I001; pylint: disable=ungrouped-imports; conflicts with Flake8;
+from legal_api.scripts.document_service_import import document_service_bp  # noqa: I001, E501; pylint: disable=ungrouped-imports; conflicts with Flake8; isort: skip
 from legal_api.schemas import rsbc_schemas
 from legal_api.services import digital_credentials, flags, gcp_queue, queue
 from legal_api.services.authz import cache
