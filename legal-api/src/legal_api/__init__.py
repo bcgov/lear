028756e4eacf92708e90e78b358a207d784d9b87
BrandonSharratt <brandon@highwaythreesolutions.com>
2025-07-15 10:56:30 -0700

    fix

--- legal-api/src/legal_api/__init__.py
+++ legal-api/src/legal_api/__init__.py
@@ -28,8 +28,8 @@ from legal_api.models import db
 from legal_api.models.db import init_db
 from legal_api.resources import endpoints
 from legal_api.scripts.document_service_import import (
-  document_service_bp  # noqa: I001; pylint: disable=ungrouped-imports; conflicts with Flake8; isort: skip
-)
+  document_service_bp
+) # noqa: I001; pylint: disable=ungrouped-imports; conflicts with Flake8;
 from legal_api.schemas import rsbc_schemas
 from legal_api.services import digital_credentials, flags, gcp_queue, queue
 from legal_api.services.authz import cache
