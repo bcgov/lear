6aad13126a3c8c7e8f301a2c49aa7918607fce0e
Lucas O'Neil <loneil@users.noreply.github.com>
2025-07-23 08:58:09 -0700

    Digital Credentials DBC Phase 2 additions (#3642)
    Signed-off-by: Lucas <lucasoneil@gmail.com>

--- python/common/business-registry-model/src/business_model/models/dc_connection.py
+++ python/common/business-registry-model/src/business_model/models/dc_connection.py
@@ -1,4 +1,4 @@
-# Copyright © 2022 Province of British Columbia
+# Copyright © 2025 Province of British Columbia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""This module holds data for digital credentials connection."""
+"""This module holds data for digital credentials connections."""
 from __future__ import annotations
 
 from enum import Enum
+from typing import Any
 
 from .db import db
 
@@ -23,39 +24,51 @@ class DCConnection(db.Model):  # pylint: disable=too-many-instance-attributes
     """This class manages the digital credentials connection."""
 
     class State(Enum):
-        """Enum of the connection states."""
+        """Enum of the didexchange protocol states."""
 
-        INIT = 'init'
-        INVITATION = 'invitation'
-        REQUEST = 'request'
-        RESPONSE = 'response'
-        ACTIVE = 'active'
+        START = 'start'  # although there is a start state we never see it
+        INVITATION_SENT = 'invitation-sent'
+        REQUEST_RECEIVED = 'request-received'
+        RESPONSE_SENT = 'response-sent'
+        ABANDONED = 'abandoned'
         COMPLETED = 'completed'
-        INACTIVE = 'inactive'
-        ERROR = 'error'
+        ACTIVE = 'active'  # artifact from the connection protocol
 
     __tablename__ = 'dc_connections'
 
     id = db.Column(db.Integer, primary_key=True)
     connection_id = db.Column('connection_id', db.String(100))
-    invitation_url = db.Column('invitation_url', db.String(4096))
-    is_active = db.Column('is_active', db.Boolean, default=False)
-
-    # connection_state values we recieve in webhook, but we may not need all of it
+    # connection_state values we receive in webhook, but we may not need all of it
     connection_state = db.Column('connection_state', db.String(50))
+    invitation_url = db.Column('invitation_url', db.String(4096))
 
-    business_id = db.Column('business_id', db.Integer, db.ForeignKey('businesses.id'))
+    is_active = db.Column('is_active', db.Boolean, default=False)
+    is_attested = db.Column('is_attested', db.Boolean, default=False)
+    last_attested = db.Column('last_attested', db.DateTime, default=None)
+
+    # DEPRECATED: use business_user_id instead, remove when all references are removed
+    business_id = db.Column('business_id', db.Integer,
+                            db.ForeignKey('businesses.id'), nullable=False)
+    business_user_id = db.Column('business_user_id', db.Integer, db.ForeignKey(
+        'dc_business_users.id'), nullable=False)
+
+    # relationships
+    business_user = db.relationship(
+        'DCBusinessUser', backref='connections', foreign_keys=[business_user_id])
 
     @property
     def json(self):
         """Return a dict of this object, with keys in JSON format."""
         dc_connection = {
             'id': self.id,
-            'businessId': self.business_id,
             'connectionId': self.connection_id,
+            'connectionState': self.connection_state,
             'invitationUrl': self.invitation_url,
             'isActive': self.is_active,
-            'connectionState': self.connection_state
+            'isAttested': self.is_attested,
+            'lastAttested': self.last_attested,
+            'businessId': self.business_id,  # DEPRECATED
+            'businessUserId': self.business_user_id,
         }
         return dc_connection
 
@@ -64,17 +77,21 @@ class DCConnection(db.Model):  # pylint: disable=too-many-instance-attributes
         db.session.add(self)
         db.session.commit()
 
+    def save_to_session(self):
+        """Save to the session, do not commit immediately."""
+        db.session.add(self)
+
     def delete(self):
         """Delete the object from the database immediately."""
         db.session.delete(self)
         db.session.commit()
 
     @classmethod
-    def find_by_id(cls, dc_connection_id: str) -> DCConnection:
+    def find_by_id(cls, connection_id: int) -> DCConnection:
         """Return the digital credential connection matching the id."""
         dc_connection = None
-        if dc_connection_id:
-            dc_connection = cls.query.filter_by(id=dc_connection_id).one_or_none()
+        if connection_id:
+            dc_connection = cls.query.filter_by(id=connection_id).one_or_none()
         return dc_connection
 
     @classmethod
@@ -82,32 +99,50 @@ class DCConnection(db.Model):  # pylint: disable=too-many-instance-attributes
         """Return the digital credential connection matching the connection_id."""
         dc_connection = None
         if connection_id:
-            dc_connection = cls.query.filter(DCConnection.connection_id == connection_id).one_or_none()
+            dc_connection = cls.query.filter(
+                DCConnection.connection_id == connection_id).one_or_none()
         return dc_connection
 
     @classmethod
-    def find_active_by(cls, business_id: str) -> DCConnection:
-        """Return the active digital credential connection matching the business_id."""
+    def find_by_business_user_id(cls, business_user_id) -> DCConnection:
+        """Return the digital credential connection matching the business_user_id."""
         dc_connection = None
-        if business_id:
-            dc_connection = (
-              cls.query
-                 .filter(DCConnection.business_id == business_id)
-                 .filter(DCConnection.is_active == True)  # pylint: disable=singleton-comparison
-                 .one_or_none())
+        if business_user_id:
+            dc_connection = cls.query.filter(
+                DCConnection.business_user_id == business_user_id).one_or_none()
         return dc_connection
 
     @classmethod
-    def find_by(cls,
-                business_id: int = None,
-                connection_state: str = None) -> list[DCConnection]:
-        """Return the digital credential connection matching the filter."""
+    def find_active_by_business_user_id(cls, business_user_id) -> DCConnection:
+        """Return the active digital credential connection matching the business_user_id."""
+        dc_connection = None
+        if business_user_id:
+            dc_connection = (
+                cls.query
+                   .filter(DCConnection.business_user_id == business_user_id)
+                   .filter(DCConnection.is_active == True)  # pylint: disable=singleton-comparison
+                   .one_or_none())
+        return dc_connection
+
+    @classmethod
+    def find_state_by_business_user_id(cls, business_user_id, connection_state: DCConnection.State) -> DCConnection:
+        """Return the active digital credential connection matching the business_user_id."""
+        dc_connection = None
+        if business_user_id:
+            dc_connection = (
+                cls.query
+                   .filter(DCConnection.business_user_id == business_user_id)
+                   .filter(DCConnection.connection_state == connection_state)
+                   .one_or_none())
+        return dc_connection
+
+    @classmethod
+    def find_by_filters(cls, filters: list[Any] = None) -> list[DCConnection]:
+        """Return the digital credential connection matching any provided filter."""
         query = db.session.query(DCConnection)
 
-        if business_id:
-            query = query.filter(DCConnection.business_id == business_id)
-
-        if connection_state:
-            query = query.filter(DCConnection.connection_state == connection_state)
+        if filters:
+            for query_filter in filters:
+                query = query.filter(query_filter)
 
         return query.all()
