8650219c09e60d6b9fcc9d53501d84e549b86650
Megan Wong <Megan.a.wong@gov.bc.ca>
2025-07-28 11:29:59 -0700

    27028 - Update lint fix

--- legal-api/src/legal_api/services/business/validations/validator.py
+++ legal-api/src/legal_api/services/business/validations/validator.py
@@ -49,8 +49,7 @@ def validate_document_request(document_type, business: Business):
         if (status := document_rules.get('status', None)) and business.state != status:
             errors.append({'error': babel('Specified document type is not valid for the current entity status.')})
 
-        good_standing = document_rules.get('goodStanding', None)
-        if good_standing and not business.good_standing:
+        if document_rules.get('goodStanding', None) and not business.good_standing:
             errors.append({'error': babel('Specified document type is not valid for the current entity status.')})
 
     if errors:
