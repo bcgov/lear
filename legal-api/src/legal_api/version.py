78f802176cb258b1e23301dfd61b65405bf198fc
bobby <bobbyjin935@gmail.com>
2025-08-14 14:49:15 -0700

    revert version

--- legal-api/src/legal_api/version.py
+++ legal-api/src/legal_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.154.2'  # pylint: disable=invalid-name
+__version__ = '2.154.1'  # pylint: disable=invalid-name
