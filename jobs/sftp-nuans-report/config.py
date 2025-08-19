a72f40831b1a8338159389d9e6f27c2a638c8f59
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2025-08-18 21:29:05 -0700

    update python / clean up unused code

--- jobs/sftp-nuans-report/config.py
+++ jobs/sftp-nuans-report/config.py
@@ -1,5 +1,6 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+
+from dotenv import find_dotenv, load_dotenv
 
 # this will load all the envars from a .env file located in the project root (api)
 load_dotenv(find_dotenv())
@@ -19,18 +20,3 @@ class Config(object):
     EMAIL_SMTP = os.getenv('EMAIL_SMTP', '')
     ENVIRONMENT = os.getenv('ENVIRONMENT', '')
     MONTH_REPORT_DATES = os.getenv('MONTH_REPORT_DATES', '')
-
-    # POSTGRESQL
-    PG_USER = os.getenv('PG_USER', '')
-    PG_PASSWORD = os.getenv('PG_PASSWORD', '')
-    PG_NAME = os.getenv('PG_DB_NAME', '')
-    PG_HOST = os.getenv('PG_HOST', '')
-    PG_PORT = os.getenv('PG_PORT', '5432')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{host}:{port}/{name}'.format(
-        user=PG_USER,
-        password=PG_PASSWORD,
-        host=PG_HOST,
-        port=int(PG_PORT),
-        name=PG_NAME,
-    )
-
