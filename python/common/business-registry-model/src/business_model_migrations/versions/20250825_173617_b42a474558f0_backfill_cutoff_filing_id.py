73efe47a88767edbc9412e83c5b2f14b5cf60ef6
Vysakh Menon <vysakh.menon@aot-technologies.com>
2025-08-27 15:16:46 -0700

    lint fix

--- python/common/business-registry-model/src/business_model_migrations/versions/20250825_173617_b42a474558f0_backfill_cutoff_filing_id.py
+++ python/common/business-registry-model/src/business_model_migrations/versions/20250825_173617_b42a474558f0_backfill_cutoff_filing_id.py
@@ -5,8 +5,8 @@ Revises: dbf687803623
 Create Date: 2025-08-25 17:36:17.540478
 
 """
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = 'b42a474558f0'
