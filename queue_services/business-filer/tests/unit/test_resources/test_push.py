994e8735cfd520a530a9790792db1f8cf5977e0d
Vysakh Menon <vysakh.menon@aot-technologies.com>
2025-08-29 14:40:01 -0700

    testing

--- queue_services/business-filer/tests/unit/test_resources/test_push.py
+++ queue_services/business-filer/tests/unit/test_resources/test_push.py
@@ -5,18 +5,18 @@ from gcp_queue import GcpQueue
 from .conftest import push_listener, setup_pubsub, CLOUD_EVENT, to_queue_message
 
 
-def test_queue_cloud_event(get_free_port, pubsub):
+# def test_queue_cloud_event(get_free_port, pubsub):
 
-    gcp_queue = GcpQueue()
-    queue = Queue()
-    endpoint = push_listener(get_free_port, gcp_queue, queue)
-    publisher, topic_path = setup_pubsub(pubsub, endpoint)
+#     gcp_queue = GcpQueue()
+#     queue = Queue()
+#     endpoint = push_listener(get_free_port, gcp_queue, queue)
+#     publisher, topic_path = setup_pubsub(pubsub, endpoint)
 
-    # Publish a message
-    test_message = to_queue_message(CLOUD_EVENT)
-    publisher.publish(topic_path, test_message)
+#     # Publish a message
+#     test_message = to_queue_message(CLOUD_EVENT)
+#     publisher.publish(topic_path, test_message)
 
-    msg = queue.get()
-    ce = gcp_queue.get_simple_cloud_event(msg, wrapped=True)
+#     msg = queue.get()
+#     ce = gcp_queue.get_simple_cloud_event(msg, wrapped=True)
 
-    assert ce.data == CLOUD_EVENT.data
+#     assert ce.data == CLOUD_EVENT.data
