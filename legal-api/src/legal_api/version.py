f87f08f4557b4a8c0aa636658f7fc2eecddbf9fa
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2025-09-03 13:03:33 -0700

    Bump version to 2.155.2 (#3750)

--- legal-api/src/legal_api/version.py
+++ legal-api/src/legal_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.155.1'  # pylint: disable=invalid-name
+__version__ = '2.155.2'  # pylint: disable=invalid-name
