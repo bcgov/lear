57d6fd6bee9ca8ee6272aec3fb7d3cebc5a39c8d
Rajandeep Kaur <144159721+Rajandeep98@users.noreply.github.com>
2025-09-11 09:13:17 -0700

    29967 - Permissions Update - Part -1 (#3772)
    * 29967 - Permissions Update - Part -1
    
    * lint fix
    
    * updated
    
    * Update legal-api/src/legal_api/services/permissions.py
    
    Co-authored-by: Vysakh Menon <vysakh.menon@gov.bc.ca>
    
    * Update legal-api/src/legal_api/services/permissions.py
    
    Co-authored-by: Vysakh Menon <vysakh.menon@gov.bc.ca>
    
    ---------
    
    Co-authored-by: Vysakh Menon <vysakh.menon@gov.bc.ca>

--- legal-api/src/legal_api/services/permissions.py
+++ legal-api/src/legal_api/services/permissions.py
@@ -15,10 +15,12 @@
 # pylint: disable=too-many-lines
 """This manages all of the permissions service."""
 from enum import Enum
+from http import HTTPStatus
 
 from flask import current_app, g
 
 from legal_api.core.filing import Filing as CoreFiling
+from legal_api.errors import Error
 from legal_api.models.authorized_role_permission import AuthorizedRolePermission
 from legal_api.services import authz
 from legal_api.services.cache import cache
@@ -67,11 +69,11 @@ class ListActionsPermissionsAllowed(str, Enum):
     FIRM_ADD_BUSINESS = 'FIRM_ADD_BUSINESS'
     FIRM_EDITABLE_DBA = 'FIRM_EDITABLE_DBA'
     FIRM_EDITABLE_EMAIL_ADDRESS = 'FIRM_EDITABLE_EMAIL_ADDRESS'
+    FIRM_NO_MIN_START_DATE = 'FIRM_NO_MIN_START_DATE'
     FIRM_REPLACE_PERSON = 'FIRM_REPLACE_PERSON'
     OVERRIDE_NIGS='OVERRIDE_NIGS'
     STAFF_COMMENTS = 'STAFF_COMMENTS'
     STAFF_PAYMENT='STAFF_PAYMENT'
-
 
 class PermissionService:
     """Service to manage permissions for user roles."""
@@ -194,3 +196,16 @@ class PermissionService:
         else:
             current_app.logger.warning(f'User does not have permission for filing type: {filing_type}')
         return False
+
+    @staticmethod
+    def check_user_permission(required_permission, message: str = None) -> Error:
+        """Check if the user has the required permission."""
+        authorized_permissions = PermissionService.get_authorized_permissions_for_user()
+        if required_permission not in authorized_permissions:
+            return Error(
+                HTTPStatus.FORBIDDEN,
+                [{
+                    'message': message or f'Permission Denied - You do not have permissions to perform {required_permission} in filing.'
+                }]
+            )
+        return None
