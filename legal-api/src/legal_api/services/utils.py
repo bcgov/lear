49b8e348162d3644d466d4d0df2d302a4b5e42ef
Megan Wong <Megan.a.wong@gov.bc.ca>
2025-09-17 16:26:43 -0700

    30385 - Cleanup

--- legal-api/src/legal_api/services/utils.py
+++ legal-api/src/legal_api/services/utils.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Common utilities used by the services."""
-
 from datetime import date
 from typing import Dict
 
