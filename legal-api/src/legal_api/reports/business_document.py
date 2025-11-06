5d347e1c460f3c3b4ef5241108d512ab116d1797
ketaki-deodhar <ketaki.deodhar@gov.bc.ca>
2025-11-06 23:29:41 +0000

    29890 - updates

--- legal-api/src/legal_api/reports/business_document.py
+++ legal-api/src/legal_api/reports/business_document.py
@@ -415,11 +415,7 @@ class BusinessDocument:
         tombstone = self._business.is_tombstone
         if (amalgamating_businesses := AmalgamatingBusiness.get_all_revision(self._business.id, tombstone)):
             for amalgamating_business in amalgamating_businesses:
-                if tombstone:
-                    amalgamation = Amalgamation.get_revision_by_id(
-                        amalgamating_business.amalgamation_id, tombstone=True)
-                else:
-                    amalgamation = Amalgamation.get_revision_by_id(
+                amalgamation = Amalgamation.get_revision_by_id(
                         amalgamating_business.amalgamation_id, amalgamating_business.transaction_id)
                 filing = Filing.find_by_id(amalgamation.filing_id)
                 state_filing = self._format_state_filing(filing)
