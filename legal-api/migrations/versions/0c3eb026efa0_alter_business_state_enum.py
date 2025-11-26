e68996dabb6796ea7da5f56b57071419401a0add
Vysakh Menon <vysakh.menon@aot-technologies.com>
2025-11-26 12:16:52 -0800

    no message

--- legal-api/migrations/versions/0c3eb026efa0_alter_business_state_enum.py
+++ legal-api/migrations/versions/0c3eb026efa0_alter_business_state_enum.py
@@ -20,17 +20,20 @@ depends_on = None
 def upgrade():
     bind = op.get_bind()
     inspector = sa.inspect(bind)
-    op.execute("ALTER TYPE state RENAME TO state_old")
+    try:
+        op.execute("ALTER TYPE state RENAME TO state_old")
 
-    state_new = postgresql.ENUM('ACTIVE', 'HISTORICAL', name='state')
-    state_new.create(bind, checkfirst=True)
+        state_new = postgresql.ENUM('ACTIVE', 'HISTORICAL', name='state')
+        state_new.create(bind, checkfirst=True)
 
-    op.execute("ALTER TABLE businesses ALTER COLUMN state TYPE state USING state::text::state")
-    op.execute("ALTER TABLE businesses_version ALTER COLUMN state TYPE state USING state::text::state")
-    if 'businesses_bak' in inspector.get_table_names():
-        op.execute("ALTER TABLE businesses_bak ALTER COLUMN state TYPE state USING state::text::state")
+        op.execute("ALTER TABLE businesses ALTER COLUMN state TYPE state USING state::text::state")
+        op.execute("ALTER TABLE businesses_version ALTER COLUMN state TYPE state USING state::text::state")
+        if 'businesses_bak' in inspector.get_table_names():
+            op.execute("ALTER TABLE businesses_bak ALTER COLUMN state TYPE state USING state::text::state")
 
-    op.execute("DROP TYPE state_old")
+        op.execute("DROP TYPE state_old")
+    except Exception as err:
+        print(f"Failed to alter business state enum: {err!r}")
 
 
 def downgrade():
