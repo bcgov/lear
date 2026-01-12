ef22833a55ebc3dfa63555c983c5a7d475f684ab
Rajandeep <rajandeep.kaur@gov.bc.ca>
2026-01-12 09:11:39 -0800

    30751 - Amalgamation Override

--- legal-api/src/legal_api/services/filings/validations/amalgamation_application.py
+++ legal-api/src/legal_api/services/filings/validations/amalgamation_application.py
@@ -37,6 +37,7 @@ from legal_api.services.filings.validations.incorporation_application import (
     validate_offices,
     validate_parties_delivery_address,
 )
+from legal_api.services.permissions import ListActionsPermissionsAllowed, PermissionService
 from legal_api.services.utils import get_str
 from legal_api.utils.auth import jwt
 
@@ -275,11 +276,23 @@ def _validate_foreign_businesses(  # noqa: PLR0913
                 "path": amalgamating_business_path
             })
     else:
-        msg.append({
-            "error": (f"{foreign_legal_name} foreign corporation cannot "
-                      "be amalgamated except by Registries staff."),
-            "path": amalgamating_business_path
-        })
+        if flags.is_on("enabled-deeper-permission-action"):
+            permission_error = PermissionService.check_user_permission(
+                ListActionsPermissionsAllowed.AML_OVERRIDES.value,
+                message="Permission Denied - You do not have permissions to amalgamate a foreign corporation."
+            )
+            if permission_error:
+                msg.append({
+                    "error": permission_error.msg[0].get("message"),
+                    "path": amalgamating_business_path
+                })
+                return msg
+        else:
+            msg.append({
+                "error": (f"{foreign_legal_name} foreign corporation cannot "
+                        "be amalgamated except by Registries staff."),
+                "path": amalgamating_business_path
+            })
 
     return msg
 
