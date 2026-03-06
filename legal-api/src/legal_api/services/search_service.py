13625ce421520f3979a4a1b326d0fff7010ec6fc
argush3 <argus@highwaythreesolutions.com>
2026-03-06 09:20:43 -0800

    32736 Update /search/affiliation_mappings endpoint to support migrated businesses

--- legal-api/src/legal_api/services/search_service.py
+++ legal-api/src/legal_api/services/search_service.py
@@ -298,16 +298,6 @@ class BusinessSearchService:  # pylint: disable=too-many-public-methods
     @staticmethod
     def get_affiliation_mapping_results(identifiers):
         """Return affiliation mapping results for the given list of identifiers."""
-        query = db.session.query(
-            Business._identifier.label("identifier"),  # pylint: disable=protected-access
-            Filing
-            .filing_json["filing"][Filing._filing_type]["nameRequest"]["nrNumber"]  # pylint: disable=protected-access
-            .label("nrNumber"),
-            RegistrationBootstrap._identifier.label("bootstrapIdentifier")  # pylint: disable=protected-access
-        ).select_from(Filing) \
-            .outerjoin(Business, Filing.business_id == Business.id) \
-            .join(RegistrationBootstrap, Filing.temp_reg == RegistrationBootstrap.identifier)
-
         temp_identifiers, nr_identifiers, business_identifiers = [], [], []
         for identifier in identifiers:
             if identifier.startswith("T"):
@@ -316,19 +306,60 @@ class BusinessSearchService:  # pylint: disable=too-many-public-methods
                 nr_identifiers.append(identifier)
             else:
                 business_identifiers.append(identifier)
-        conditions = []
-        if business_identifiers:
-            conditions.append(Business._identifier.in_(business_identifiers))  # pylint: disable=protected-access
-        if nr_identifiers:
-            conditions.append(Filing
-                              .filing_json["filing"][Filing._filing_type]  # pylint: disable=protected-access
-                              ["nameRequest"]["nrNumber"]
-                              .astext.in_(nr_identifiers))
-        if temp_identifiers:
-            conditions.append(RegistrationBootstrap._identifier.in_(identifiers))  # pylint: disable=protected-access
-        query = query.filter(db.or_(*conditions))
 
-        rows = query.all()
-        result_list = [dict(row) for row in rows]
+        result_list = []
+
+        # Permanent business identifiers, including migrated businesses, can exist without any
+        # filing/bootstrap linkage and must be resolved directly from the businesses table.
+        if business_identifiers:
+            business_rows = db.session.query(
+                Business._identifier.label("identifier")  # pylint: disable=protected-access
+            ).filter(
+                Business._identifier.in_(business_identifiers)  # pylint: disable=protected-access
+            ).all()
+
+            result_list.extend([
+                {
+                    "identifier": row.identifier,
+                    "nrNumber": None,
+                    "bootstrapIdentifier": None
+                }
+                for row in business_rows
+            ])
+
+        draft_conditions = []
+        if nr_identifiers:
+            draft_conditions.append(
+                Filing
+                .filing_json["filing"][Filing._filing_type]  # pylint: disable=protected-access
+                ["nameRequest"]["nrNumber"]
+                .astext.in_(nr_identifiers)
+            )
+        if temp_identifiers:
+            draft_conditions.append(
+                RegistrationBootstrap._identifier.in_(temp_identifiers)  # pylint: disable=protected-access
+            )
+
+        if draft_conditions:
+            draft_rows = db.session.query(
+                Business._identifier.label("identifier"),  # pylint: disable=protected-access
+                Filing
+                .filing_json["filing"][Filing._filing_type]["nameRequest"]["nrNumber"]  # pylint: disable=protected-access
+                .label("nrNumber"),
+                RegistrationBootstrap._identifier.label("bootstrapIdentifier")  # pylint: disable=protected-access
+            ).select_from(Filing) \
+                .outerjoin(Business, Filing.business_id == Business.id) \
+                .join(RegistrationBootstrap, Filing.temp_reg == RegistrationBootstrap.identifier) \
+                .filter(db.or_(*draft_conditions)) \
+                .all()
+
+            result_list.extend([
+                {
+                    "identifier": row.identifier,
+                    "nrNumber": row.nrNumber,
+                    "bootstrapIdentifier": row.bootstrapIdentifier
+                }
+                for row in draft_rows
+            ])
 
         return result_list
