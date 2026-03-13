9d8d8a2d31b893e49daf810d5cbd0208c06b4307
Kial Jinnah <kialj876@gmail.com>
2026-03-13 09:39:40 -0400

    chore: ruff fix
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- python/common/business-registry-model/src/business_model_migrations/versions/b5ded56cab5b_filing_lear_only.py
+++ python/common/business-registry-model/src/business_model_migrations/versions/b5ded56cab5b_filing_lear_only.py
@@ -5,8 +5,8 @@ Revises: 2467e09986f2
 Create Date: 2026-03-13 09:27:35.973908
 
 """
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
