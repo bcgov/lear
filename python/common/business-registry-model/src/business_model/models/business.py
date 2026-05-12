25d275ab57281c080dcaff1c76fd3e81d92979cb
Kial Jinnah <kialj876@gmail.com>
2026-05-12 16:51:25 -0400

    Business Model - minor consolidation between legal-api model and common
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- python/common/business-registry-model/src/business_model/models/business.py
+++ python/common/business-registry-model/src/business_model/models/business.py
@@ -851,7 +851,7 @@ class Business(db.Model, Versioned):  # pylint: disable=too-many-instance-attrib
     @classmethod
     def get_expired_restoration(cls):
         """Return all identifier with an expired restoration_expiry_date."""
-        businesses = (db.session.query(Business.identifier)
+        businesses = (db.session.query(Business.identifier, Business.legal_type)
                       .filter(Business.restoration_expiry_date <= datetime.utcnow())
                       .all())
         return businesses
