c9c4669cbe5630e05a48179619c7c19432e6fdec
Lucas O'Neil <loneil@users.noreply.github.com>
2026-05-26 13:27:27 -0700

    Update to 2.171.7 (#4435)
    Signed-off-by: Lucas <lucasoneil@gmail.com>

--- legal-api/src/legal_api/version.py
+++ legal-api/src/legal_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = "2.171.6"  # pylint: disable=invalid-name
+__version__ = "2.171.7"  # pylint: disable=invalid-name
