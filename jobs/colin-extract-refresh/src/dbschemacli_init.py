c6ab8e5b35a8ec913b51d6d97fafe16491e5dc9f
Rajandeep <rajandeep.kaur@gov.bc.ca>
2026-07-06 14:51:46 -0700

    updated

--- jobs/colin-extract-refresh/src/dbschemacli_init.py
+++ jobs/colin-extract-refresh/src/dbschemacli_init.py
@@ -22,7 +22,7 @@ def write_dbschema_init(cfg: _Config) -> Path:
         'register driver PostgreSql org.postgresql.Driver jdbc:postgresql://<host>:<port>/<db>?reWriteBatchedInserts=true "port=5432"',
         'register driver Oracle oracle.jdbc.OracleDriver jdbc:oracle:thin:@<host>:<port>:<db> "port=1521"',
         f'connection my_proxy_test -d PostgreSql -u {user} -p {password} -h {host} -P {port} -D {name}',
-        f'connection ctst -d Oracle -u {ora_user} -p {ora_password} -h {ora_host} -P {ora_port} -D {ora_name}',
+        f'connection ctst_pg -d Oracle -u {ora_user} -p {ora_password} -h {ora_host} -P {ora_port} -D {ora_name}',
     ]
 
     init_path.write_text('\n'.join(lines) + '\n')
