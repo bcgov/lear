7b367a119e14a58bde4ce1ba78314c9cf8de0dcb
ketaki-deodhar <ketaki.deodhar@gov.bc.ca>
2026-07-22 15:43:46 -0700

    34349 - update to remove court oder columns from filings table

--- data-tool/flows/tombstone/tombstone_utils.py
+++ data-tool/flows/tombstone/tombstone_utils.py
@@ -503,9 +503,7 @@ def format_filings_data(data: dict, config=None) -> dict:
             'paper_only': paper_only,
             'status': status,
             'submitter_id': user_id,  # will be updated to real user_id when loading data into db
-            'submitter_roles': 'staff' if x.get('u_role_typ_cd') and x['u_role_typ_cd'].lower() == 'staff' else None,
-            'court_order_file_number' : x['court_order_num'],
-            'court_order_effect_of_order' : 'planOfArrangement' if x['arrangement_ind'] == True else None,
+            'submitter_roles': 'staff' if x.get('u_role_typ_cd') and x['u_role_typ_cd'].lower() == 'staff' else None
         }
 
         # conversion still need to populate create-new-business info
